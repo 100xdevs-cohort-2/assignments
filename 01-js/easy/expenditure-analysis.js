ad6a00c15b84d2cd1e47bf59c63518794079bb7e
Harshit Garg <harshit118garg@gmail.com>
2023-12-04 11:35:20 +0530

    solved easy questions

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,36 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
+function groupBy(array, property) {
+  return array.reduce((acc, obj) => {
+    const key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categorySeparated = groupBy(transactions, 'category');
+  let res = [];
+  for (cate in categorySeparated) {
+    let arr = categorySeparated[cate];
+    let c = arr.reduce(
+      (acc, curr) => {
+        (acc.category = curr.category),
+          (acc.totalSpent = acc.totalSpent + curr.price);
+        return acc;
+      },
+      {
+        category: "",
+        totalSpent: 0,
+      }
+    );
+    res.push(c);
+  }
+  return res;
 }
 
 module.exports = calculateTotalSpentByCategory;
