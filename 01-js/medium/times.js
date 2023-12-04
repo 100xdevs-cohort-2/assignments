25142c0f251cb28d5b6131978f908e1eb5dad98b
maharun0 <maharun.afroz@northsouth.edu>
2023-12-04 23:28:08 +0600

    solved times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,18 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date();
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++){
+        sum += i;
+    }
+
+    let endTime = new Date();
+
+    return endTime - startTime;
 }
+
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
