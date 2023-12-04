162bb5701d1be09e563bcef5dacac305ac1d0d32
Yash Dixit <ydixit007@gmail.com>
2023-12-04 20:44:36 +0530

    solved anagram, findLargestElement and countVowels

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length == str2.length) {
+    for (let index = 0; index < str1.length; index++) {
+      if (!str1.toLowerCase().includes(str2[index].toLowerCase())) {
+        return false;
+      }
+    }
+  } else{
+    return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
