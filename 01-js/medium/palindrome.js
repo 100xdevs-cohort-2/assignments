63b9d1b6c02525dc76b9ac71867e56878c03bcc5
Raj04 <rajjoinsall@gmail.com>
2023-12-04 21:54:55 +0530

    completed hard problems

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,10 +4,20 @@
 */
 
 function isPalindrome(str) {
-  if (str == str.split("").reverse()) {
+  if (str.length == 1) {
     return true;
   }
-  return false;
-}
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let lastIndex = str.length - 1;
 
+  for (let i = 0; i <= str.length / 2; i++) {
+    if (str[i] >= "a" && str[i] <= "z") {
+      if (!(str[i] == str[lastIndex])) {
+        return false;
+      }
+      lastIndex--;
+    }
+  }
+  return true;
+}
 module.exports = isPalindrome;
