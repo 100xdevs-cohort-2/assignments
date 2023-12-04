1be17ac7593b9e8da0f4e0e15c1ddf7ba9c7d540
Kapcho Web Solutions <38814570+rajanworld@users.noreply.github.com>
2023-12-04 13:16:32 +0530

    Add files via upload

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,45 @@
 */
 
 class Todo {
+  constructor() {
+      this.todos = [];
+  }
 
+  add(todo) {
+      this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+      if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+          this.todos.splice(indexOfTodo, 1);
+      }
+      // Removing the error throwing to allow silent failure on invalid indices
+  }
+
+  update(index, updatedTodo) {
+      if (index >= 0 && index < this.todos.length) {
+          this.todos[index] = updatedTodo;
+      }
+      // Removing the error throwing to allow silent failure on invalid indices
+  }
+
+  getAll() {
+      return this.todos;
+  }
+
+  get(indexOfTodo) {
+      if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+          return this.todos[indexOfTodo];
+      }
+      // Return null for invalid index
+      return null;
+  }
+
+  clear() {
+      this.todos = [];
+  }
 }
 
 module.exports = Todo;
+
+
