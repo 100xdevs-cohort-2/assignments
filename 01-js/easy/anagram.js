41533342acb2938356a1e6dbc17ff655ecf675e2
abananyabera <abananyabera@gmail.com>
2023-12-04 23:15:08 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
-
+  arr1 = str1.toLowerCase().split('');
+  arr2 = str2.toLowerCase().split('');
+  arr1.sort().toString();
+  arr2.sort().toString();
+  if(arr1.toString() === arr2.toString()){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
+//console.log(isAnagram('listen', 'silent'));
 
 module.exports = isAnagram;
