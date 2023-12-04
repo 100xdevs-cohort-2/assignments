acad1a770b85e63bb4d56ff23e7aac4378a08733
janardan <janardonhazarika7@gmail.com>
2023-12-04 21:44:45 +0530

    solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,65 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let obj = []
+  for (let i = 0; i < transactions.length; i++) {
+    let category = transactions[i].category;
+
+    if (category in obj) {
+      obj[category] += transactions[i].price
+    }
+    else {
+      obj[category] = transactions[i].price
+    }
+  }
+
+  let objs = []
+  for (k in obj) {
+    objs.push({
+      category: k,
+      totalSpent: obj[k]
+    })
+  }
+
+  return objs;
 }
+
+console.log(calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+]))
 
 module.exports = calculateTotalSpentByCategory;
