d8435ab34b19f4bec87e0ee7c52a31fd437f3c31
Devananth <devananthnkl@gmail.com>
2023-12-04 10:32:33 +0530

    done:completed easy prblms

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,34 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
+function isCategoryAlreadPresent(category, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].category === category) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let resultArr = [];
+  for (let i = 0; i < transactions.length; i++) {
+    let currentCategory = transactions[i].category;
+    let indexOfCategoryInReslutArr = isCategoryAlreadPresent(
+      currentCategory,
+      resultArr
+    );
+    if (indexOfCategoryInReslutArr !== -1) {
+      resultArr[indexOfCategoryInReslutArr].totalSpent += transactions[i].price;
+    } else {
+      resultArr.push({
+        category: currentCategory,
+        totalSpent: transactions[i].price,
+      });
+    }
+  }
+  return resultArr;
 }
 
 module.exports = calculateTotalSpentByCategory;
