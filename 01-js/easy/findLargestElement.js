373d077af8129d18ce74740ba316d78a1dbfa1fc
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2023-12-04 18:25:28 +0530

    Add defintion for findLargestElement function

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,17 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length == 0) {
+    return;
+  }
+
+  let maxElement = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    maxElement = maxElement > numbers[i] ? maxElement : numbers[i];
+  }
+
+  return maxElement;
 }
 
 module.exports = findLargestElement;
