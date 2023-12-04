013e3c9b306df08654cda6bf8240f7f6e80c0fbc
AdarshJha <adarsh2005j@gmail.com>
2023-12-04 19:38:49 +0530

    easy-q1-anagram

---
+++ 01-js/rough.js
@@ -0,0 +1,15 @@
+/* Easy Problems 
+
+let str1 = "Debit Card" // with spaces input
+let str2 = "Bad Credit" // with spaces input
+
+let string1 = str1.toLowerCase().split('').sort().join('');; 
+// console.log(string1);
+// string1 = string1.join('');
+console.log(string1);
+let string2 = str2.toLowerCase().split('').sort().join('');;
+// console.log(string2);
+// string2 = string2.join('');
+console.log(string2);
+
+*/
