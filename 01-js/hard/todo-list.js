aca85d0658ddc5ec18db969c3b2f3d21f4ecd9c2
janardan <janardonhazarika7@gmail.com>
2023-12-05 01:41:09 +0530

    easy and hard done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,56 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = []
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(idx) {
+    if (idx >= this.todos.length || idx < 0) {
+      throw new Error('Index out of bounds!');
+    }
+    else {
+      if (idx === this.todos.length - 1) {
+        this.todos.pop();
+      }
+      else {
+        for (let i = idx; i < this.todos.length; i++) {
+          this.todos[i] = this.todos[i + 1]
+        }
+        this.todos.pop();
+      }
+    }
+  }
+
+  update(idx, updatedTodo) {
+    if (idx >= this.todos.length || idx < 0) {
+      throw new Error('Index out of bounds!');
+    }
+    else {
+      this.todos[idx] = updatedTodo;
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(idx) {
+    if (idx >= this.todos.length || idx < 0) {
+      throw new Error('Index out of bounds!');
+    }
+    else {
+      return this.todos[idx]
+    }
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
