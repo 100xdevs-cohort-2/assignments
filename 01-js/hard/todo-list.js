d9a2a470ff7ba199620263973fa03c4833abf369
akshay-thakur-dc <akshay.thakur@digicert.com>
2023-12-04 18:53:27 +0530

    completed assignment

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -10,7 +10,52 @@
   Once you've implemented the logic, test your code by running
 */
 
+
 class Todo {
+  constructor(){
+    this.todos = {};
+    this.idx = 0;
+  }
+
+  add(todo){
+    this.todos[this.idx] = todo;
+    this.idx+=1;
+  }
+
+  remove(index){
+    if(this.todos.hasOwnProperty(index)){
+       console.log('deleting property at index '+ index);
+       delete this.todos[index];
+    }
+  }
+
+  update(index, todo){
+
+    if(this.todos.hasOwnProperty(index)){
+      this.todos[index] = todo;
+    }
+  }
+
+  getAll(){
+    let curr_todos = [];
+    for (let key in this.todos){
+      curr_todos.push(this.todos[key]);
+    }
+
+    return curr_todos;
+  }
+
+  get(index){
+    if((this.todos.hasOwnProperty(index))){
+      return this.todos[index];
+    }else{
+      return null;
+    }
+  }
+
+  clear(){
+    this.todos = {};
+  }
 
 }
 
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const start = Date.now();
+    let sum = 0;
+    for(let i = 1; i<=n; i++){
+        sum += i;
+    }
+    const end = Date.now();
+    return (end-start)/1000;
 }
+
+console.log(calculateTime(100));
+console.log(calculateTime(100000));
+console.log(calculateTime(1000000000));
