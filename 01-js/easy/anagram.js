9d6a06fb65b7f3b84b33e467cd8b9df5c84c4c4f
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-04 22:59:02 +0530

    solved some

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,34 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
-
 function isAnagram(str1, str2) {
+ 
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
+  
+  if (cleanStr1.length !== cleanStr2.length) {
+      return false;
+  }
+
+ 
+  const frequencyMap1 = {};
+  const frequencyMap2 = {};
+
+ 
+  for (let char of cleanStr1) {
+      frequencyMap1[char] = (frequencyMap1[char] || 0) + 1;
+  }
+
+  
+  for (let char of cleanStr2) {
+      frequencyMap2[char] = (frequencyMap2[char] || 0) + 1;
+  }
+
+   
+  for (let key in frequencyMap1) {
+      if (frequencyMap1[key] !== frequencyMap2[key]) {
+          return false;
+      }
+  }
+
+  return true;
 }
-
 module.exports = isAnagram;
--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -1,12 +1,14 @@
-/*
-  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
-  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
-
-  Once you've implemented the logic, test your code by running
-*/
-
 function countVowels(str) {
-    // Your code here
+  let count = 0;
+  let strLower = str.toLowerCase();
+
+  for (let i = 0; i < strLower.length; i++) {
+      if ('aeiou'.includes(strLower[i])) {
+          count++;
+      }
+  }
+
+  return count;
 }
 
 module.exports = countVowels;
