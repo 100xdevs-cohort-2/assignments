51e255b8511de4f55d9e95bf3095a5bb48b4ba80
Kapcho Web Solutions <38814570+rajanworld@users.noreply.github.com>
2023-12-04 13:16:03 +0530

    Add files via upload

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+    if (numbers.length === 0) {
+        return undefined; // Return undefined if the array is empty
+    }
+    let findLargestNumber = numbers[0];
+    for (let i = 1; i < numbers.length; i++) { 
+        if (numbers[i] > findLargestNumber) {
+            findLargestNumber = numbers[i];
+        }
+    }
+    return findLargestNumber;
 }
 
 module.exports = findLargestElement;
