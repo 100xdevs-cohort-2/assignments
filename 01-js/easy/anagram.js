69dcffbfe79c0a9c545ef7dd70e49c56b80194cc
aman singh <aman.singh@codibex.com>
2023-12-04 14:09:50 +0530

    completed easy assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,38 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
 
+function isAnagram(str1, str2) {
+  let map = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let character = str1[i].toLowerCase(); // Convert to lowercase
+    if (map[character] == undefined) {
+      map[character] = 1;
+    } else {
+      map[character] = map[character] + 1;
+    }
+  }
+
+  for (let index = 0; index < str2.length; index++) {
+    let character = str2[index].toLowerCase(); // Convert to lowercase
+
+    if (map[character] != undefined) {
+      let count = map[character];
+      count = count - 1;
+
+      if (count == 0) {
+        delete map[character];
+      } else {
+        map[character] = count;
+      }
+
+    } else {
+      return false;
+    }
+  }
+
+  return Object.keys(map).length === 0;
 }
 
 module.exports = isAnagram;
