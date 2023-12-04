82813d939f8851c85f9d349ac7564fac9565e1ed
Umera.Z <cryptokukduku@gmail.com>
2023-12-04 12:36:53 +0530

    anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,13 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
-  const isStr1 = str1 ==="anagrams";
-  const isStr2 = str2 ==="anagrams";
-  return {
-    str1: isStr1,
-    str2: isStr2
-  }
-}
-console.log(isAnagram("story", "create"));
+  const Str1 = str1.toLowerCase().split(''); //spliting words separately
+  const join1 = Str1.sort() //arranging all alphabetes in A-Z order and then joining all letters together
 
+  const Str2 = str2.split('');
+  const join2 = Str2.sort().join('');
+
+  return join1 === join2; //comparing both the parameters
+}
 module.exports = isAnagram;
