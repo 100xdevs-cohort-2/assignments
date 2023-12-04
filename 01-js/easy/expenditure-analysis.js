04d2807d4851ab2d2646adc64cbd27663585c147
piyush <piyushtiwarindls220@gmail.com>
2023-12-04 17:14:54 +0530

    Done all easy questions

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,9 +4,30 @@
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
-
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let ans = [];
+
+  for (let i = 0; i < transactions.length; i++) {
+    let found = false;
+
+    for (let j = 0; j < ans.length; j++) {
+      if (transactions[i].category === ans[j].category) {
+        ans[j].totalSpent += transactions[i].price;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      ans.push({
+        category: transactions[i].category,
+        totalSpent: transactions[i].price
+      });
+    }
+  }
+
+  return ans;
 }
+
 
 module.exports = calculateTotalSpentByCategory;
