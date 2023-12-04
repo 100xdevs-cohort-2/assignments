6f2e3d7549f2ab431fee37543c33646411c209aa
Vasanth971 <145746283+Vasanth971@users.noreply.github.com>
2023-12-04 16:14:15 +0530

    Update findLargestElement.js-done by VK

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let val = numbers[0];
+    for(let i=0; i<numbers.length; i++){
+        if (numbers[i] > val){
+            val = numbers[i];
+        }
+    }
+    return val
 }
 
 module.exports = findLargestElement;
