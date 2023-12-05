502c69e95f71a52ce8912c56b88230052f7e6caf
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2023-12-05 14:04:46 +0530

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,21 +1,54 @@
-/*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+class Calculator {
+    constructor() {
+        this.result = 0;
+    }
 
-  Once you've implemented the logic, test your code by running
-*/
+    add(number) {
+        this.result += number;
+    }
 
-class Calculator {}
+    subtract(number) {
+        this.result -= number;
+    }
 
-module.exports = Calculator;
+    multiply(number) {
+        this.result *= number;
+    }
+
+    divide(number) {
+        if (number === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        this.result /= number;
+    }
+
+    clear() {
+        this.result = 0;
+    }
+
+    getResult() {
+        return this.result;
+    }
+
+    calculate(expression) {
+        // Remove extra spaces and validate expression
+        expression = expression.replace(/\s+/g, "");
+        if (!/^[0-9+\-*/().]+$/.test(expression)) {
+            throw new Error("Invalid expression");
+        }
+
+        // Evaluate the expression
+        this.result = eval(expression);
+    }
+}
+
+// Example usage:
+const calculator = new Calculator();
+
+calculator.add(5);
+calculator.multiply(2);
+console.log(calculator.getResult()); // Output: 10
+
+calculator.clear();
+calculator.calculate("10 + 2 * (6 - (4 + 1) / 2) + 7");
+console.log(calculator.getResult()); // Output: 24
