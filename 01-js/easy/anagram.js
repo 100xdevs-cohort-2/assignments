213724085a4d25a606a937d5a32e6381e377dd0c
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-12-05 14:15:32 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,9 +3,27 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
-
+// METHIOD-1
 function isAnagram(str1, str2) {
+  const cleanedStr1=str1.replace(/ /g,'').toLowerCase();
+  const cleanedStr2=str1.replace(/ /g,'').toLowerCase();
 
+  if (cleanedStr1.length != cleanedStr2.length){
+    return false;
+  }
+  const sortedStr1=cleanedStr1.split('').sort().join('');
+  const sortedStr2=cleanedStr2.split('').sort().join('');
+
+  if (sortedStr1 === sortedStr2){
+    return true;
 }
 
+// METHOD-2
+// we can use obj and store the frequeny of the char or we can use set ds 
+
+
+// Test the function
+console.log(isAnagram('listen', 'silent')); // true
+console.log(isAnagram('hello', 'world'));   // false
+console.log(isAnagram('Debit card', 'Bad credit')); // true
 module.exports = isAnagram;
