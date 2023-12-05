daa67d5dac3fa6cd0f0d980cef75845a3939eedf
ngworks1909 <76480955+ngworks1909@users.noreply.github.com>
2023-12-05 15:29:18 +0530

    first commit

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,19 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length === 0) {
+    return undefined; 
+  }
+
+  let largest = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largestElement) {
+      largest = numbers[i];
+    }
+  }
+
+  return largest;
 }
 
 module.exports = findLargestElement;
