4d2425b4bdb10820bbd850999cb387ccbbf94719
ngworks1909 <76480955+ngworks1909@users.noreply.github.com>
2023-12-05 14:55:58 +0530

    first commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,26 @@
 */
 
 function isAnagram(str1, str2) {
+  const map1 = new Map();
+  const map2 = new Map();
+        
+  if(a.length !== b.length){
+      return false;
+  }
+        
+  for (const char of a) {
+      map1.set(char, (map1.get(char) || 0) + 1);
+  }
+  for (const char of b) {
+      map2.set(char, (map2.get(char) || 0) + 1);
+  }
+        
+  for (const [key, value] of map1) {
+      if (map2.get(key) !== value) {
+          return false;
+      }
+  }
+  return true;
 
 }
 
