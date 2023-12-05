3bd6b24f66783b18d67243ce45e6c3b105891915
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2023-12-05 14:01:40 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
+    // Remove spaces and convert to lowercase for case-insensitive comparison
+    str1 = str1.replace(/\s/g, "").toLowerCase();
+    str2 = str2.replace(/\s/g, "").toLowerCase();
+  
+    // Check if the sorted characters of both strings are the same
+    return str1.split("").sort().join("") === str2.split("").sort().join("");
+  }
 
+const word1 = "spar";
+const word2 = "rasp";
+const result = isAnagram(word1, word2);
+
+if (result) {
+  console.log(`${word1} and ${word2} are anagrams.`);
+} else {
+  console.log(`${word1} and ${word2} are not anagrams.`);
 }
 
 module.exports = isAnagram;
