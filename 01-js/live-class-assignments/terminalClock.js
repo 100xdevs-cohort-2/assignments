96c963dd6e7d0368ae9f6fa2614b68c3e57571dd
Devananth <devananthnkl@gmail.com>
2023-12-05 11:45:14 +0530

    done:completed live class assignments

---
+++ 01-js/live-class-assignments/terminalClock.js
@@ -0,0 +1,21 @@
+/*
+  Create a terminal clock.
+  Note: Press ctrl+C to stop executing.
+*/
+
+let timerId;
+
+function activateClock() {
+  let timerId = setInterval(() => {
+    let date = new Date();
+    console.log(
+      `${date.getHours()} Hrs : ${date.getMinutes()} Mins : ${date.getSeconds()} secs`
+    );
+  }, 1000);
+}
+
+// document.addEventListener("keydown", () => {
+//   clearInterval(timerId);
+// });
+
+activateClock();
