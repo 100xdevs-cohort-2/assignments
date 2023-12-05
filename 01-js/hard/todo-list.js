664c94576674c4f5ca11598970b929c3aa825ace
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2023-12-05 14:05:11 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,17 +1,57 @@
-/*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
-
-  Once you've implemented the logic, test your code by running
-*/
-
 class Todo {
+    constructor() {
+        this.todos = [];
+    }
 
+    add(todo) {
+        this.todos.push(todo);
+    }
+
+    remove(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            this.todos.splice(indexOfTodo, 1);
+        } else {
+            throw new Error("Invalid index");
+        }
+    }
+
+    update(index, updatedTodo) {
+        if (index >= 0 && index < this.todos.length) {
+            this.todos[index] = updatedTodo;
+        } else {
+            throw new Error("Invalid index");
+        }
+    }
+
+    getAll() {
+        return this.todos;
+    }
+
+    get(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            return this.todos[indexOfTodo];
+        } else {
+            throw new Error("Invalid index");
+        }
+    }
+
+    clear() {
+        this.todos = [];
+    }
 }
 
-module.exports = Todo;
+// Example usage:
+const todoList = new Todo();
+
+todoList.add("Buy groceries");
+todoList.add("Read a book");
+console.log(todoList.getAll()); // Output: ["Buy groceries", "Read a book"]
+
+todoList.update(0, "Buy fruits");
+console.log(todoList.get(0)); // Output: "Buy fruits"
+
+todoList.remove(1);
+console.log(todoList.getAll()); // Output: ["Buy fruits"]
+
+todoList.clear();
+console.log(todoList.getAll()); // Output: []
