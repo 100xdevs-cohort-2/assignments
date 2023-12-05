d9532411114971026d39ce7b002eefd53b4acb53
CodeTorso <pruthvirajthinks@gmail.com>
2023-12-05 11:27:25 +0530

    assignment done

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -1,12 +1,22 @@
 /*
-  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Implement a function `countVowels` that takes a str as an argument and returns the number of vowels in the str.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
   Once you've implemented the logic, test your code by running
 */
 
 function countVowels(str) {
-    // Your code here
+  let counter = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  str = str.toLowerCase().replace(/\s/g, '')
+  vowels.forEach(vowel => {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === vowel){
+        counter += 1
+      }
+    }
+  });
+  return counter;
 }
 
 module.exports = countVowels;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,26 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+// My approach
+// I later got to know that you can also subtract new Date() objects creted one after another (https://github.com/NKJ14/Assigments/blob/main/01-js/medium/times.js)
+
 function calculateTime(n) {
-    return 0.01;
+    const beforeTime = new Date()
+    const beforeTimeinS = beforeTime.getMinutes()*60 
+                        + beforeTime.getSeconds() 
+                        + (beforeTime.getMilliseconds() / 1000)
+    sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i;
+    }
+    const afterTime = new Date()
+    const afterTimeinS = afterTime.getMinutes()*60 
+                        + afterTime.getSeconds() 
+                        + (afterTime.getMilliseconds() / 1000)
+    
+    const calcTime = afterTimeinS - beforeTimeinS
+
+    return [calcTime, afterTimeinS, beforeTimeinS];
 }
+
+console.log(calculateTime(1000000000))
