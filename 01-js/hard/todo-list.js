91cc74de5a9375ceb4f6b49a7e50f83663ddbb18
Rupani Harsh Vallabhbhai <Harsh.Rupani1@ibm.com>
2023-12-05 23:04:37 +0530

    added working code for assign - 1

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,35 @@
 */
 
 class Todo {
+  constructor() {
+    this.todoArr = [];
+  }
 
+  add(todo) {
+    this.todoArr.push(todo);
+  }
+
+  remove(index) {
+    if (this.todoArr.length < index) return "out of bound error";
+    this.todoArr.splice(index, 1);
+  }
+
+  update(index, todo) {
+    if (index >= this.todoArr.length) return "Invalid Task";
+    this.todoArr[index] = todo;
+  }
+
+  getAll() {
+    return this.todoArr;
+  }
+
+  get(index) {
+    return this.todoArr.length > index ? this.todoArr[index] : null;
+  }
+
+  clear() {
+    this.todoArr = [];
+  }
 }
 
 module.exports = Todo;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,11 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let start = performance.now();
+  let sum = 0;
+  for (let i = 0; i < n; i++) sum += i;
+  let end = performance.now();
+
+  let timeinSec = (end - start) / 1000;
+  return timeinSec;
 }
