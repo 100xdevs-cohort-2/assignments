fa6bb81cf3744ba2b564547931cef5d4b55413c4
Kuswanth Kumar <kuswanthkumarbokam16@gmail.com>
2023-12-05 14:57:48 +0530

    completed all tasks week 1.2

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,35 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length !== str2.length) return false;
 
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] in obj1) {
+      obj1[str1[i]] += 1;
+    } else {
+      obj1[str1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str2.length; i++) {
+    
+
+    if (str2[i] in obj2) {
+      obj2[str2[i]] += 1;
+    } else {
+      obj2[str2[i]] = 1;
+    }
+  }
+  for(let letter in obj1) {
+    if (obj1[letter] !== obj2[letter]) {return false;}
+  }
+  return true;
 }
+
+
 
 module.exports = isAnagram;
