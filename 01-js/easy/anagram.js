35ae02b0c0b76637877c2f6d4a4adf63c5243810
KATTASAHAN <kattasahan2234@gmail.com>
2023-12-06 01:18:09 +0530

    Finished Assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
 
+  [str1, str2] = [
+    [...str1.toLowerCase()].sort(),
+    [...str2.toLowerCase()].sort(),
+  ];
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) return false;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
