43347150bbc5e62652230cdf1212768cd5ff685a
shreeup <shree912@yahoo.com>
2023-12-04 20:11:58 -0800

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,145 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+
+class Calculator {
+  constructor() {
+    this.result = 0;
+    
+  }
+  add(num){
+    this.result += num;
+  }
+  subtract(num){
+    this.result -= num;
+  }
+  multiply(num){
+    this.result *= num;
+  }
+  divide(num){
+    if(num!=0){
+      this.result /= num;
+    }else{
+      throw new Error("Division by 0 not possible!!");
+    }
+  }
+  clear(){
+    this.result = 0;
+  }
+  getResult(){
+    return this.result;
+  }
+  calculate(str){
+      return evaluate(str)
+  }
+}
+
+
+
+function evaluate(expression)
+    {
+        let tokens = expression.split('');
+  
+  let values = [];
+  
+  let ops = [];
+  
+        for (let i = 0; i < tokens.length; i++)
+        {
+            if (tokens[i] == ' ')
+            {
+                continue;
+            }
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                let sbuf = "";
+                
+                 while (i < tokens.length &&
+                        tokens[i] >= '0' &&
+                            tokens[i] <= '9')
+                {
+                    sbuf = sbuf + tokens[i++];
+                }
+                values.push(parseInt(sbuf, 10));
+                
+                 i--;
+            }
+            else if (tokens[i] == '(')
+            {
+                ops.push(tokens[i]);
+            }
+            else if (tokens[i] == ')')
+            {
+                while (ops[ops.length - 1] != '(')
+                {
+                  values.push(applyOp(ops.pop(),
+                                   values.pop(),
+                                  values.pop()));
+                }
+                ops.pop();
+            }
+  
+   else if (tokens[i] == '+' ||
+                     tokens[i] == '-' ||
+                     tokens[i] == '*' ||
+                     tokens[i] == '/')
+            {
+               
+                while (ops.length > 0 &&
+                         hasPrecedence(tokens[i],
+                                     ops[ops.length - 1]))
+                {
+                  values.push(applyOp(ops.pop(),
+                                   values.pop(),
+                                 values.pop()));
+                }
+                ops.push(tokens[i]);
+            }
+        }
+        while (ops.length > 0)
+        {
+            values.push(applyOp(ops.pop(),
+                             values.pop(),
+                            values.pop()));
+        }
+        return values.pop();
+    }
+  
+    
+     function hasPrecedence(op1, op2)
+    {
+        if (op2 == '(' || op2 == ')')
+        {
+            return false;
+        }
+        if ((op1 == '*' || op1 == '/') &&
+               (op2 == '+' || op2 == '-'))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    function applyOp(op, b, a)
+    {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0)
+            {
+                document.write("Cannot divide by zero");
+            }
+            return parseInt(a / b, 10);
+        }
+        return 0;
+    }
 
 module.exports = Calculator;
