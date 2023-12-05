a8d1b1b7ca53572b92bceed24f98f3cff2834541
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-12-05 14:50:03 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,22 @@
 */
 
 function isPalindrome(str) {
+  const lowercasedStr = str.toLowerCase();
+
+  let left = 0;
+  let right = lowercasedStr.length - 1;
+
+  while (left < right) {
+    if (lowercasedStr[left] !== lowercasedStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
   return true;
 }
 
-module.exports = isPalindrome;
+
+console.log(isPalindrome("Nan"));  
+
