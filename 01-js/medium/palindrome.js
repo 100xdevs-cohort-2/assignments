9831b9dba8cd056d37f7297c0656fb9897567d67
ngworks1909 <76480955+ngworks1909@users.noreply.github.com>
2023-12-05 16:33:34 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,13 @@
 */
 
 function isPalindrome(str) {
-  return true;
+   str = str.toLowerCase();
+   for(let i = 0; i < Math.ceil(str.length/2); i++){
+      if(str[i] !== str[str.length-i-1]){
+         return false;
+      }
+   }
+   return true;
 }
 
 module.exports = isPalindrome;
