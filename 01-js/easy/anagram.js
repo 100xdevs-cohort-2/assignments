e2714dcca7e6d83c26cf885821d5dfeebb8a5214
chukkanagapraneeth <cnagapraneeth@gmail.com>
2023-12-05 18:49:42 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,36 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  } else if (str1.length == 0 && str2.length == 0) {
+    return true;
+  } else {
+    let set1 = new Set();
+    let set2 = new Set();
 
+    for (let i = 0; i < str1.length; i++) {
+      set1.add(str1.charAt(i).toLowerCase());
+      set2.add(str2.charAt(i).toLowerCase());
+    }
+
+    if (set1.size === set2.size) {
+      const arr1 = Array.from(set1);
+      const arr2 = Array.from(set2);
+
+      arr1.sort();
+      arr2.sort();
+
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = isAnagram;
