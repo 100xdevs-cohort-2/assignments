f648a23cacbb5fc36df8b5cd85ce5f726d096d3e
Ananadharajan <79890154+Ananadharajan@users.noreply.github.com>
2023-12-05 12:04:46 +0530

    Updatee expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,41 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory ( transactions ) {
+
+  // Empty object to store category-wise totals.
+  const categoryTotals = {};
+
+  /* Iterating over each transaction to destructure the object to extract 
+     category and price
+  */
+  transactions.forEach ( transactions => {
+    const { category, price } = transaction;
+
+    /* Check if the category in categoryTotals if yes add the price 
+       to existing total
+    */
+    if ( category in categoryTotals ) {
+      categoryTotals [ category ] += price;
+    } else {
+      categoryTotals [ category ] = price;
+    }
+  });
+
+    // Convert the categoryTotals object into an array of objects.
+    const result = Object.entries ( categoryTotals ) .map (([ category, total ]) => ({ [ category ]: total }));
+
+    return result;
 }
+
+const transaction = [
+  { itemName: "Item1", category: "Groceries", price: 20 },
+  { itemName: "Item2", category: "Medical-item", price: 70 }
+]
+
+const result = calculateTotalSpentByCategory ( transactions );
+
+console.log ( result );
+
 
 module.exports = calculateTotalSpentByCategory;
