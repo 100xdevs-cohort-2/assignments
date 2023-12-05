2e3cac65f99a9b43896117b3e6052080be9b5419
“Sanket <sanketsj00900@gmail.com>
2023-12-05 12:15:36 +0530

    anagram problem solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
-
+  function isAnagram(str1, str2) {
+    return (
+      str1.toLowerCase().split("").sort().join("") ===
+      str2.toLowerCase().split("").sort().join("")
+    );
+  }
 }
 
 module.exports = isAnagram;
