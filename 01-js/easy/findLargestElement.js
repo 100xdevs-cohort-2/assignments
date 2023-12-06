35447b4f0ce66e33a3fa1f1a47fe8d026ae09a25
Vinesh001 <vineshbaghel10@gmail.com>
2023-12-06 23:19:24 +0530

    findLargestElement Question has been done

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -7,6 +7,19 @@
 
 function findLargestElement(numbers) {
     
+    // declare a variable and assign first value of array or you assign the lowest value in number
+    let max = numbers[0];
+
+    // Run a loop of array length
+    for(let i = 0; i < numbers.length; i++){
+
+        // Now update max value if numbers[i] is greater than max
+        if(numbers[i]>max){
+            max = numbers[i];
+        }
+    }
+    // return the answer
+    return max;
 }
 
 module.exports = findLargestElement;
