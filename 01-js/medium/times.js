06025e46cf1fbff597c49bf16fc8c7353f22cfca
Ranjit Yadav <yadavranjit521@gmail.com>
2023-12-06 22:59:33 +0530

    fixed times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -10,15 +10,14 @@ There is no automated test for this one, this is more for you to understand time
 
 function calculateTime(n) {
     const time1 = Date.now();
-
+    // code starts here
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i;
     }
-    console.log(sum);
-
+    // code ends here
     const time2 = Date.now();
-    return time2 - time1;
+    return (time2 - time1) / 1000;
 }
 
-console.log(calculateTime(100))
+console.log(calculateTime(100000000000))
