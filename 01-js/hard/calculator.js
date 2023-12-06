9cf0dac86e056980427ccfdf35c605de72bf1224
yerva pavan <yervapavan16@gmail.com>
2023-12-06 13:23:36 +0530

    finished calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -15,7 +15,46 @@
 
   Once you've implemented the logic, test your code by running
 */
-
-class Calculator {}
+const math=require("mathjs");
+class Calculator {
+  constructor(){
+    this.result=0;
+  }
+  add(value){
+    this.result+=value;
+  }
+  subtract(value){
+    this.result-=value;
+  }
+  multiply(value){
+    this.result*=value;
+  }
+  divide(value){
+    if (value==0) 
+    throw new Error;
+    else{
+      this.result/=value;
+    }
+  }
+  clear(){
+    this.result=0;
+  }
+  getResult(){
+    return this.result;
+  }
+  calculate(expr){
+    expr=expr.replace(/\s/g,"");
+    if(/[a-zA-z@!$#]/g.test(expr)){
+      throw new Error;
+   }
+   else if(expr.includes("/0")){
+     throw new Error;
+   }
+   else{
+      return this.result=eval(expr);
+   }
+    
+  }
+}
 
 module.exports = Calculator;
