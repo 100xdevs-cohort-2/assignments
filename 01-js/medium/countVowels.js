3be4d0f301df20d4e49eb06059fb006fdf03d55a
Sreya Reddy <134381727+sreya22-git@users.noreply.github.com>
2023-12-07 01:30:31 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,16 @@
 
 function countVowels(str) {
     // Your code here
+    let s1=str.toLowerCase()
+    let vow="aeiou";
+    let count=0
+    let n=str.length;
+    for(let i=0;i<n;i++){
+      if(isVowel(s1[i])){
+        count+=1;
+      }
+    }
+    return count
 }
 
 module.exports = countVowels;
