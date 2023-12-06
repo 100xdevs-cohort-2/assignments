069dc92e7a77199128050a2f5b570d56fe385665
“Sanket <sanketsj00900@gmail.com>
2023-12-06 23:44:54 +0530

    JS class added

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,16 @@
+//JS Classes
+// 1. Create a class Animal with name and legCount properties in its constructor method
+// 2. Create a method describe in Animal class that returns a string like: "Animal name has legCount legs"
+// 3. Create a class Dog that extends Animal and has breed as its own property in constructor
+// 4. Create a method describe in Dog class that returns a string like: "Animal name has legCount legs and it is a breed dog"
+// 5. Create an object of Dog class and call describe method on it
 
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
-
-
