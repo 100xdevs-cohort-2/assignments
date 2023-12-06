3f28fe5e4f34317db596e2401b849fd2e0ca65ef
Shiladitya Mukerji <shiladitya.mukerji@gmail.com>
2023-12-06 13:59:23 +0530

    adding solution for expenditure-nalysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,39 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let result = [];
+
+  for (let i = 0; i < transactions.length; i++) {
+    let found = false;
+
+    // Check if the category already exists in the result array
+    for (let j = 0; j < result.length; j++) {
+      if (result[j].category === transactions[i].category) {
+        result[j].totalSpent += transactions[i].price; // Accumulate the total spent for the category
+        found = true;
+        break;
+      }
+    }
+
+    // If category doesn't exist, add it to the result array
+    if (!found) {
+      result.push({
+        category: transactions[i].category,
+        totalSpent: transactions[i].price // Set the total spent to the price for a new category
+      });
+    }
+  }
+
+  return result;
 }
+
+module.exports = calculateTotalSpentByCategory;
+
+// calculateTotalSpentByCategory(
+//   { itemName: 'Item 1', category: 'Grocery', price: 25, timestamp: Date.now() },
+//   { itemName: 'Item 2', category: 'Grocery', price: 10, timestamp: Date.now() }
+
+// );
+
 
 module.exports = calculateTotalSpentByCategory;
