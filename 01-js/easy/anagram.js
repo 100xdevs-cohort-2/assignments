6a32c6bfe1b7658997c431fa73cc815b4a484432
Buyyala Chandan Kumar <chandanbuyyala21@gmail.com>
2023-12-07 01:04:49 +0530

    Completed all assignment solutions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+    if (str1.length != str2.length) {
+      return false;
+    }
+    // str1 = str1.split(' ').join('').toLowerCase();
+    // str2 = str2.split(' ').join('').toLowerCase();
+    // for (let i = 0; i < str1.length; i++) {
+    //   if (!str2.includes(str1[i])) {
+    //     return false;
+    //   }
+    // }
+    // return true;
 
+    return str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
 }
 
 module.exports = isAnagram;
