a291899192756f3c62a11dea7b881855b31d228f
Vishruth <vishruthvs2003@gmail.com>
2023-12-07 22:14:43 +0530

    easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,34 @@
 */
 
 function isAnagram(str1, str2) {
+  // if (str1.length == str2.length) {
+  //   let i = 0,
+  //     j = 0;
+  //   for (i = 0; i < str1.length; i++) {
+  //     for (j = 0; j < str2.length; j++) {
+  //       if (str1[i] == str2[j]) {
+  //         return true;
+  //       } else return false;
+  //     }
+  //   }
+  // } else {
+  //   return false;
+  // }
+  // if ((str1 == str2) == null) {
+  //   return true;
+  // }
 
+  const cleanStr1 = str1.toLowerCase();
+  const cleanStr2 = str2.toLowerCase();
+
+  if (cleanStr1.length !== cleanStr2.length) {
+    return false;
+  }
+
+  const sortedStr1 = cleanStr1.split("").sort().join("");
+  const sortedStr2 = cleanStr2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
