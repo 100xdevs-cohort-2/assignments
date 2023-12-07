c15c0ea000a022cc65a1c246ad7e3c45d9d4e5c8
“Sanket <sanketsj00900@gmail.com>
2023-12-08 00:07:11 +0530

    Object Properties Added

--- week-1/offline-class-1/level-2/O5-Object.js
+++ week-1/offline-class-1/level-2/O5-Object.js
@@ -2,22 +2,25 @@
 function objectMethods(obj) {
   console.log("Original Object:", obj);
 
+  // Returns keys from object
   let keys = Object.keys(obj);
   console.log("After Object.keys():", keys);
 
+  // Returns values from object
   let values = Object.values(obj);
   console.log("After Object.values():", values);
 
+  // Returns keys and values from object
   let entries = Object.entries(obj);
   console.log("After Object.entries():", entries);
 
-  let hasProp = obj.hasOwnProperty("property");
+  // Returns a string representation of an object - array of key value pairs
+  let hasProp = obj.hasOwnProperty("key1");
   console.log("After hasOwnProperty():", hasProp);
 
+  // Adds new key value pair to object
   let newObj = Object.assign({}, obj, { newProperty: "newValue" });
   console.log("After Object.assign():", newObj);
-
-
 }
 
 // Example Usage for Object Methods
