be3675fccce186c14abc33d63e0d6ccf70ec079d
Lokeshwar Reddy Jonnalagadda <10lokeshnani@gmail.com>
2023-12-07 16:58:33 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -3,13 +3,13 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-      {
-				id: 1,
-				timestamp: 1656076800000,
-				price: 10,
-				category: 'Food',
-				itemName: 'Pizza',
-			}
+        {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
