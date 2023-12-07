4b4d0d0bc4ee867c2f012e900796713a0b5b9a64
XProtocol <tyagiaviral@gmail.com>
2023-12-07 23:21:03 +0530

    adds answer for hard folder

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,38 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  update(idx, todo) {
+    if(!isIndexValid(idx, this.todos.length)) return;
+    this.todos[idx] = todo;
+  }
+
+  get(idx) {
+    if(!isIndexValid(idx, this.todos.length)) return null;
+    return this.todos[idx];
+  }
+
+  getAll() { return this.todos; }
+
+  remove(idx) {
+    if(!isIndexValid(idx, this.todos.length)) return;
+    this.todos.splice(idx, 1);
+  }
+
+  clear() {
+    this.todos = [];
+  }
+}
+
+function isIndexValid(idx, len) {
+  return !(idx < 0 || idx >= len);
 }
 
 module.exports = Todo;
