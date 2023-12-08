834ac89d215398285b59d24d3ee35057ca1395ca
Taha <tiftikhar@student.iul.ac.in>
2023-12-08 06:36:29 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,17 @@
 */
 
 function findLargestElement(numbers) {
-    
+    if (numbers.length === 0) {
+        return;
+    }
+    let max = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < numbers.length; i++) {
+        // if (numbers[i] > max) {
+        //     max = numbers[i];
+        // }
+        max = Math.max(max, numbers[i]);
+    }
+    return max;
 }
 
 module.exports = findLargestElement;
