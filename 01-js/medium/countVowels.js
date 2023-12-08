b8f71d2ba289e7d787b7ae37682c076a1888336c
HusainModiwala <hussainmodiwala@gmail.com>
2023-12-08 18:42:25 +0530

    completed assignment-1

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,15 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  str = str.toLowerCase();
+  let count = 0;
+  for(let char of str){
+    if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
