661c0a0e80d29a9d08e61152de22dcba00191626
Vittrojan <vittrojan@gmail.com>
2023-12-08 16:22:41 +0530

    Week 1 Assignments completion

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,32 @@
 */
 
 function isAnagram(str1, str2) {
+  var map1 = {};
+  var map2 = {};
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+	
+    if (!(str1[i] in map1)) {
+      map1[str1[i]] = 0;
+    }
+    if (!(str2[i] in map2)) {
+      map2[str2[i]] = 0;
+    }
+    map1[str1[i]]++;
+    map2[str2[i]]++;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if(map1[str1[i]] != map2[str1[i]]){
+      return false;
+    }
+  }
+  return true
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,9 +4,25 @@
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
-
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  let catMap = {}
+  let retVal = []
+  if(transactions.length==0){
+    return [];
+  }
 
+  for (i=0;i<transactions.length;i++){
+    if(!(transactions[i].category in catMap)){
+      catMap[transactions[i].category] = 0;
+    }
+    catMap[transactions[i].category] += transactions[i].price;
+  }
+  
+  // console.log(catMap)
+  for (key in catMap){
+    retVal.push({'category':key, 'totalSpent': catMap[key]})
+  }
+  console.log(retVal)
+  return (retVal)
+}
 module.exports = calculateTotalSpentByCategory;
