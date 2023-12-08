f9ebd17dd7cd46bd029bb4cda30e72f87b5828d9
Taha <tiftikhar@student.iul.ac.in>
2023-12-08 06:39:29 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,14 @@
 
 function countVowels(str) {
     // Your code here
+  str = str.toLowerCase();
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+          count++;
+        }
+    }
+    return count;
 }
 
 module.exports = countVowels;
