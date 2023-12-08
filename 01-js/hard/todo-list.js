faefca695ba4d359792e858d550c9313cf92166c
Lokesh Mishra <flutterydev@gmail.com>
2023-12-09 04:17:21 +0530

    todo-list done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,44 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo){
+    if (indexOfTodo>=this.todos.length) {
+      return;
+    }
+    this.todos[indexOfTodo]= this.todos[this.todos.length-1]
+    this.todos.pop();
+  }
+
+  update(index, updatedTodo){
+
+    if (index>=this.todos.length) {
+      return;
+    }
+    this.todos[index]= updatedTodo;
+  } 
+  
+  getAll(){
+    return this.todos;
+  }
+
+  get(index){
+    if (index>=this.todos.length) {
+      return null;
+    }
+    return this.todos[index];
+  }
+
+  clear(){
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
