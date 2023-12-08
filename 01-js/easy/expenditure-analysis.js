2fa5bd05eff587e18d43b8f52ea502c8e2ab01a4
Ameer jafar <ameerjafar123@gmail.com>
2023-12-08 15:18:19 +0530

    add into my github

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -29,28 +29,5 @@ function calculateTotalSpentByCategory(transactions) {
  }
   return list;
 }
-const transactions = [
-  {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: 'Food',
-    itemName: 'Pizza',
-  },
-  {
-    id: 2,
-    timestamp: 1656105600000,
-    price: 20,
-    category: 'Food',
-    itemName: 'Burger',
-  },
-  {
-    id: 3,
-    timestamp: 1656134400000,
-    price: 30,
-    category: 'Food',
-    itemName: 'Sushi',
-  },
-];
 console.log(calculateTotalSpentByCategory(transactions))
 module.exports = calculateTotalSpentByCategory;
