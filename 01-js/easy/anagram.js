22d93e3f131c9e346dcca9573be29b0ef4ab6415
Devesh Kumar Patel <69602548+dev3z@users.noreply.github.com>
2023-12-09 02:14:54 +0530

    Update anagram.js
    solves easy 1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,31 @@
 */
 
 function isAnagram(str1, str2) {
+ 
+  let str3= str1.toLowerCase();
+  let str4= str2.toLowerCase();
 
+  let charArr1= str3.split('');
+  let charArr2= str4.split('');
+
+  if(charArr1.length != charArr2.length){
+    return false;
+  }
+else{
+
+ 
+  charArr1.sort();
+  charArr2.sort();
+
+  for(let i=0; i<charArr1.length ; i++){
+    if(
+      charArr1[i] != charArr2[i]){
+        return false;
+      }
+
+  }
+}
+return true;
 }
 
 module.exports = isAnagram;
