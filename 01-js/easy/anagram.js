a15a4bc07305f955f890f44c72570eba1ef88d53
Shaan Kashyap <150347784+drkryndr@users.noreply.github.com>
2023-12-08 20:03:41 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,46 @@
 */
 
 function isAnagram(str1, str2) {
+  
+  // steps
+  // check if the length of the strings are same
+  // store the counts of the chars of the respective strings in respective objects
+  // iterate through the object's keys and check if all chars have same count
+
+  // time complexity - O(n)
+  // space complexity - O(n)
+
+  if(str1.length !== str2.length)
+    return false;
+
+  let str1Count = {};
+  let str2Count = {}; 
+
+  for(let char of str1.toLowerCase())
+  {
+    str1Count[char] = (str1Count[char] || 0) + 1;
+  }
+
+  for(let char of str2.toLowerCase())
+  {
+    str2Count[char] = (str2Count[char] || 0) + 1;
+  }
+
+  console.log(str1Count)
+  console.log(str2Count)
+
+  for(let char of Object.keys(str1Count))
+  {
+    if (str1Count[char] !== str2Count[char])
+      return false;
+  }
+
+  return true;
 
 }
+
+console.log(isAnagram('Debit Card', 'Bad Credit'));
+
+
 
 module.exports = isAnagram;
