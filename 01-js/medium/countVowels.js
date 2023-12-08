63071d0a41afbd2e9a4a12438c1740f006007284
Shaan Kashyap <150347784+drkryndr@users.noreply.github.com>
2023-12-08 20:11:12 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,24 @@
 */
 
 function countVowels(str) {
-    // Your code here
-}
+    
+  // steps
+  // create a set of vowels
+  // iterate through the string to store the vowel count in var count
+  // return count
 
+  // time complexity - O(n)
+  // space complexity - O(1) (as we are storing 5 vowels we can consider it to be constant)
+
+    let count = 0;
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+
+    for(let char of str.toLowerCase())
+    {
+      if(vowels.has(char))
+        count++;
+    }
+
+    return count;
+}
 module.exports = countVowels;
