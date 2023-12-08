e48368ac4eb9b12abc13a9b7eea49bb55e434efd
Lokesh Mishra <flutterydev@gmail.com>
2023-12-08 21:07:39 +0530

    comments added

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,15 +1,13 @@
-//Algorithm for the below code
-
-//1. convert string to lowercase
-//2. split string into array
-//3. sort array
-//4. join array back into string
-//5. compare strings
-//6. return true or false
-
-
+// This function checks if two provided strings are anagrams of each other
 function isAnagram(str1, str2) {
+  // Convert both strings to lowercase to ensure the comparison is case-insensitive
+  // Split each string into an array of characters
+  // Sort the array of characters
+  // Join the sorted array back into a string
+  // The above steps are done for both strings
+  // If the resulting strings are equal, then the original strings are anagrams
   return str1.toLowerCase().split('').sort().join() == str2.toLowerCase().split('').sort().join();
 }
 
+// Export the function for use in other modules
 module.exports = isAnagram;
