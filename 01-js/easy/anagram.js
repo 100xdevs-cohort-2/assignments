37fbcb4732f0dbf0b1838d96ac4a9c55f834fe78
Lokesh Mishra <flutterydev@gmail.com>
2023-12-08 20:20:35 +0530

    anagram completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,15 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
+//Algorithm for the below code
+
+//1. convert string to lowercase
+//2. split string into array
+//3. sort array
+//4. join array back into string
+//5. compare strings
+//6. return true or false
+
 
 function isAnagram(str1, str2) {
-
+  return str1.toLowerCase().split('').sort().join() == str2.toLowerCase().split('').sort().join();
 }
 
 module.exports = isAnagram;
