b8ba22d506e04cc8d57955133937eaaa4ace31f6
Lokesh Mishra <flutterydev@gmail.com>
2023-12-08 21:07:39 +0530

    comments added

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,17 +1,3 @@
-/*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  transactions is an array where each
-  Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
-  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
-*/
 
 function calculateTotalSpentByCategory(transactions) {
 
@@ -22,29 +8,32 @@ function calculateTotalSpentByCategory(transactions) {
   let categories = [];
   let totalSpent = [];
   let result = [];
-  
-  //iterate through the transactions array and push the unique categories into the categories array
-  
-  transactions.forEach(transaction =>{
-    if (!categories.includes(transaction.category)) {
-      categories.push(transaction.category);
+  // Iterate through the transactions array
+transactions.forEach(transaction => {
+  // If the current transaction's category is not already in the categories array
+  if (!categories.includes(transaction.category)) {
+    // Add the category to the categories array
+    categories.push(transaction.category);
+  }
+});
+
+// Iterate through the categories array
+categories.forEach(category => {
+  // Initialize a variable to keep track of the total spent in the current category
+  let spent = 0;
+
+  // Iterate through the transactions array again
+  transactions.forEach(transaction => {
+    // If the current transaction's category matches the current category
+    if (transaction.category == category) {
+      // Add the transaction's price to the total spent in the current category
+      spent += transaction.price;
     }
   });
 
-  //iterate through the categories array and calculate the total spent for each category
-  //push the category and total spent into the result array
-
-  categories.forEach( category => {
-    let spent = 0;
-     transactions.forEach( transaction => {
-      if(transaction.category == category){
-        spent+= transaction.price;
-      }
-     })
-     result.push({category: category, totalSpent: spent});
+  // Add an object to the result array that contains the current category and the total spent in that category
+  result.push({ category: category, totalSpent: spent });
   });
-
-
   return result;
 }
 
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -1,12 +1,27 @@
 /*
-  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
-  Example:
-  - Input: [3, 7, 2, 9, 1]
-  - Output: 9
+  This function `findLargestElement` is designed to find the largest number in an array.
+  It takes an array of numbers as an argument.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
 */
 
 function findLargestElement(numbers) {
-    
+    // Initialize the variable `max` with the first element of the array
+    let max = numbers[0];
+
+    // Iterate over each number in the array
+    numbers.forEach(number => {
+        // If the current number is greater than the current max
+        if(number > max){
+            // Update max with the current number
+            max = number;
+        }
+    });
+
+    // After checking all numbers, return the max number
+    return max;
 }
 
+// Export the function for use in other modules
 module.exports = findLargestElement;
