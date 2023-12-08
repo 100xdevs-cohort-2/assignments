8033120f0ef44ec9260068f03aabad63613cb431
Shaan Kashyap <150347784+drkryndr@users.noreply.github.com>
2023-12-08 20:09:51 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,63 @@
 
 class Todo {
 
+  constructor() {
+    this.todos = [];
+  }
+
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  remove(index)
+  {
+    if(index >= this.todos.length)
+      return;
+    this.todos.splice(index, 1);
+  }
+
+  update(index, todo){
+    if(index >= this.todos.length)
+      return;
+    
+    for(let i=0; i<index; i++)
+      this.todos[index] = todo;
+  }
+
+  getAll()
+  {
+    return this.todos;
+  }
+
+  get(index)
+  {
+    if(index >= this.todos.length)
+      return null;
+    return this.todos[index];
+  }
+
+  clear()
+  {
+    this.todos = [];
+  }
 }
+
+let todoList = new Todo();
+// uncomment for manual testing
+// todoList.add('Task 1');
+// todoList.add('Task 2');
+// todoList.add('Task 3');
+
+// todoList.update(1, 'Updated Task 2');
+// console.log(todoList.get(1));
+
+// todoList.update(3, 'Invalid Task');
+// console.log(todoList.getAll());
+
+// todoList.remove(2);
+// console.log(todoList.getAll());
+
+
+
 
 module.exports = Todo;
