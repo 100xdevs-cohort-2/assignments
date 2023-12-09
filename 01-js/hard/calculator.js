08241d30ed7c8663bbc007a04859a8511333d107
Arsh <arshhasan68@gmail.com>
2023-12-09 19:55:09 +0930

    hard problems solved

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,54 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+constructor() {
+  this.result = 0;
+}
+
+add(number) {
+  this.result += number;
+}
+
+subtract(number) {
+  this.result -= number;
+}
+
+multiply(number) {
+  this.result *= number;
+}
+
+divide(number) {
+  if (number === 0) {
+      throw new Error('Division by zero');
+  }
+  this.result /= number;
+}
+
+clear() {
+  this.result = 0;
+}
+
+getResult() {
+  return this.result;
+}
+
+calculate(expression) {
+  // Remove spaces from the expression
+  expression = expression.replace(/\s+/g, '');
+
+  // Validate expression (basic check for invalid characters)
+  if (!/^[\d+\-*/().]+$/.test(expression)) {
+      throw new Error('Invalid expression');
+  }
+
+  // Evaluate the expression
+  try {
+      this.result = eval(expression);
+  } catch (e) {
+      throw new Error('Error in evaluating the expression');
+    }
+}
+}
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,17 +1,31 @@
-/*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
-
-  Once you've implemented the logic, test your code by running
-*/
-
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1);
+  }
+
+  update(index, updatedTodo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    return this.todos[indexOfTodo];
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
-module.exports = Todo;
+export default Calculator;
