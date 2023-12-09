e458a81b1d0366d04cbc978b794c8b2392a621c1
rohit-singh-bisht <thersbisht@gmail.com>
2023-12-09 22:45:47 +0530

    calculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,48 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    // Initialise a result variable in constructor
+    this.result = 0; // we need to update this after every operation;
+  }
+  add(number) {
+    this.result = this.result + number;
+  }
+  subtract(number) {
+    this.result = this.result - number;
+  }
+  multiply(number) {
+    this.result = this.result * number;
+  }
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Number is not divisble by 0");
+    }
+    this.result = this.result / number;
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(expression) {
+    const removeExtraSpaces = expression.replace(/\s+/g, "");
+    const isNonNumericalExpression = /[a-z]/gi.test(removeExtraSpaces);
+    const isNonExecutableExpression = !/^[0-9+\-*/().]/.test(removeExtraSpaces);
+
+    if (isNonNumericalExpression || isNonExecutableExpression) {
+      throw new Error("invalid expression");
+    } else {
+      const newResult = eval(removeExtraSpaces);
+      if (newResult === Infinity) {
+        throw new Error("Number is not divisble by 0");
+      } else {
+        this.result = newResult;
+      }
+    }
+  }
+}
 
 module.exports = Calculator;
