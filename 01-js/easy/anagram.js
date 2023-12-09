82a152de39263b349d68b782f5bcf3b6402ba69c
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-12-09 13:46:21 +0530

    feat: anagram solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,52 @@
 
 function isAnagram(str1, str2) {
 
+// make sure the length are equal 
+  if (str1.length !== str2.length) {
+    return false
+}
+// case check 
+let s1 = str1.toLowerCase()
+let s2 = str2.toLowerCase()
+
+// naive approach 
+// Time complexity - O(nlogn)
+// Space complexity - O(n)
+// // sort and compare 
+// const sSort = s1.split("").sort().join("")
+// const tSort = s2.split("").sort().join("")
+
+// return sSort === tSort
+
+// optimize approach - 
+// Time Complexity is O(n)
+// Space Complexity is O(n)
+
+
+let charFreq = new Map() 
+// loop and store the char freq 
+for (let i = 0; i < s1.length; i++){
+  if(!str1[i]){
+    charFreq[s1[i]] = 1
+  } else {
+    charFreq[s1[i]]++ 
+  }
+}
+
+
+// check freq of char are same 
+for(let i = 0; i < s2.length; i++){
+  // if char is undefined
+  if(charFreq[s2[i]] === undefined) {
+    return false
+  }
+  // if char freq is less than 1
+  if(charFreq[s2[i]] < 1){
+    return false
+  }
+  charFreq[s2[i]]--
+}
+return true
 }
 
 module.exports = isAnagram;
