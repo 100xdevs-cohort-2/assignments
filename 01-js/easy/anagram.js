a8065c038865489c9d6a48a99d4da9532bf2d481
Anand <anand22121996@gmail.com>
2023-12-09 23:10:06 +0530

    Week1 Assignments completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,10 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
-
+function isAnagram (str1, str2) {
+  str1 = str1.toLowerCase().split('').sort()
+  str2 = str2.toLowerCase().split('').sort()
+  return JSON.stringify(str1) == JSON.stringify(str2)
 }
 
-module.exports = isAnagram;
+module.exports = isAnagram
