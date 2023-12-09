e30dc3d5c74a7d6a9731dc6a1c6bb007de077cd8
MethuParoi <paroimethu@gmail.com>
2023-12-09 08:29:27 +0530

    initial commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
+  //filtering only alphabets
+  const clean_str = (str) => str.toLowerCase().replace(/[^a-z]/g, "");
+  //filtering special charecter
+  const clean_str_sc = (str_sc) => str_sc.toLowerCase().replace(/[a-z]/g, "");
 
+  let sc1 = clean_str_sc(str1);
+  let sc2 = clean_str_sc(str2);
+
+  let clean_str1 = clean_str(str1);
+  let clean_str2 = clean_str(str2);
+
+  const a = clean_str1.split("").sort().join("");
+  const b = clean_str2.split("").sort().join("");
+
+  if (sc1 != sc2) {
+    return false;
+  } else {
+    return (
+      clean_str1.split("").sort().join("") ===
+      clean_str2.split("").sort().join("")
+    );
+  }
 }
+
 
 module.exports = isAnagram;
--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,23 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const clean_str = str.toLowerCase().replace(/[^a-z]/g, "");
+  const str_arr = clean_str.split("");
+  let vowel = 0;
+
+  for (let i = 0; i < str_arr.length; i++) {
+    if (
+      str_arr[i] === "a" ||
+      str_arr[i] === "e" ||
+      str_arr[i] === "i" ||
+      str_arr[i] === "o" ||
+      str_arr[i] === "u"
+    ) {
+      vowel++;
+    }
+  }
+
+  return vowel;
 }
 
 module.exports = countVowels;
