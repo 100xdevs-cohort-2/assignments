968e4371ebd4392b04722dbc51bdb7eb051852e8
arka8038 <ghosh.arka8038@gmail.com>
2023-12-09 13:14:43 +0530

    Hard assignment problems solutions

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,134 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  add(value) {
+    this.result += value;
+  }
+
+  subtract(value) {
+    this.result -= value;
+  }
+
+  multiply(value) {
+    this.result *= value;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    this.result /= value;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  calculate(expression) {
+    const cleanExp = expression.replace(/\s/g, '');
+
+    if (!/^[0-9+\-*/().]+$/.test(cleanExp)) {
+      throw new Error('Invalid expression');
+    }
+
+    const operatorStack = [];
+    const operandStack = [];
+
+    for (let i = 0; i < cleanExp.length; i++) {
+      const char = cleanExp[i];
+
+      if (char === '(') {
+        operatorStack.push(char);
+      } else if (char === ')') {
+        if (!operatorStack.includes('(')) {
+          throw new Error('Invalid parentheses');
+        }
+        while (operatorStack.length !== 0 && operatorStack[operatorStack.length - 1] !== '(') {
+          this.performOperation(operandStack, operatorStack.pop());
+        }
+        if (operatorStack.pop() !== '(') {
+          throw new Error('Invalid parentheses');
+        }
+      } else if ('+-*/'.includes(char)) {
+        while (operatorStack.length !== 0 && this.operatorPrecedence(char) <= this.operatorPrecedence(operatorStack[operatorStack.length - 1])) {
+          this.performOperation(operandStack, operatorStack.pop());
+        }
+        operatorStack.push(char);
+      } else if (this.isNumber(char)) {
+        let num = parseInt(char);
+
+        while (i + 1 < cleanExp.length && this.isNumber(cleanExp[i + 1])) {
+          num = num * 10 + parseInt(cleanExp[++i]);
+        }
+
+        if (cleanExp[i + 1] === '.') {
+          let dec = '.';
+          let j = i + 2;
+
+          while (j < cleanExp.length && this.isNumber(cleanExp[j])) {
+            dec += cleanExp[j];
+            i = j;
+            j++;
+          }
+
+          num += parseFloat(dec);
+        }
+        operandStack.push(num);
+      }
+    }
+
+    if (operatorStack.includes('(') || operatorStack.includes(')')) {
+      throw new Error('Invalid parentheses');
+    }
+
+    while (operatorStack.length !== 0) {
+      this.performOperation(operandStack, operatorStack.pop());
+    }
+
+    this.result = operandStack.pop();
+  }
+
+  isNumber(char) {
+    return /^[0-9]+$/.test(char);
+  }
+
+  operatorPrecedence(opt) {
+    if (opt === '+' || opt === '-') return 1;
+    if (opt === '*' || opt === '/') return 2;
+    return 0;
+  }
+
+  performOperation(operandStack, operator) {
+    const operand2 = operandStack.pop();
+    const operand1 = operandStack.pop();
+
+    switch (operator) {
+      case '+':
+        operandStack.push(operand1 + operand2);
+        break;
+      case '-':
+        operandStack.push(operand1 - operand2);
+        break;
+      case '*':
+        operandStack.push(operand1 * operand2);
+        break;
+      case '/':
+        if (operand2 === 0) {
+          throw new Error('Cannot divide by zero');
+        }
+        operandStack.push(operand1 / operand2);
+        break;
+    }
+  }
+}
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,40 @@
 */
 
 class Todo {
+  constructor() {
+    this.todoList = []
+  }
 
+  add(todo) {
+    this.todoList.push(todo)
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo < this.todoList.length) {
+      this.todoList.splice(indexOfTodo, 1)
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (this.todoList[index]) {
+      this.todoList[index] = updatedTodo
+    }
+  }
+
+  getAll() {
+    return this.todoList
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todoList.length) {
+      return this.todoList[indexOfTodo];
+    }
+    return null;
+  }
+
+  clear() {
+    this.todoList.splice(0, this.todoList.length)
+  }
 }
 
 module.exports = Todo;
