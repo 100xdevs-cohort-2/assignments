9c67fb2fdcc07e61d4ad0544153cadddd5e17fe8
Deep Patel <deeppatel7521@gmail.com>
2023-12-09 18:19:27 +0530

    isAnagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (sort(str1) == sort(str2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+function sort(a) {
+  a = a.toLowerCase();
+  var arr = a.split("");
+  arr.sort();
+  return arr.join("");
+}
+
+console.log(isAnagram("hello world", "hello qorld"));
 
 module.exports = isAnagram;
