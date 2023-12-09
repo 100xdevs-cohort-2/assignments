450b0a08d0ed0b7575a435f07419504c8fa73b63
HAXAZE <28skofficial01@gmail.com>
2023-12-09 21:13:11 +0530

    count vowels done(medium)

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,11 @@
 
 function countVowels(str) {
     // Your code here
+    let cnt = 0;
+    for(let i = 0;i<str.length;i++){
+      if(str[i]=='A' || str[i]=='a' ||str[i]=='e'  ||str[i]=='E'  ||str[i]=='i' ||str[i]=='I' ||str[i]=='o' ||str[i]=='O' ||str[i]=='u' ||str[i]=='U') cnt++;
+    }
+    return cnt;
 }
 
 module.exports = countVowels;
