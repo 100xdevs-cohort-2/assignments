a6a3234db979d33689598351ce980d4ca62e2f7d
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-10 16:59:57 +0530

    added sleep sloution

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,16 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  const targetDate = new Date().setSeconds(new Date().getSeconds() + seconds);
+  let time = new Date();
+  let timer = targetDate - time.getTime();
+  while (timer > 0) {
+    time = new Date();
+    timer = targetDate - time.getTime();
+  }
 }
+
+console.log("before timer");
+sleep(5);
+console.log("after timer");
