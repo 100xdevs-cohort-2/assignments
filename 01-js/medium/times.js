e68d6a9029ccd6f8c1d6525fa1eaa4ca64d66cb1
Krish Murarka <murarka.krish@gmail.com>
2023-12-10 16:15:11 +0530

    Added Solution times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,12 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+
+    let oldDate = new Date();
+    let answer =0;
+    for(let i=0;i<n;i++){
+        answer += i;
+    }
+    let newDate = new Date();
+    return (newDate.getSeconds() - oldDate.getSeconds());
 }
