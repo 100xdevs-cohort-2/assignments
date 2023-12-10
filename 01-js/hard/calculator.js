d738da4dca211c172e81770a72748c469c9a9fd2
Krish Murarka <murarka.krish@gmail.com>
2023-12-10 16:44:10 +0530

    Added Solution todo.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -8,7 +8,7 @@
     - clear: makes the `result` variable to 0
     - getResult: returns the value of `result` variable
     - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+      example input: `10 -   2 *    (   6 - (4 + 1) / 2) + 7`
       Points to Note: 
         1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
@@ -16,6 +16,45 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+
+  constructor() {
+    this.result = 0;
+  }
+  add(num) {
+    this.result += num;
+  }
+  subtract(num) {
+    this.result -= num;
+  }
+  multiply(num) {
+    this.result *= num;
+  }
+  divide(num) {
+    this.result /= num;
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(expression) {
+    let number =0;
+    this.clear();
+    for (let i = 0; i < expression.length; i++){
+      if(expression[i] == ' ')continue;
+      if(expression[i]>='0' && expression[i]<='9'){
+        number = number*10 + parseInt(expression[i]);
+      }
+      if(expression[i]=='-'){
+
+      }
+
+    }
+    return result;
+  }
+
+}
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,39 @@
 
 class Todo {
 
+  constructor() {
+    this.todos = [];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+    // Use splice to remove the todo at the specified index
+      this.todos.splice(indexOfTodo, 1);
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length)
+    this.todos[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if(indexOfTodo >= 0 && indexOfTodo < this.todos.length)
+      return this.todos[indexOfTodo];
+    return null;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
