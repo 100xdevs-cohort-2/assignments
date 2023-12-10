f1978ce5e894cd365fdfe20c6fdc62de37f49b7e
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-10 16:58:00 +0530

    small fix

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -1,7 +1,7 @@
 /*
  * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
  * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
- * Return a promise.all which return the time it takes to complete the entire operation.
+ * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
 function wait1(t) {
--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -1,7 +1,7 @@
 /*
  * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
  * Write a function that sequentially calls all 3 of these functions in order.
- * Return a promise chain which return the time it takes to complete the entire operation.
+ * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
 
