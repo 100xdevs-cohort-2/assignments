b3dcb7a58808e936cbd1d498950e559e2b716c24
shubham bajaj <shmbajaj32@gmail.com>
2023-12-10 14:39:18 +0530

    palindrome done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,44 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+/**
+ *  /[a-zA-z]/ig
+ * Could have used above regex but avoided
+ */
+
+function parseStr(str) {
+  let result = '';
+  for (let index in str) {
+    const char = str[index];
+    const charCode = str.charCodeAt(index);
+
+    if (charCode >= 65 && charCode <= 90) {
+      result += String.fromCharCode(charCode + 32);
+    }
+
+    if (charCode >= 97 && charCode <= 122) {
+      result += char;
+    }
+  }
+  return result;
+}
+
 function isPalindrome(str) {
+  const parsedStr = parseStr(str);
+
+  let leftIndex = 0;
+  let rightIndex = parsedStr.length - 1;
+
+  for (
+    ;
+    leftIndex < parsedStr.length && leftIndex !== rightIndex;
+    ++leftIndex, --rightIndex
+  ) {
+    const leftCharacter = parsedStr[leftIndex];
+    const rightCharacter = parsedStr[rightIndex];
+    if (leftCharacter !== rightCharacter) return false;
+  }
+
   return true;
 }
 
