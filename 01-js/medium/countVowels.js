027b3f8af48c0648313b58957fb4c3c7af712153
tanishq <tanishqarya24@gmail.com>
2023-12-10 14:43:03 +0530

    01-js easy, medium assignment solved

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  let count = 0;
+  for(let ch of str) {
+    if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+      count += 1;
+    }
+  } 
+  return count;
 }
 
 module.exports = countVowels;
--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,29 @@
 */
 
 function isPalindrome(str) {
+  str = str.replace(/[\?\.,! ]/g,"").toLowerCase();
+  let start = 0
+  // console.log(str);
+  let end = str.length-1;
+  while(start < end) {
+    // if(!str[start].match(/[a-zA-Z]/)) {
+      //   start++;
+    // }
+    // if(!str[end].match(/[a-zA-Z]/)) {
+    //   end--;
+    // }
+    
+    if(str[start] != str[end]) {
+      // console.log(start +","+ end +","+ str[start] +","+ str[end])
+      return false;
+    }
+
+    start++;
+    end--;
+  }
   return true;
 }
-
 module.exports = isPalindrome;
+
+// console.log(isPalindrome("race car")); // Remove Punctuation marks
+// console.log(isPalindrome("Eva, can I see bees in a cave?")); // 
