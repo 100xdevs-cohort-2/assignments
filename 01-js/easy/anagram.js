57d589c6379bc9ee14cf6c270b5c4f6de933e32e
Krish Murarka <murarka.krish@gmail.com>
2023-12-10 15:48:21 +0530

    Added Solution Anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,26 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let mapping = new Map();
+  for(let i =0;i<str1.length;i++){
+    if (!mapping.has(str1[i])) {
+      mapping.set(str1[i], 0);
+    }
+    mapping.set(str1[i], mapping.get(str1[i]) + 1);
+  }
+  for(let i=0;i<str2.length;i++){
+    if(!mapping.has(str2[i])){
+      mapping.set(str2[i], 0);
+    }
+    mapping.set(str2[i], mapping.get(str2[i]) - 1);
+  }
+  let answer = true;
+  mapping.forEach((values,keys)=>{
+    console.log(values + " " + keys)
+    if(values != 0)answer = false;
+  });
+  return [...mapping.values()].every(value => value === 0);
 }
-
 module.exports = isAnagram;
