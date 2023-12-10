3be592f720fd606a8c4d8aceb5c7e26211bd8de3
GREEDnim <sivadhas007@gmail.com>
2023-12-10 23:17:41 +0530

    fix(w2) : new approach for 2 in easy

--- week-2/01-async-js/easy/2.js
+++ week-2/01-async-js/easy/2.js
@@ -1,13 +1,36 @@
+// ## Counter without setInterval
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+
 function printTime(){
     date=new Date();
     curTimeString=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     console.log(curTimeString)
 }
-function setInterval(callback,duration){
+
+// v1
+
+function customSetInterval1(callback,duration){
     setTimeout(()=>{
         callback();
-        setInterval(callback,duration);
+        customSetInterval1(callback,duration);
     },duration)
 
 }
-setInterval(printTime,1000);
+customSetInterval1(printTime,1000);
+
+// v2
+
+function sleep(duration){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>res(),duration)
+    })
+}
+
+async function customSetInterval2(callback,duration){
+    while(true){
+        await sleep(duration);
+        callback();
+    }
+}
+customSetInterval2(printTime,1000);
--- week-2/01-async-js/easy/4.js
+++ week-2/01-async-js/easy/4.js
@@ -1,6 +1,10 @@
+// ## Write to a file
+// Using the fs library again, try to write to the contents of a file.
+// You can use the fs library to as a black box, the goal is to understand async tasks.
+
 const fs=require('fs')
 
-fs.readFile('4-write-to-file.md','utf-8',(err,data)=>{
+fs.readFile('4.js','utf-8',(err,data)=>{
     // data="aaaa"
     fs.writeFile('4-write-to-file.md',data,(err)=>{
         console.log(err);
