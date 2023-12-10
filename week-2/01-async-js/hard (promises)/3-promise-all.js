e00ea7f7ea6b096ede888f8d3bdcecd08126c614
Shaan Kashyap <150347784+drkryndr@users.noreply.github.com>
2023-12-10 18:23:41 +0530

    solved 3-promise-all.js

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,52 @@
 
 
 function waitOneSecond() {
-
-}
-
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('Resolved after 1 second');
+      }, 1000);
+    });
+  }
+  
+// Function that returns a promise resolving after 2 seconds
 function waitTwoSecond() {
-
+return new Promise(resolve => {
+    setTimeout(() => {
+    resolve('Resolved after 2 seconds');
+    }, 2000);
+});
 }
 
+// Function that returns a promise resolving after 3 seconds
 function waitThreeSecond() {
-
+return new Promise(resolve => {
+    setTimeout(() => {
+    resolve('Resolved after 3 seconds');
+    }, 3000);
+});
 }
 
 function calculateTime() {
 
+    console.time("Time Taken to resolve all the three Promises");
+
+    const promise1 = waitOneSecond();
+    const promise2 = waitTwoSecond();
+    const promise3 = waitThreeSecond();
+
+    Promise.all([promise1, promise2, promise3])
+    .then((results) => {
+        console.log(results);
+        console.timeEnd("Time Taken to resolve all the three Promises")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 }
+
+function main()
+{
+    calculateTime();
+}
+
+main();
