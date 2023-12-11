6dfb9019a9fd9a35e8ad4a5038e6e23b0762b20a
Aditi Bhadoriya <awdhesh1700@gmail.com>
2023-12-11 11:56:30 +0530

    add easy assignment

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,39 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+ /* let result = {};
+
+  for (let transaction of transactions) {
+    const { category, price } = transaction
+    ;
+
+    if (!result[category]) {
+      result[category] = 0;
+    }
+
+    result[category] += price;
+  }
+
+  // Convert result object to the desired output format
+  const output = Object.entries(result).map(([category, totalAmount]) => ({
+    [category]: totalAmount,
+  }));
+
+  return output;*/
+
+  let expenses = {};
+  for (const tnx of transactions) {
+    expenses[tnx.category] = (expenses[tnx.category] || 0) + tnx.price;
+  }
+
+  let output = [];
+  for (const category of Object.keys(expenses)) {
+    const categoryObj = {};
+    categoryObj[category] = expenses[category];
+    output.push(categoryObj);
+  }
+
+  return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
