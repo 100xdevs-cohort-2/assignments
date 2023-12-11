89bf606a17f73c55d943b46bb74165c780b5dd04
mcace007 <mrinalace@gmail.com>
2023-12-12 00:36:51 +0530

    Updated: Solved

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,47 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
-}
+function sum(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  function calculateTime(n) {
+    const startTime = new Date();
+  
+    sum(n);
+  
+    const endTime = new Date();
+  
+    const timeDiff = (endTime - startTime) / 1000;
+  
+    return timeDiff;
+  }
+  
+  console.log("Time taken for sum from 1 to 100:", calculateTime(100));
+  console.log("Time taken for sum from 1 to 100000:", calculateTime(100000));
+  console.log(
+    "Time taken for sum from 1 to 1000000000:",
+    calculateTime(1000000000)
+  );
+  
+  //Outputs:
+  
+  // First Try:
+  // Time taken for sum from 1 to 100: 0
+  // Time taken for sum from 1 to 100000: 0.001
+  // Time taken for sum from 1 to 1000000000: 1.063
+  
+  // Second Try:
+  // Time taken for sum from 1 to 100: 0
+  // Time taken for sum from 1 to 100000: 0.001
+  // Time taken for sum from 1 to 1000000000: 1.005
+  
+  // Third Try:
+  // Time taken for sum from 1 to 100: 0
+  // Time taken for sum from 1 to 100000: 0.001
+  // Time taken for sum from 1 to 1000000000: 1.014
+  
