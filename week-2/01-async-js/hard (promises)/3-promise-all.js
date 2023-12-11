5d977e6b6c129095f00cc9b8c2bc3c574dfb9bc4
shubham bajaj <shmbajaj32@gmail.com>
2023-12-11 15:24:08 +0530

    done with all promises

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,45 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1 * 1000, 1);
+  });
 }
 
 function waitTwoSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2 * 1000, 2);
+  });
 }
 
 function waitThreeSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, 3 * 1000, 3);
+  });
 }
 
 function calculateTime() {
-
+  const startTime = Date.now();
+  Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()])
+    .then((value) => console.log({ value }))
+    .catch((e) => console.error({ e }))
+    .finally(() => {
+      const endTime = Date.now();
+      const elapsed = endTime - startTime;
+      const seconds = elapsed / 1000;
+      console.log(`It took ${seconds} seconds.`);
+    });
 }
+
+async function calculateTimeX() {
+  const startTime = Date.now();
+  await Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()]);
+  const endTime = Date.now();
+  const elapsed = endTime - startTime;
+  const seconds = elapsed / 1000;
+  console.log(`It took ${seconds} seconds for X.`);
+}
+
+calculateTime();
+calculateTimeX();
