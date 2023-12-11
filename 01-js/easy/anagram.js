a26336fa28926db75267ff85d5a4a5877296bcd0
mcace007 <mrinalace@gmail.com>
2023-12-12 00:29:34 +0530

    Updated: Solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    return false;
+  }
 
+  str1 = str1.toLowerCase().split("").sort().join("");
+  str2 = str2.toLowerCase().split("").sort().join("");
+
+  if (str1 === str2) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
