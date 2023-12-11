69b0041b9be5fd46cb4164e1a25657fa1914cbd5
AdarshJha <adarsh2005j@gmail.com>
2023-12-11 15:54:58 +0530

    new

--- week-2/01-async-js/easy/3-read-from-file.js
+++ week-2/01-async-js/easy/3-read-from-file.js
@@ -3,7 +3,7 @@ let count = () => {for(let i=0; i<=1000000; i++){
 }}
 count();
 const fs = require('fs');
-fs.readFile('one-piece.txt', 'utf8', (err, data) => {
+fs.readFile('3-one-piece.txt', 'utf8', (err, data) => {
     if(err) throw err;
     console.log(data);
 })
