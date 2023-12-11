1ba8b69c8db592f553e0777dfbb580eb7d9d9f77
AdarshJha <adarsh2005j@gmail.com>
2023-12-11 17:18:49 +0530

    1-promisify-setTimeout solutin

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,14 @@
 */
 
 function wait(n) {
+    let ms = n*1000;
+    if(ms<1000){
+        return -1;
+    }
+    const promise = new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+    return promise;
 }
 
 module.exports = wait;
