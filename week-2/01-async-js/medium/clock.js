fb78d3da7eabda5a690e504c45c94df2272462c1
Rajpratim <rajpratim.001@gmail.com>
2023-12-11 19:25:58 +0530

    fixed clock problem

--- week-2/01-async-js/medium/clock.js
+++ week-2/01-async-js/medium/clock.js
@@ -1,10 +1,12 @@
 function clock() {
   setInterval(() => {
-    const time = new Date()
-    console.log(time.toLocaleTimeString() + "  " + time.toLocaleTimeString("en-us", { hour12: false }));
+    const day = new Date()
+    console.log(day.toLocaleTimeString() + "  " + day.toLocaleTimeString("en-us", { hour12: false }));
   }, 1000)
 }
 clock()
-// const time = new Date()
-// console.log(time.toLocaleTimeString("en-us", { hour12: false }))
+// const time = new Date("01/01/2022 09:15 PM")
+// console.log(time.toTimeString().substring(0, 8))
+
+
 
