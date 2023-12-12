82322c07339f6cdac552f1f0b9e01f47b1e612ee
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-13 00:51:33 +0530

    2-sleep-completely solution

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,16 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
 }
+
+(async () => {
+  console.log("Start");
+  await sleep(5);
+  console.log("End");
+})();
