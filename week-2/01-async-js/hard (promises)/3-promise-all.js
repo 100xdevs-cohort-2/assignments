d8152459d465d51437ab873fe79837ae3f8b0a6c
AbhayMishra <grabhaymishra@gmail.com>
2023-12-13 02:53:00 +0530

    Solved week-2 assignments

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,20 +4,63 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
-
+async function wait1(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, t * 1000)
+    })
 }
 
-function wait2(t) {
-
+async function wait2(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, t * 1000)
+    })
 }
 
-function wait3(t) {
+async function wait3(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, t * 1000)
+    })
+}
+async function run(t1, t2, t3) {
 
+    const result = await Promise.all([
+        wait1(t1),
+        wait2(t2),
+        wait3(t3)
+    ])
+    return result;
 }
 
-function calculateTime(t1, t2, t3) {
 
+
+// async function calculateTime(t1, t2, t3) {
+
+//     const start = new Date()
+//      await run(t1,t2,t3)
+//     const end = new Date()
+
+//     return end - start
+// }
+
+
+async function calculateTime(t1, t2, t3) {
+
+    const start = new Date()
+    await Promise.all([
+        wait1(t1),
+        wait2(t2),
+        wait3(t3)
+    ])
+    const end = new Date()
+
+    return end - start
 }
+
 
 module.exports = calculateTime;
