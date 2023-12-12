38065b820e13bc35379e1b4fb46d6b8fda9bbccb
Mohd Amaan <mhdamaan79@gmail.com>
2023-12-12 18:30:50 +0530

    completed hard (promises) - (week-2)

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,24 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const startTime = new Date();
+  return new Promise((resolve) => {
+    Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(() => {
+      resolve(new Date() - startTime);
+    });
+  });
 }
 
 module.exports = calculateTime;
--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,25 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const startTime = new Date();
+  return new Promise((resolve) => {
+    wait1(t1)
+      .then(() => wait2(t2))
+      .then(() => wait3(t3))
+      .then(() => resolve(new Date() - startTime));
+  });
 }
 
 module.exports = calculateTime;
