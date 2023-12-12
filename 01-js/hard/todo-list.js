9f193f4fafa392f11634b7f0a2622bc52d121ff2
Abhishek <abhishek4080@gmail.com>
2023-12-12 16:37:50 +0530

    hard assignment-todolist done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,86 @@
 */
 
 class Todo {
+  constructor()
+  {
+    this.todoList = [];
+  }
+  /** adds todo to list of todos */
+   add(taskname){
+    this.taskname = taskname;
+    return this.todoList.push(taskname);
+    
+
+  }
+
+  /**returns all todos */
+  getAll(){
+    console.log(this.todoList)
+    return this.todoList
+  }
+
+/** remove todo from list of todos  */  
+  remove(indexOfTodo){
+    if (indexOfTodo<this.todoList.length){
+      return this.todoList.splice((indexOfTodo),1)
+    }
+    else{
+      return "Invalid index"
+    }
+    
+    // this.todoList.splice((indexOfTodo-1),1)
+  }
+
+  clear(){
+    const len = this.todoList.length
+    for(let i =0;i<len;i++){
+      this.todoList.pop()
+    }
+    console.log("TODO list cleared")
+    return this.getAll()
+  }
+
+/**returns todo at given index */
+  get(indexOfTodo){
+    if (this.todoList[indexOfTodo]){
+      return this.todoList[indexOfTodo]
+    }
+    else{
+      return null
+    }
+    
+    
+
+  }
+
+  /**update todo at given index */
+  update(index, updatedTodo){
+    if (index<this.todoList.length){
+      this.todoList[index]=updatedTodo
+      console.log("TODOLIST updated")
+    }
+    
+    
+    return this.getAll()
+
+  }
+  
 
 }
+
+let mytodo = new Todo()
+mytodo.add("Go for a walk")
+mytodo.add("Go for a run")
+mytodo.add("Go for a swim")
+mytodo.add("Go for a dance")
+mytodo.getAll()
+mytodo.remove(24)
+// mytodo.getAll()
+// console.log(mytodo.clear())
+// console.log(mytodo.get(6))
+mytodo.update(7,"Take dog for a walk")
+
+
+
 
 module.exports = Todo;
