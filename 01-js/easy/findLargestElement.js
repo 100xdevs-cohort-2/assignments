fadc7446addd20cad5e1da27192bd2b9a46038fe
Ayush <ayush0999aryan@gmail.com>
2023-12-14 00:14:57 +0530

    largest element

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -7,6 +7,14 @@
 
 function findLargestElement(numbers) {
     
+    let biggestElement = numbers[0];
+    for (let i = 0; i< numbers.length; i++){
+        if ( numbers[i] > biggestElement){
+            biggestElement = numbers[i];
+        }
+    }
+    return biggestElement;
+    
 }
 
 module.exports = findLargestElement;
