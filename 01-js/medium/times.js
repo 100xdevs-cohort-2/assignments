978cb492c27c5a3b02d67b2545e0a9221a75bd70
arnnvv <arnav.900@protonmail.com>
2023-12-13 23:59:17 +0530

    ***

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,10 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const date = new Date();
+  let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+  return (Date.now()-date)/1000;
 }
