977460488c2422377e210eaea308d36009396b9f
sahil <sahilcodes23@gmail.com>
2023-12-13 23:03:02 +0530

    main

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length == 0) {
+    return undefined;
+  }
+  let largest = -Infinity;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
 }
 
 module.exports = findLargestElement;
