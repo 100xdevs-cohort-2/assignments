6604f4a88c9b060ba4ca874b9fc2dc06a0e58649
Anindo Choudhury <choudhuryanindo2004@gmail.com>
2023-12-14 17:46:03 +0530

    Added easy and medium solutions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,7 +3,7 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
-
+//
 function isAnagram(str1, str2) {
   const arr1 = Array.from(str1.toLowerCase()).sort(); 
   const arr2= Array.from(str2.toLowerCase()).sort(); 
