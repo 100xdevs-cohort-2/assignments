74655e962ff5b8f66aa41f9a067cda4722020fd2
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-12-15 02:10:11 +0530

    week 1 medium

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,13 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const startTime = new Date();
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  const endTime = new Date();
+  const res = (endTime - startTime) / 1000;
+  return res;
 }
+console.log(calculateTime(100000));
