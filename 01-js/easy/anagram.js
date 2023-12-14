002ea0a8f11c2326812e5c4ec842d519955dd391
Anindo Choudhury <choudhuryanindo2004@gmail.com>
2023-12-14 17:40:48 +0530

    Added easy and medium solutions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const arr1 = Array.from(str1.toLowerCase()).sort(); 
+  const arr2= Array.from(str2.toLowerCase()).sort(); 
+  let f=1; 
+  arr1.forEach((item,i)=>
+  {
+   if(item !== arr2[i])
+   f=0; 
+  })
+  return Boolean(f); 
 }
 
 module.exports = isAnagram;
+
