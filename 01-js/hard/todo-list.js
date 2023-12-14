52ff202d32641f9a5a9b2dbd8029313edde75f4d
nothuman2718 <nothuman2718@gmail.com>
2023-12-14 10:26:51 +0530

    Complete 01-js/hard

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,17 +1,78 @@
 /*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
+  Implement a class `Calculator` having below methods
+    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
+    - add: takes a number and adds it to the result
+    - subtract: takes a number and subtracts it from the result
+    - multiply: takes a number and multiply it to the result
+    - divide: takes a number and divide it to the result
+    - clear: makes the `result` variable to 0
+    - getResult: returns the value of `result` variable
+    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
+      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+      Points to Note: 
+        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {
+class Calculator {
+  constructor(result) {
+    this.result = 0;
+  }
 
+  add(num) {
+    if (typeof num === "number") {
+      this.result += num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+
+  subtract(num) {
+    if (typeof num === "number") {
+      this.result -= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+
+  multiply(num) {
+    if (typeof num === "number") {
+      this.result *= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+
+  divide(num) {
+    if (typeof num === "number" && num !== 0) {
+      this.result /= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    // Remove multiple spaces, if present and evaluate the expression.
+    this.result = eval(expression.replace(/\s+/g, " "));
+    // Check if the result is Infinity. If so, throw an Error.
+    // This is to handle invalid expressions.
+    if (this.result === Infinity) {
+      throw new Error("Infinity");
+    }
+    return this.result;
+  }
 }
+let calc = new Calculator();
+console.log(calc.calculate("10 +   2 *    (   6 - (4 + 1) / 2) + 7"));
 
-module.exports = Todo;
+module.exports = Calculator;
