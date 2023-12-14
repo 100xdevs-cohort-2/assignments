9069eb076158a5e61bbad189e0c3ca8e4fc78b07
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-12-15 01:44:58 +0530

    week 01 easy done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false; 
+  }
 
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+  
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
