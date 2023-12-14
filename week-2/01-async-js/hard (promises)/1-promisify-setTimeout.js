18c90b18ab288f30541004d20310a1277d6322d0
Ameer jafar <ameerjafar123@gmail.com>
2023-12-14 15:38:35 +0530

    this change was made my the owner

---
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -0,0 +1,18 @@
+/*
+    Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+*/
+
+function wait(n) {
+    return new Promise(function(resolves) {
+        setTimeout(function() {
+            resolves();
+        }, n * 1000);
+    })
+}
+let a = wait(3000);
+a.then(function () {
+    console.log(a);
+})
+
+
+module.exports = wait;
---
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -0,0 +1,18 @@
+/*
+ * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
+ * During this time the thread should not be able to do anything else.
+ * the function should return a promise just like before
+ */
+
+function sleep(milliseconds) {
+    return new Promise(function(resolves) {
+        setTimeout(function() {
+            resolves();
+        }, milliseconds);
+    })
+}
+async function anotherCall() {
+    await sleep(3000);
+}
+anotherCall();
+module.exports = sleep;
