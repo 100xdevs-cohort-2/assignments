7a2103878eefed1c1bc97c80d2d1e22f9677f32d
Nevil844 <nevil.j@ahduni.edu.in>
2023-12-14 16:46:26 +0530

    week-1-easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length!=str2.length){
+    return false;
+  }
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  let a=0;
+  for(let i=0;i<str1.length;i++){
+    for(let j=0;j<str2.length;j++){
+      if(str1[i]==str2[j]){
+        a=1;
+        str2[j]='';
+      }
+    }
+    if(a==0){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,62 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let ans=[];
+  for(let i = 0; i < transactions.length; i++) {
+    let arr={};
+    if(ans.some(e=>e.category==transactions[i].category)) continue;
+    else{arr.category=transactions[i].category;}
+    arr.totalSpent=0;
+    for(let j = 0; j < transactions.length; j++) {
+      if(transactions[i].category==transactions[j].category){
+        arr.totalSpent+=transactions[j].price;
+      }
+    }
+    ans.push(arr);
+  }
+  return ans;
 }
+
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
+
+ans=calculateTotalSpentByCategory(transactions);
+
+console.log(ans);
 
 module.exports = calculateTotalSpentByCategory;
