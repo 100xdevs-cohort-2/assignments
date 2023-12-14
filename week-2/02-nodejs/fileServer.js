d74c3bc8533d50b40c65e9b437701a6578b6382b
saif <mdsaifurrahman@navneettoptech.com>
2023-12-14 15:10:22 +0530

    fileserver made

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,41 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const bodyParse = require("body-parser");
 const app = express();
+app.use(bodyParse.json());
+const portNumber = 9000;
 
+app.get("/files", (req, res) => {
+  const folderPath = path.join(__dirname, "files");
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to retrieve files" });
+    }
+    res.json(files);
+  });
+});
+
+app.get("/files/:filename", async (req, res) => {
+  const fileName = req.params.filename;
+  const getFile = path.join(__dirname, "files", fileName);
+  fs.readFile(getFile, "utf-8", (err, content) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(content);
+  });
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(portNumber, () => {
+  console.log(`port running at ${portNumber}`);
+});
 
 module.exports = app;
---
+++ week-2/02-nodejs/files/sample.js
@@ -0,0 +1,7 @@
+let sampleData = {
+  name: "Saif",
+  age: 23,
+  profession: "aspiring 100xdev software engineer",
+  currentWorkingLocation: "Bengaluru",
+  preferredWorkLocation: "Hyderabad",
+};
