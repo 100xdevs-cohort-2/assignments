62835787dac4d4d97fc83cda6a877a47bc58bed4
krishvsoni <sonikrish994@gmail.com>
2023-12-15 00:21:23 +0530

    async solved

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -1,13 +1,24 @@
-## File cleaner
-Read a file, remove all the extra spaces and write it back to the same file.
+const fs = require('fs');
 
-For example, if the file input was
-```
-hello     world    my    name   is       raman
-```
+const filePath = 'inputfile.txt'; 
+function cleanFile() {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
 
-After the program runs, the output should be
+    const cleanedContent = data.replace(/\s+/g, ' ');
 
-```
-hello world my name is raman
-```
+    fs.writeFile(filePath, cleanedContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+        return;
+      }
+
+      console.log('File has been cleaned and updated successfully.');
+    });
+  });
+}
+
+cleanFile();
