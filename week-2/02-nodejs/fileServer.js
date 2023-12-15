216b00908bba932322f67a65b54dc466223f03b9
GREEDnim <sivadhas007@gmail.com>
2023-12-15 11:36:52 +0530

    feat(w2.2) : added solution for file server

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -13,9 +13,31 @@
     Testing the server - run `npm run test-fileServer` command in terminal
  */
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
 const app = express();
 
 
+app.get('/files', (req, res)=> {
+  fs.readdir('./files', (err, files) => {
+  if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
+  }
+  res.json(files);
+  });
+});
+
+app.get('/file/:filename',(req,res)=>{
+  const filename='./files/'+req.params.filename;
+
+  fs.readFile(filename,'utf8',(err,data)=>{
+    if(err) return res.status(404).send("File not found");
+    res.status(200).send(data);
+  })
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// app.listen(3000);
 module.exports = app;
