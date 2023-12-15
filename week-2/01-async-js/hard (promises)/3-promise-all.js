6ca27d3f0b8f037b845b966f59a854ceadb8e244
Taha <tiftikhar@student.iul.ac.in>
2023-12-15 10:58:31 +0530

    Update 3-promise-all.js

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,36 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, t*1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, t*1000);
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, t*1000);
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    let start = Date.now();
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(() => {
+        return new Promise((resolve) => {
+            resolve(Date.now() - start);
+        });
+    });
 }
 
 module.exports = calculateTime;
