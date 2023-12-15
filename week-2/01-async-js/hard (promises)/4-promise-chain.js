66a59a9a6bf0e29621f15d46479cb1c28e11432e
Gurram Ranga Manoj Kumar <GurramRanga.ManojKumar@philips.com>
2023-12-15 11:10:17 +0530

    merge conflicts

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -1,11 +1,11 @@
 /*
- * Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
+ * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
  * Write a function that sequentially calls all 3 of these functions in order.
- * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
+ * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
+
 
 function waitOneSecond() {
     return new Promise(function(resolve){
@@ -13,7 +13,7 @@ function waitOneSecond() {
     })
 }
 
-function wait2(t) {
+
 
 function waitTwoSecond() {
     return new Promise(function(resolve){
@@ -21,7 +21,7 @@ function waitTwoSecond() {
     })
 }
 
-function wait3(t) {
+
 
 function waitThreeSecond() {
     return new Promise(function(resolve){
@@ -29,11 +29,9 @@ function waitThreeSecond() {
     })
 }
 
-function calculateTime(t1, t2, t3) {
 
-}
 
-module.exports = calculateTime;
+
 
 async function calculateTime() {
     const startTime = Date.now();
@@ -44,4 +42,7 @@ async function calculateTime() {
   const timeTaken = endTime - startTime;
   console.log(timeTaken);
 }
+
 calculateTime();
+
+module.exports = calculateTime;
