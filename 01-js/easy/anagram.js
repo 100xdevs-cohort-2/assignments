f4802faac3bf4736d6295ecd8ebc3ce96749b3e5
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2023-12-15 13:41:35 +0530

    merge test

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
+  if(cleanStr1.length !== cleanStr2.length){
+    return false
+  }
+  const sortedStr1 = cleanStr1.split('').sort().join('')
+  const sortedStr2 = cleanStr2.split('').sort().join('')
+
+  return sortedStr1 === sortedStr2
 }
+
+const result1 = isAnagram('listen', 'silent');
+console.log(result1);
+const result2 = isAnagram('Hello World!', 'dlrow olleh');
+console.log(result2)
+const result3 = isAnagram('hello', 'world');
+console.log(result3);
 
 module.exports = isAnagram;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,14 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0]
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i] > max){
+            max = numbers[i]
+        }
+    }
+    return max
 }
+let numbers = [3, 7, 2, 9, 1]
 
 module.exports = findLargestElement;
