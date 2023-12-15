d6711b6868ed38cb2b32c5923599389394dff19b
Abhisek <sahabhisek11@gmail.com>
2023-12-16 01:57:48 +0530

    Week1(Easy)-Anagram-Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,38 @@
 */
 
 function isAnagram(str1, str2) {
+  const a = new Array(256);
+  const b = new Array(256);
+  for(let i=0;i<256;i++){
+          
+    a[i]=0;
+    b[i]=0;
+  }
+        for(let i=0;i<str1.length;i++){
+          let x=str1.charCodeAt(i);
+          if(x>=65 && x<=90){
+            x=x+32;
+          }
+          a[x]++;
+        }
 
+        for(let i=0;i<str2.length;i++){
+          let x=str2.charCodeAt(i);
+          if(x>=65 && x<=90){
+            x=x+32;
+          }
+          b[x]++;
+          
+        }
+        //console.log(b);
+
+        for(let i=0;i<256;i++){
+          
+          if(a[i]!==b[i]){
+            return false;
+          }
+        }
+        return true;
 }
 
 module.exports = isAnagram;
