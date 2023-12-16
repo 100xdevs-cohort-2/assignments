98afbd491e190d1279d470e89f6edde444c42aab
Teja Annamdevula <tejaannamdevula@gmail.com>
2023-12-16 15:38:56 +0530

    completedanagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if( str1.length!= str2.length){
+    return false;
+  } 
+  let visited = {};
+  for (let i =0; i< str1.length;i++){
+    visited[str1[i].toLowerCase()] = 1;
+  }
+  for(let i =0;i<str2.length;i++){
+    if(visited[str2[i].toLowerCase()] != 1){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
