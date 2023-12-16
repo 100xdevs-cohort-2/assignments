20288c6b31c3eaae37bbbae4590ce93931c73868
Deep Patel <deeppatel7521@gmail.com>
2023-12-16 14:59:36 +0530

    fileSever.js using experss get methods status code

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,30 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.get("/files", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) throw new Error(err);
+
+    res.status(200).json(files);
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filename = req.params.filename;
+
+  fs.readFile(`./files/${filename}`, (err, data) => {
+    if (err) res.status(404).send("File not found");
+    else res.status(200).send(data);
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
