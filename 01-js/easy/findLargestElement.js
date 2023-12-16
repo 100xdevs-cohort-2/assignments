3d880375bddbf5c9b52d90fc5c6524ff2cf08a07
karananeja <karananeja.r@gmail.com>
2023-12-16 22:11:48 +0530

    solved second question for easy cat

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,17 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length === 0) return undefined;
+
+  let largestElement = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largestElement) {
+      largestElement = numbers[i];
+    }
+  }
+
+  return largestElement;
 }
 
 module.exports = findLargestElement;
