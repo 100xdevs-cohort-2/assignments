6fcaa63a5811e4ef7164cb11a25d0518bb938aed
AdilLearns2584 <mohemmed2584@gmail.com>
2023-12-17 18:01:36 +0530

    week-2

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,37 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve()
+        },t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve()
+        },t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve()
+        },t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const dateInMillis = Date.now()
+    return Promise.all([wait1(t1),wait2(t2),wait3(t3)]).then(()=>
+            {
+                return Date.now() - dateInMillis
+           })
+    
 }
+
 
 module.exports = calculateTime;
