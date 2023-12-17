77d62187a4641d6042da9136ca02d72601bdfa94
Farjul Mallik <farjulmallik10@gmail.com>
2023-12-17 13:41:41 +0530

    solved week-2 assignment questions

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -4,21 +4,44 @@
  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
+const calculatedTime = require("./3-promise-all");
 
 function wait1(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startTime = Date.now();
+    return wait1(t1)
+    .then(() => wait2(t2))
+    .then(() => wait3(t3))
+    .then(() => {
+      const endTime = Date.now();
+      const totalTimeTaken = endTime - startTime;
+      return totalTimeTaken;
+    });
+    // const threePromise = calculatedTime(t1, t2, t3).then(function (result) {return result;})
+    // return totalTimeTaken - threePromise;
 }
 
 module.exports = calculateTime;
