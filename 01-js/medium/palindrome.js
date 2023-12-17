9d619ef96eb4f215127e98453cfa34dcb191311d
karananeja <karananeja.r@gmail.com>
2023-12-17 15:47:46 +0530

    spell check

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -1,6 +1,6 @@
 /*
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
-  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrome as 'N' and 'n' are considered case-insensitive.
 */
 
 function validChar(char) {
