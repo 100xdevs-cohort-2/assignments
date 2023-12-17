7a11a3ec07fbe3e23101d4f61e4124a0fd1c6b8a
mannemraviteja25 <mannemraviteja25@gmail.com>
2023-12-17 18:08:13 +0530

    all assignments completed

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,20 +5,40 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
+function delay(t) {
+    return new Promise(resolve => setTimeout(resolve, t * 1000));
 }
 
-function wait2(t) {
-
+function wait1(t1) {
+    return delay(t1).then(() => "Function 1");
 }
 
-function wait3(t) {
+function wait2(t2) {
+    return delay(t2).then(() => "Function 2");
+}
 
+function wait3(t3) {
+    return delay(t3).then(() => "Function 3");
 }
 
 function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
 
+    return wait1(t1)
+        .then(result1 => {
+            console.log(result1);
+            return wait2(t2);
+        })
+        .then(result2 => {
+            console.log(result2);
+            return wait3(t3);
+        })
+        .then(result3 => {
+            console.log(result3);
+            const endTime = Date.now();
+            const totalTime = endTime - startTime;
+            return totalTime;
+        });
 }
 
 module.exports = calculateTime;
