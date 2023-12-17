6bdb15169aa7188710550ae396b9b1dee8ca2967
HackerdX <cooleasycodebyanmol@gmail.com>
2023-12-17 19:06:24 +0530

    deleted files

--- week-2/01-async-js/easy/1-counter.js
+++
@@ -1,9 +0,0 @@
-let i = 0;
-const logIt = (i) => {
-    console.log(i);
-}
-
-setInterval(() => {
-    console.log(++i);
-}, 1000);
-
--- week-2/01-async-js/easy/3-read-from-file.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-fs.readFile('a.txt', 'UTF-8', (err, data) => {
-    console.log(data);
-})
