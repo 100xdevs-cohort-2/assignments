d3f2e39909839d2461a5df206f928de7b3ca7982
subrat2004 <subratsamal5000@gmail.com>
2023-12-17 13:34:21 +0530

    finished week 2 assignment

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -3,21 +3,45 @@
  * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
-
 function wait1(t) {
-
+const prom1=new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve();
+    }, t*1000);
+}) 
+return prom1;
 }
 
 function wait2(t) {
-
+const prom2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve();
+    },t*1000);
+})
+return prom2;
 }
 
 function wait3(t) {
-
+ const prom3=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve();
+    },t*1000);
+    
+ }) 
+ return prom3;
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startime=Date.now();
+const p1=wait1(t1);
+const p2=wait2(t2);
+const p3=wait3(t3);
+const finalpromise=new Promise.all([p1,p2,p3]).then((result) => {
+    const endtime=Date.now();
+    const timetaken=endtime-startime;
+    return timetaken;
+}) 
+return finalpromise;
 }
 
 module.exports = calculateTime;
