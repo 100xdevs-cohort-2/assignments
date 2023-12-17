f955ed81c5dfa94a1ea5345657cf8f395899d89b
Anindo Choudhury <111510940+AnindoChoudhury@users.noreply.github.com>
2023-12-17 23:38:31 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -10,39 +10,3 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {
-  constructor()
-  {
-     this.result=[]; 
-  }
-  add(todo)
-  {
-     this.result.push(todo); 
-  }
-  remove(index)
-  {
-    if(index<this.result.length)
-    this.result.splice(index,1); 
-  }
-  update(index,updatedTodo)
-  {
-    if(index<this.result.length)
-    this.result[index]=updatedTodo; 
-  }
-  getAll()
-  {
-    return this.result; 
-  }
-  get(index)
-  {
-    if(index<this.result.length)
-    return this.result[index]; 
-    return null; 
-  }
-  clear()
-  {
-    this.result.splice(0,this.result.length); 
-  }
-}
-
-module.exports = Todo;
