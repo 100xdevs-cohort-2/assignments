225da99639efabe0f374d4aa6a7becebd6c8b9c4
Sunil1068 <penikilapatisunil@gmail.com>
2023-12-18 10:03:24 +0530

    weel-01 assignment completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,23 @@
 
 function isAnagram(str1, str2) {
 
+  if (str1.length !== str2.length) return false;
+
+  const charFrequency = {};
+  const lowerCaseStr1 = str1.toLowerCase(),
+    lowerCaseStr2 = str2.toLowerCase();
+
+  for (const char of lowerCaseStr1) {
+    charFrequency[char] = (charFrequency[char] || 0) + 1;
+  }
+
+  for (const char of lowerCaseStr2) {
+    if (charFrequency[char]) charFrequency[char]--;
+    else return false;
+  }
+
+  return true;
+
 }
 
 module.exports = isAnagram;
