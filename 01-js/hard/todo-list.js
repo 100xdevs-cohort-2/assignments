79a524472951cd63fd707bf302b0dfe42c3866d8
stainsleto <stainsmault@gmail.com>
2023-12-19 05:17:27 +0530

    hard in progress

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,47 @@
 
 class Todo {
 
+  allTodo =[]
+  newTodo =[]
+
+  add(todo){
+
+    this.allTodo.push(todo)
+  }
+
+  remove(index){
+    this.allTodo.splice(index, 1);
+  }
+
+  update(index, todo){
+    this.allTodo[index] = todo
+  }
+
+  getAll(){
+    console.log(this.allTodo);
+    return this.allTodo
+  }
+
+  get(index){
+    return this.allTodo[index]
+  }
+
+  clear(){
+    this.allTodo.splice(0,this.allTodo.length)
+  }
+
+
+
 }
+
+
+// let todoClass = new Todo
+// todoClass.add("Task 1")
+// todoClass.add("Task 2")
+// todoClass.add("Task 3")
+// todoClass.add("Task 4")
+
+// todoClass.remove(1)
+// todoClass.getAll()
 
 module.exports = Todo;
