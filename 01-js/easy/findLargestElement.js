2d66bd541272c964534bac4ab68c74835a40b003
Mahaveer Prasad Agarwal <mveer.jaipur@gmail.com>
2023-12-18 06:24:03 +0100

    largest number

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,22 @@
 */
 
 function findLargestElement(numbers) {
+    if(!Array.isArray(numbers)) {
+        return 'Error! Numbers is not an array.'
+    } 
+
+    if(numbers.length === 0) {
+        return undefined;
+    }
+
+    let largestElement = numbers[0];
+    for(const number of numbers) {
+        if(number > largestElement) {
+            largestElement = number;
+        }
+    }
+
+    return largestElement;
     
 }
 
