3ac9202134241e7cd423119589c5c70ea5c31bab
“Sanket <sanketsj00900@gmail.com>
2023-12-20 00:13:28 +0530

    03-errorcount.js solution added

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -8,14 +8,25 @@ let errorCount = 0;
 // You have been given an express server which has a few endpoints.
 //TODO
 // Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 1. Ensure that if there is ever an exception, the end user seaes a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get("/user", function (req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: "john" });
+let errorcount = 0;
+
+app.use((err, req, res, next) => {
+  errorCount++;
+  console.error(err);
+  res.status(400).json({ error: "nof found" });
 });
 
+app.get("/user", function (req, res, next) {
+  try {
+    throw new Error("User not found");
+  } catch (error) {
+    next(error);
+  }
+  res.status(200).json({ name: "john" });
+});
 app.post("/user", function (req, res) {
   res.status(200).json({ msg: "created dummy user" });
 });
