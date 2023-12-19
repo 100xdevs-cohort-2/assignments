2c6f9c33374137bb60f75b66048797a267fff29c
Ranjit Yadav <yadavranjit521@gmail.com>
2023-12-19 22:38:10 +0530

    completed week 2 async assignment

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,27 +5,22 @@
  */
 
 function wait1(t) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, t * 1000);
-    })
+    return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait2(t) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, t * 1000);
-    })
+    return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait3(t) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, t * 1000);
-    })
+    return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
-function calculateTime(t1, t2, t3) {
-    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then((values) => {
-        //
-    })
+async function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
+    await Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
+    const endTime = Date.now();
+    return endTime - startTime;
 }
 
 module.exports = calculateTime;
