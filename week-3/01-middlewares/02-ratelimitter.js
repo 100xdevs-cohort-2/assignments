f3588cdbe232fc127d8adfe70dd33056bea7f0cc
“Sanket <sanketsj00900@gmail.com>
2023-12-20 00:05:28 +0530

    02-ratelimitter solution added

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,27 +1,46 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
+const { error } = require("console");
 const app = express();
+
+//TODO
 // You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// rate limit the requests from a user to only 5 request per second
-// If a user sends more than 5 requests in a single second, the server
-// should block them with a 404.
+// Your task is to create a global middleware (app.use) which will rate limit the requests from a user to only 5 request per second.
+//! If a user sends more than 5 requests in a single second, the server should block them with a 404.
+
 // User will be sending in their user id in the header as 'user-id'
-// You have been given a numberOfRequestsForUser object to start off with which
-// clears every one second
+
+// You have been given a numberOfRequestsForUser object to start off with which clears every one second
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use((req, res, next) => {
+  const userId = req.headers.userid;
+
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+
+  if (numberOfRequestsForUser >= 5) {
+    return res.status(404).json({ msg: "Rate limit exceeded" });
+  }
+
+  numberOfRequestsForUser[userId]++;
+  next();
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
 });
 
 module.exports = app;
