04a8cd24426b669eb64b2c6e7bf9ea95dd8c28c6
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2023-12-19 10:44:37 +0530

    Code added

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,17 @@
 */
 
 function isAnagram(str1, str2) {
+  // Prakhar
+  var n1 = str1.length;
+  var n2 = str2.length;
+  if (n1 != n2)
+      return false;
+  str1.sort();
+  str2.sort()
+  for (let i = 0; i < n1; i++)
+      if (str1[i] != str2[i])
+          return false;
+  return true;
 
 }
 
