a294684c396965019a758ffe97ee6e35c087e1fd
Mathana Suman <99850231+Maddy000777@users.noreply.github.com>
2023-12-19 19:27:42 +0530

    Update 4-promise-chain.js

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,47 @@
  */
 
 function wait1(t) {
-
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        },t);
+    });
 }
 
 function wait2(t) {
-
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        },t);
+    });
 }
 
 function wait3(t) {
-
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        },t);
+    });
 }
 
 function calculateTime(t1, t2, t3) {
+    const start = Date.now();
 
+    return wait1(1000)
+    .then(() => wait2(2000))
+    .then(() => wait3(3000))
+    .then(() => {
+      const end = Date.now();
+      const elapsedTime = end - start;
+      return elapsedTime;
+});
 }
 
+calculateTime()
+.then(result => {
+    console.log('Total time elapsed:', result, 'milliseconds');
+})
+.catch(error  =>{
+    console.error('Error:', error);
+})
 module.exports = calculateTime;
