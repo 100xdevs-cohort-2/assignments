2f1ab5e425e108ecf282a7c92d0893da997532fa
Aditya <devchakkeaditya37@gmail.com>
2023-12-20 20:05:01 +0530

    medium done

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,30 @@
 // ```
 // hello world my name is raman
 // ```
+
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
+
+
+const p = new Promise((resolve,reject)=>{
+    fs.readFile('temp.txt','utf-8',(err,data)=>{
+       if(err){
+        reject("Unable to read file")
+       }
+
+       let dataWithoutExtraSpace = data.replace(/\s+/g,' ')
+       resolve(dataWithoutExtraSpace)
+    })
+
+
+})
+
+p.then((data)=>{
+    fs.writeFile('temp.txt',data,(err)=>{
+        if(err){
+            console.log("error writing to file",err);
+        }
+        console.log("writing file success");
+    })
+})
