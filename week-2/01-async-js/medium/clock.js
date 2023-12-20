fba31392ec116b20409287c7a390a7b5f91037a8
Shenoy <shravanshenoy1998@hotmail.com>
2023-12-20 13:18:11 +0530

    Solved w2 async js problems - easy, medium, hard

---
+++ week-2/01-async-js/medium/clock.js
@@ -0,0 +1,12 @@
+
+function clock()
+{
+    //let time = new Date().toLocaleTimeString();
+    setInterval(
+        function(){
+            console.log(new Date().toLocaleTimeString()); 
+        }, 1000);
+
+}
+
+clock()
---
+++ week-2/01-async-js/medium/file-cleaner.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+let unclean_data = "";
+
+function cleanText(text){
+    let cleaned_text = text.split(" ").filter(
+        (word) => word.length > 0
+        ).join(" ");
+
+    return cleaned_text;
+}
+
+fs.readFile("unclean-file.txt", "utf8", function(err, data){
+    unclean_data = data;
+    console.log(unclean_data);
+    let clean_data = cleanText(unclean_data);
+    fs.writeFile("clean-file.txt", clean_data, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("Clean data written to file");
+        }
+    });
+
+});
+
+
