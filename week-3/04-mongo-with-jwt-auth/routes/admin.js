3f59c41b3da4c93073c5614ba92661ffdaf162a5
kedarshenoy <akedarshenoy@gmail.com>
2023-12-20 20:43:29 +0530

    mongo assign week3

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,21 +1,60 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-
+const {Admin,Course} =require("../db/index")
+const jwt = require('jsonwebtoken');
+const jwtPassword = 'secret';
 // Admin Routes
-app.post('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password=req.body.password;
+
+    // console.log(token)
+    const newAdmin= new Admin({
+        username:username,
+        password:password
+    })
+    newAdmin.save().then(result=>{
+        // console.log(res);
+        res.status(200).json()
+    }).catch(Error=>{res.status(404).json({msg:Error})})
 });
 
-app.post('/signin', (req, res) => {
+router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password = req.body.password;
+    const token =jwt.sign({username:username},jwtPassword);
+
+    Admin.find({username:username,password:password}).then(result=>{if(result.length>0){res.status(200).json({msg:"loged in",token:token}) }})
+    .catch(err=>{
+        res.send(err);
+    })
+    
 });
 
-app.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const newCourse= new Course({
+        id:req.body.id,
+            title: req.body.title, 
+        description: req.body.description,
+         price: req.body.price,
+          imageLink: req.body.imageLink ,
+          CourseID:req.body.CourseID
+    });
+    newCourse.save().then(result=>{
+        res.status(200).json({msg:"Saved Succesfully"})
+    }).catch(err=>{console.log(err)})
 });
 
-app.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, (req, res) => {
+    Course
+    .find()
+    .then(courses => {
+        res.json(courses);
+    })
     // Implement fetching all courses logic
 });
 
--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,24 +1,77 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const {User,Course, Purchased} =require("../db/index")
+const jwt = require('jsonwebtoken');
+const jwtPassword = 'secret';
 // User Routes
-app.post('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username=req.body.username;
+    const password= req.body.password;
+    const newUser= new User(
+        {username:username,
+        password:password
+        }
+    );
+
+    newUser.save().then(result=>{res.status(200).json({msg:"user saved"})}).catch(err=>{res.status(400).json({msg:err})})
 });
 
-app.post('/signin', (req, res) => {
+router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username=req.body.username;
+    const password = req.body.password;
+    const token =jwt.sign({username:username},jwtPassword);
+
+    User.find({username:username,password:password}).then(result=>{if(result.length>0){res.status(200).json({msg:"loged in",token:token}) }})
+    .catch(err=>{
+        res.send(err);
+    })
 });
 
-app.get('/courses', (req, res) => {
+router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find().then(result=>{
+        res.status(200).json({
+            result
+        })
+    })
 });
 
-app.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseid=req.params.courseId;
+    Course.find({CourseID:courseid}).then(result=>{
+        const newcourse =new Purchased({
+                title: result[0].title,
+                description: result[0].description,
+                price: result[0].price,
+                imageLink:result[0].imageLink,
+                CourseID:courseid,
+                username:req.username
+        })
+        // console.log(req.username)
+        // console.log(result[0].title)
+        newcourse.save().then(result=>{res.status(200).json({msg:"course purchased"})})
+    
+    })
+    .catch(error =>{
+        res.status(400).send("course not found")
+    })
+    
+   
 });
 
-app.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
+    const username =req.username;
+    Purchased.find({username:username}).then(result => {
+        res.status(200).json({MyCourses:result})
+    }).catch(error =>{
+        res.status(200).json({error:error})
+    })
 });
+
+module.exports =router;
+
