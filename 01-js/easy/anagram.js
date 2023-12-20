cdfc2b1ad5b3f53672e1cea929c2faf884d16218
Manthan <manthansoul@gmail.com>
2023-12-20 19:55:55 +0530

    01-js Assignments Completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length != str2.length) {
+    return false;
+  }
+  let strSmall1 = str1.toLowerCase();
+  let strSmall2 = str2.toLowerCase();
+  let arr1 = Array(26).fill(0);
+  for (let i = 0; i < str1.length; i++) {
+    arr1[strSmall1.charCodeAt(i) - 97]++;
+    arr1[strSmall2.charCodeAt(i) - 97]--;
+  }
+  for (let i = 0; i < 26; i++) {
+    if (arr1[i] !== 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
