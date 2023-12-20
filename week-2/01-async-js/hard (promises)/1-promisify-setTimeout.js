5c2f479fa6a927ca0b5d69c17a321af63f9c7e67
Khushi Pandey <99187977+Khushi-Pandey@users.noreply.github.com>
2023-12-20 22:29:26 +0530

    Solution 1-promisify-setTimeout.js

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,16 @@
 */
 
 function wait(n) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve();
+        }, n*1000);
+    });
 }
+
+wait(2);
+
+// it's not important to write .then 
+// simply promisified fn can be called
 
 module.exports = wait;
