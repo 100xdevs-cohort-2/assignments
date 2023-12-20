736aabe32e76a93ae3c90c022dbaf80edb0d0656
ShanPS <mohammedshan.p.s@gmail.com>
2023-12-20 05:55:00 +0530

    add solution for mongo - week3

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
-// Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+const { Admin } = require("../db/index");
+
+// Middleware for handling admin auth
+async function adminMiddleware(req, res, next) {
+  try {
+    const { username, password } = req.headers;
+    const admin = await Admin.findOne({ username, password });
+
+    if (!admin) return res.status(401).json({ error: "Unauthorized" });
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 module.exports = adminMiddleware;
