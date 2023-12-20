63b2d3a39411ce907bdc39f6bb3de31b64358fe7
Khushi Pandey <99187977+Khushi-Pandey@users.noreply.github.com>
2023-12-20 23:09:13 +0530

    Solution 3-promise-all.js

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,35 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve()
+        }, t*1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve()
+        }, t*1000);
+    })
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve()
+        }, t*1000);
+    })
 }
 
-function calculateTime(t1, t2, t3) {
-
+async function calculateTime(t1, t2, t3) {
+    const start = Date.now();
+    await Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
+    return totalTime = Date.now() - start;
 }
+
+calculateTime(2, 3, 4)
 
 module.exports = calculateTime;
