47030444d5a110792c8940768169f9e9d51ec7d6
SagarUniyal16 <85723642+SagarUniyal16@users.noreply.github.com>
2023-12-22 00:41:51 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,15 @@
 
 function countVowels(str) {
     // Your code here
+  let count=0;
+    for(let i=0;i<str.length;i++)
+    {
+      if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'|| str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+      {
+        count++;
+      }
+    }
+    return count;
 }
 
 module.exports = countVowels;
