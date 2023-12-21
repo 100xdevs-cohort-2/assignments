19b7c9519207154394f1799a14c33e9e6f15a644
AdarshJha <adarsh2005j@gmail.com>
2023-12-21 18:23:42 +0530

    02-ratelimitter.js solution

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,18 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+  numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId] > 5){
+      return res.status(404).send();
+    }
+    if(numberOfRequestsForUser[userId] <= 5){
+      next();
+    }
+    
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
