371b9abfe184092dfc7f0f9bf2fe3eae9895a0a0
VijeshRBhat <vijeshrbhat@gmail.com>
2023-12-21 21:02:41 +0530

    week-3/01,02 completed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,24 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req,res,next)=>{
+  const userId = req.headers["user-id"];
+
+  if(numberOfRequestsForUser.userId) {
+    numberOfRequestsForUser.userId++;
+    if(numberOfRequestsForUser.userId > 5) {
+      res.status(404).json("Sorry, u have exceeded request count");
+    }
+    else{
+      next();
+    }
+  }
+  else{
+    numberOfRequestsForUser.userId = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
