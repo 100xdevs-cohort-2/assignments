52bc396a08498a8e450baa0e5ec4ace9aa47aee2
SagarUniyal16 <85723642+SagarUniyal16@users.noreply.github.com>
2023-12-22 00:45:30 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -11,6 +11,7 @@ There is no automated test for this one, this is more for you to understand time
 function calculateTime(n) {
 
     const startTime = new Date();
+
     
     let sum=0;
     for(let i=1;i<=n;i++)
