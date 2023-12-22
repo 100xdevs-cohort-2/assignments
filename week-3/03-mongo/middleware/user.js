5fd2d47e364ee1a3c9e6805e378985b4f72d0bf8
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-22 20:15:23 +0530

    week 3 mongo

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,17 @@
-function userMiddleware(req, res, next) {
+const { User } = require("../db");
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const {username, password} = req.headers;
+    const verifiedUser = await User.findOne({username: username, password: password})
+    if(!verifiedUser) {
+        res.status(401).send("unauthorized user")
+        return
+    } else {
+        req.user = verifiedUser;
+        next();
+    }
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,38 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+    res.json({
+        message: "User created successfully"
+    })
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course
+        .find()
+        .then(courses => {
+            res.json({ courses })
+        })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const getUser = await User.findOne(req.user).populate('purchasedCourses');
+    res.status(200).json({purchasedCourses:getUser.purchasedCourses})
 });
 
 module.exports = router
