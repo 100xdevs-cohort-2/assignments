6c789a2a02423e60e1fcd8850f07fb06791ef411
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-22 20:22:11 +0530

    week 3 mongo

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-const PORT = 3000;
+const PORT = 3012;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
