c9fda4fdaaa0e059f78c4b1222b5c08081d9d722
Saksham <142968746+sam-the-dev@users.noreply.github.com>
2023-12-23 01:13:17 +0530

    Update 02-ratelimitter.js

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+const limitRequests = function (req, res, next) {
+  const id = req.headers["user-id"];
+
+  if (!numberOfRequestsForUser[id]) numberOfRequestsForUser[id] = 0;
+
+  if (numberOfRequestsForUser[id] > 5) res.status(404).send();
+
+  numberOfRequestsForUser[id]++;
+
+  console.log(numberOfRequestsForUser);
+
+  next();
+};
+
+app.use(limitRequests);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
