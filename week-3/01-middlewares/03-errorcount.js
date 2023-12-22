c40db54cbc961e180665095f781618456c4e0d90
varunsp-163 <spvarun47@gmail.com>
2023-12-22 12:01:06 +0530

    Hals done week-3

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,22 +1,24 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
 let errorCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+// Middleware to handle errors globally
+app.use((err, req, res, next) => {
+  errorCount++; // Increment error count for every error
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+  // Respond with a 404 status code for any error
+  res.status(404).json({ error: 'Not Found' });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get('/user', function(req, res, next) {
+  try {
+    throw new Error('User not found');
+  } catch (err) {
+    // Pass the error to the error handling middleware
+    next(err);
+  }
+  res.status(200).json({ name: 'john' }); // This won't execute due to the previous error
 });
 
 app.get('/errorCount', function(req, res) {
