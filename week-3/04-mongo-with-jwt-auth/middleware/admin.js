062975c7049b59a2963d0825ac5a11e15e729d55
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2023-12-22 19:22:38 +0530

    Complete week-3 04-mongo-with-jwt assignment.

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,25 @@
-// Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+const jwt = require("jsonwebtoken");
+
+const jwtPassword = process.env.JWT_PASSWORD;
+
+/**
+ * Middleware function to verify admin authentication and access control.
+ * This middleware checks if the user making the request is an admin.
+ * If the user is an admin, it allows access to the next middleware or route handler.
+ * If not, it returns a response with 401 status code.
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ * @returns {Promise<void>}
+ */
+async function adminMiddleware(req, res, next) {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  try {
+    jwt.verify(token, jwtPassword);
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
 }
 
 module.exports = adminMiddleware;
