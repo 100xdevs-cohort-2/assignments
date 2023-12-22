ed9e616cc831b4d42d966af5880afb41825a29a3
Satyam Maurya <msatyam120@gmail.com>
2023-12-23 00:37:03 +0530

    request count solved

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -9,7 +9,12 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+function CountofRequest(req,res,next){
+  ++requestCount;
+  next();
+}
 
+app.use(CountofRequest);
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
