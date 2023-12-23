c4f59084e4996755adb0b5623c873b5b78dd1a4f
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-12-23 22:46:33 +0530

    solved mongo assignment
    had to promisify all db calls. good learning lesson

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
-
+mongoose.connect('mongodb://localhost:27017/mongo-intro');
+console.log("Connected to Mongo DB")
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-});
-
-const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username : String,
+    password : String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    id : Number,
+    title : String,
+    description : String,
+    price : Number,
+    imageLink : String,
+    published : Boolean
 });
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username : String,
+    password : String,
+    purchasedCourses : [CourseSchema]
+});
+
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
