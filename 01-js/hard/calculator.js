f49d7e38ccb569a311df9822d9da9e1e242d2984
UtkarshDeoli <utkarsh.deoli@gmail.com>
2023-12-23 18:22:05 +0530

    did all lol

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,45 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  result;
+  constructor()
+  {
+    this.result=0;
+  }
+
+  add(x)
+  {
+    this.result += x;
+  }
+  subtract(x)
+  {
+    this.result -=x;
+  }
+  multiply(x)
+  {
+    this.result *= x;
+  }
+  divide(x)
+  {
+    if(x!=0)
+      this.result /= x;
+    else
+      throw new Error("divide by zero");
+  }
+
+  clear(){this.result =0;}
+
+  getResult(){return this.result}
+
+  calculate(expression) {
+    if (eval(expression) == Infinity) {
+      throw new Error("Cannot divide by zero");
+    } else {
+      return (this.result = eval(expression));
+    }
+  }
+
+}
 
 module.exports = Calculator;
