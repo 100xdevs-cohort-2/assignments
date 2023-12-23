2a15e991d4236ac1c636c5cc78eda2c33908f08a
tanishq <tanishqarya24@gmail.com>
2023-12-23 18:25:47 +0530

    week-3/02-jwt, 03-mongo solved

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,15 @@
+const { User } = require("../db");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    User.find({username: req.headers.username, password:req.headers.password})
+        .then(function (user) {
+            next();
+        })
+        .catch(function (err) {
+            res.sendStatus(403)
+        })
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,20 +1,48 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-app.post('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    user.save();
+    res.status(201).json({message: 'user created successfully'})
 });
 
-app.get('/courses', (req, res) => {
+router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find({})
+        .then(function (courses) {
+            res.json({courses: courses})
+        })
 });
 
-app.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    Course.find({courseId: req.params.courseId})
+        .then(function (newCourse) {
+            console.log("course => " + JSON.stringify(newCourse[0]))
+            User.findOneAndUpdate({username: req.headers.username}, {$push : {purchasedCourses: newCourse[0]}}, {new:true})
+            .then(function (updatedDocument){
+                console.log("course purchased")
+                res.sendStatus(200)
+            })
+            .catch()
+        })
 });
 
-app.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
+    User.findOne({username: req.headers.username})
+        .then(function (user){
+            console.log("user " + user + ", type=" + typeof(user))
+            res.json({purchasedCourses: user["purchasedCourses"]})
+        })
 });
+
+module.exports = router;
