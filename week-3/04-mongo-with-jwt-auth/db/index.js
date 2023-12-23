e746ddec69cbf6272d1212e0b9ea553cf2004763
VijeshRBhat <vijeshrbhat@gmail.com>
2023-12-24 01:23:47 +0530

    week3/ all completed

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,35 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+const connectToMongo = async ()=>{
+    try{
+        await mongoose.connect('<MONGO URL GOES HERE>')
+        console.log("Connected to MongoDB successfully")
+    } catch (e) {
+        console.log("Some error connecting to MongoDB")
+    }
+}
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: {
+        type: mongoose.Types.ObjectId,
+        ref: "Course"
+    }
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    img: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
