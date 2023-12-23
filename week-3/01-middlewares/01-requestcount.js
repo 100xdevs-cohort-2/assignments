8967ab6be8bc2a25da069768d67e0f4694ffadb4
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-12-23 14:28:08 +0530

    added solutions for middleware, jwt

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -9,6 +9,11 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+function requestCounter(req, res, next) {
+    requestCount++;
+    next();
+}
+app.use(requestCounter);
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -10,6 +10,11 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
+function errorHandler(err, req, res,next) {
+    errorCount++;
+    res.status(404).json({msg : "Error Detected"});
+}
+
 app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
@@ -22,5 +27,7 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(errorHandler);
 
 module.exports = app;
