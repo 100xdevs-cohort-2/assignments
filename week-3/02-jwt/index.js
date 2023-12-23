690c65315ab1b731fe425ec324aaf511878b1e33
Shivam Vishwakarma <shivamkumar5026@gmail.com>
2023-12-24 00:02:43 +0530

    completed upto jwt

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const z = require("zod");
 
 
 /**
@@ -13,8 +14,19 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = z.string().email();
+const passwordSchema = z.string().min(6);
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
+    return signature
 }
 
 /**
@@ -26,7 +38,13 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    try{
+        jwt.verify(token,jwtPassword);
+        return true;
+    }catch(e){
+        
+    }
+    return false;
 }
 
 /**
@@ -37,7 +55,13 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const decoded = jwt.decode(token);
+    if(decoded){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
