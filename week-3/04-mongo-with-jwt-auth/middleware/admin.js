3070efe5983d6f680d8db34e8201e137b26f4d74
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-23 19:45:05 +0530

    added 04 mongo with auth solutions

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 async function adminMiddleware(req, res, next) {
   // Implement admin auth logic
   // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req?.headers?.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).send({ message: "Unauthorized" });
   }
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,25 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const jwt = require("jsonwebtoken")
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const token = req?.headers?.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  try {
+    jwt.verify(token, process.env.JWT_KEY, (err, data) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+      req._id = data._id;
+      next();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error verying token", error: error });
+  }
 }
 
 module.exports = userMiddleware;
