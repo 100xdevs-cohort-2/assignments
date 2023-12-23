88a739e7b6f7b8e788178d57385cd0eda074bb78
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2023-12-23 17:20:46 +0530

    added 03-mongo solution

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,27 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose
+  .connect(
+    "mongodb+srv://codesoniyt:Mbe1U7pZYhmPz3du@cluster0.jprgeac.mongodb.net/cohort-assignment"
+  )
+  .then((data) => {
+    console.log("connected to MongoDB");
+  });
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  myCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  title: String,
+  description: String,
+  price: Number,
+  image: String,
+  owner: String,
+  published: { type: Boolean, default: false },
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+async function adminMiddleware(req, res, next) {
+  const { username, password } = req.headers;
+  if (!username || !password) {
+    return res.status(400).send({ message: "Invalid username or password" });
+  }
+  const admin = await Admin.findOne({ username: username, password: password });
+  if (!admin) {
+    return res.status(400).send({ message: "Admin not found" });
+  } else {
+    req.admin = admin;
+    next();
+  }
 }
 
 module.exports = adminMiddleware;
