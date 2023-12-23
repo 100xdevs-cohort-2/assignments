2abcf6d46e66e21daa60c9bf658d0e416ef9c6ad
shivam9536 <shivambhardwaj3124@gmail.com>
2023-12-23 19:49:13 +0530

    provlem solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,21 @@
 
 function isAnagram(str1, str2) {
 
+ const sortedStr1 = str1.split("").sort().join("").toLocaleLowerCase(); 
+ const sortedStr2 = str2.split("").sort().join("").toLocaleLowerCase();
+
+ const n1 = sortedStr1.length;
+ const n2 = sortedStr2.length;
+ if(n1!=n2){
+  return false;
+ }
+
+ for(let i=0;i<n1;i++){
+  if(sortedStr1[i]!=sortedStr2[i]){
+    return false;
+  }
+ }
+ return true;
 }
 
 module.exports = isAnagram;
