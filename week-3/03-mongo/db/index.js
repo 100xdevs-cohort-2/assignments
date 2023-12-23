52c711d185334e9a47b36abf690ed6a83aaa947e
Narottam Narhari <narottamishra67@gmail.com>
2023-12-24 00:23:25 +0530

    solved 03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("mongodb+srv://narottamishra67:2L8CPhISitEUl63e@cluster0.wwhjhpq.mongodb.net/edtech-app");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    publishedCourses: [Number]
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [Number]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    id: Number,
+    title: String,
+    description: String,
+    price : Number,
+    imageLink : String,
+    published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
