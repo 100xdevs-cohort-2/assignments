a8c14ad93389530e4be51bc3f02658131cb4c4e7
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2023-12-23 22:46:33 +0530

    solved mongo assignment
    had to promisify all db calls. good learning lesson

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,20 @@
-function userMiddleware(req, res, next) {
+const { User } = require('../db/index')
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
-}
+    let username =  req.headers.username;
+    let password =  req.headers.password;
 
+    try {
+        let user = await User.findOne({username: username, password: password});
+        if(!user) {
+            res.status(404).json({ message : "User Not Found"});
+            return;
+        }
+        next();
+    } catch(err) {
+        res.status(500).json({ message : "Internal Server Error - User middleware"});
+    }
+}
 module.exports = userMiddleware;
