25713b77b41ba6e5b0ed6c5f0a0456f4e2ed3c7b
VijeshRBhat <vijeshrbhat@gmail.com>
2023-12-23 18:52:45 +0530

    03 mongo completed

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,39 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+const connectTomongo = async ()=>{
+    try{
+        await mongoose.connect('<MONGO URL GOES HERE>');
+        console.log("Connected to MongoDB");
+    } catch (e) {
+        console.log("Error connecting to MongoDB")
+    }
+}
+
+connectTomongo();
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: 
+    {
+        type: mongoose.Types.ObjectId,
+        ref: 'Course'
+    }
+
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    img: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
+const {Admin} = require("../db/index")
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {a
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const {username, password} = JSON.parse(req.headers);
+    const admin = await Admin.findOne({username, password});
+    if(!admin) {
+        res.json({msg: "username or password is not correct"});
+        return;
+    }
+
+    next();
 }
 
 module.exports = adminMiddleware;
