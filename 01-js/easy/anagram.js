ba76e214458c3de7e2f1271e31150e21810144af
Nikhil Kumar <nikhilkumar84157@gmail.com>
2023-12-24 12:23:11 +0530

    done with 01-js easy assignment
    completed easy assignemt 1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,25 @@
 */
 
 function isAnagram(str1, str2) {
+  s1 = str1.toLowerCase();
+  s2 = str2.toLowerCase();
 
+
+  if(s1.length !== s2.length) {
+    return false;
+  }
+  let countFrq = {};
+  for(let char of s1) {
+    countFrq[char] = (countFrq[char] || 0) + 1;
+  }
+
+  for(let char of s2) {
+    if(!countFrq[char]) {
+      return false;
+    }
+    countFrq[char]--;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
