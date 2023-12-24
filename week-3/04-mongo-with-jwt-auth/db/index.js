ce21ce6c1af7118ae38f7b138474e2053e568b58
EarthIsHeaven <thinleylama44@gmail.com>
2023-12-24 09:31:31 +0530

    04-mongo-jwt admin done.

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(`mongodb+srv://thinley:${process.env.password}@cluster0.edn0jaw.mongodb.net/03-mongo-jwt`);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,18 @@
 // Middleware for handling auth
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    let token = req.headers.authorization;
+    try{
+        jwt.verify(token, process.env.jwtPassword);
+        next();
+      } catch (err){
+        res.status(404).json({
+            message: "Invalid token"
+        })
+      }
 }
 
 module.exports = adminMiddleware;
