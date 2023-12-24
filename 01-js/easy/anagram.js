6bfaf2e31e57aab458c07b56115a37ea392c6327
Rohit Bhati <rohitbhati8833@gmail.com>
2023-12-24 20:25:29 +0530

    Completed week 01 assignments

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,32 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let n = str1.length;
+  let m = str2.length;
+  if(n!=m)return false;
+  let count = {};
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  // console.log(str1, str2)
+  for(let i=0;i<n;i++){
+    count[str1[i]] = (count[str1[i]] || 0) + 1;
+  }
+  // for(let key in count){
+  //   console.log(key, count[key])
+  // }
+  for(let i=0;i<m;i++){
+    count[str2[i]] = (count[str2[i]] || 0) - 1;
+  }
+  // for(let key in count){
+  //   console.log(key, count[key])
+  // }
+  for(let key in count){
+    if(count[key] != 0){
+      return false;
+    }
+  }
+  return true;
 }
+// isAnagram('Debit Card', 'Bad Credit')
 
 module.exports = isAnagram;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let largest = numbers[0];
+    for(let i=0;i<numbers.length;i++){
+        if(largest < numbers[i]){
+            largest = numbers[i];
+        }
+    }
+    return largest;
 }
 
 module.exports = findLargestElement;
