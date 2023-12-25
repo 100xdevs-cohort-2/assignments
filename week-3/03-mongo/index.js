f5fa27ddfaafd85bd2cc087ecb720da1f8e93d86
Ranjit Yadav <yadavranjit521@gmail.com>
2023-12-25 17:19:05 +0530

    added admin module

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const adminRouter = require("./routes/admin")
+const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 // Middleware for parsing request bodies
-app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+); // to support URL-encoded bodies
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
