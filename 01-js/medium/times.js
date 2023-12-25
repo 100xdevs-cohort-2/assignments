d2770735e844035f916e7d412420ffd802a013e6
ishita067btmae21@igdtuw.ac.in <104856237+ishitarakchhit@users.noreply.github.com>
2023-12-25 16:05:04 +0530

    adding week 1 assignments

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let a = getTime();
+    let sum=0;
+    for(let i=1; i<=n; i++){
+        sum= sum+ i;
+    }
+    let b  = getTime();
+    console.log(b);
+    console.log(a);
+    console.log(b-a);
+    return b-a;
 }
+calculateTime(100);
