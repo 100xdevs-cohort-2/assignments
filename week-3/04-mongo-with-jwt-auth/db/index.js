2527be83c4ee0857d6d714fd118c724399155b74
yerva pavan <yervapavan16@gmail.com>
2023-12-25 17:21:51 +0530

    completed mongodb with auth and jwt

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://127.0.0.1:27017/jwt');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String,
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String,
+    purchasedcourses:Array,
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    id:Number,
+    title:String,
+    description:String,
+    price:Number,
+    imageLink:String,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,17 @@
+const jwt=require("jsonwebtoken");
+const jwtSecret="ynscuywuiqhdsnwer";
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token=req.headers["authorization"].split(" ")[1];
+    try{
+        const decoded=jwt.verify(token,jwtSecret);
+        req.headers['username']=decoded.username;
+    }
+    catch(err){
+        res.status(401).send(err.message);
+    }
+    next();
 }
 
 module.exports = userMiddleware;
