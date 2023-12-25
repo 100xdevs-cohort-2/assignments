3a012ba8532c9b7541611d9c7f12a89b3f24fb92
Krishna Kant Verma <55027576+krishna6431@users.noreply.github.com>
2023-12-25 10:42:41 +0000

    week3-01-middleware solution done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,5 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 const app = express();
 // You have been given an express server which has a few endpoints.
@@ -13,14 +11,30 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+app.use((req, res, next) => {
+  const user = req.header["user-id"];
+  if (numberOfRequestsForUser[user]) {
+    numberOfRequestsForUser[user] = numberOfRequestsForUser[user] + 1;
+    if (numberOfRequestsForUser[user] > 5) {
+      res.status(404).send("No Entry Allowed Vro")
+    }
+    else {
+      next();
+    }
+  }
+  else {
+    numberOfRequestsForUser[user] = 1;
+    next();
+  }
+})
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
