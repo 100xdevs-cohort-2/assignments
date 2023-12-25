9d8af08a6751026fdbe2bc696e6de24babf85796
Krishna Kant Verma <55027576+krishna6431@users.noreply.github.com>
2023-12-25 15:13:46 +0000

    week 3 assignment done

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,18 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const { User } = require("../db/index")
+async function userMiddleware(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = await User.findOne({
+        username: username,
+        password: password
+    })
+    if (user) {
+        res.json({
+            msg: "user alredy exist",
+        });
+    }
+    else {
+        next();
+    }
 }
-
 module.exports = userMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -5,22 +5,84 @@ const userMiddleware = require("../middleware/user");
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    let username = req.body.username;
+    let password = req.body.password;
+    User.create({
+        username: username,
+        password: password,
+    });
+    res.json({
+        msg: "User created successfully",
+    });
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    let username = req.body.username;
+    let password = req.body.password;
+    let token = jwt.sign(
+        {
+            username: username,
+            password: password,
+        },
+        jwtPassword
+    );
+    res.json({
+        token: token,
+    });
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    let token = req.headers.authorization;
+    try {
+        let decoded = jwt.verify(token, jwtPassword);
+        Course.find().then((courses) => {
+            res.json(courses);
+        });
+    } catch (err) {
+        res.json({
+            msg: "Athurization err",
+        });
+    }
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    let token = req.headers.authorization;
+    try {
+        let decoded = jwt.verify(token, jwtPassword);
+        let courseId = req.params.courseId;
+        Course.findOneAndUpdate(
+            {
+                courseId: courseId,
+            },
+            {
+                purchased: true,
+            }
+        );
+    } catch (err) {
+        res.json({
+            msg: "Athorization err",
+        });
+    }
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
+    let token = req.headers.authorization;
+    try {
+        let decoded = jwt.verify(token, jwtPassword);
+        Course.find()
+            .$where({ purchased: true })
+            .then((purchases) => {
+                res.json(purchases);
+            });
+    } catch (err) {
+        res.json({
+            msg: "Athorization err",
+        });
+    }
 });
 
 module.exports = router
