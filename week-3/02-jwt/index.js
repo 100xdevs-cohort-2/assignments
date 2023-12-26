cca39fd29d8ceed1976e22a6a64dbf4a66fa7728
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-12-26 17:55:36 +0530

    dome

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,21 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
 
 function signJwt(username, password) {
+  const payload = { username, password };
+  const options = { expiresIn: "1h" };
 
+  return jwt.sign(payload, jwtPassword, options);
 }
 
 function verifyJwt(token) {
-
+  try {
+    return jwt.verify(token, jwtPassword);
+  } catch (err) {
+    return null;
+  }
 }
 
 function decodeJwt(token) {
-
+  return jwt.decode(token, { complete: true });
 }
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
-}
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
