1ad08f2b101394d9eb7ed3be5dfdaafe242cb43d
Dushyant Goswami <goswami26dushyant@gmail.com>
2023-12-27 00:25:08 +0530

    solution

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,13 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+function requestsCount(req,res, next){
+  requestCount++;
+  next();
+}
+
+app.use(requestsCount);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
