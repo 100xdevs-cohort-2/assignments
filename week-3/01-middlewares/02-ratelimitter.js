f70daba7ee23c73289ed43f63815d699939a9228
hrithik.vibhu <hrithik.vibhu@gmail.com>
2023-12-26 15:35:12 +0530

    week 3 01-middlewares

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -13,14 +13,31 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+const myMiddleware = (req, res, next) => {
+  const userId = req.headers["user-id"];
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] += 1;
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("invalid");
+    }
+    else {
+      next();
+    }
+  }
+  else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+}
+app.use(myMiddleware);
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
