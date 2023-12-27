4d6cfbe222cc29f1f753fac8bda1cb1b832ae042
Kadiyala Siva Prasad <sivaprasad.ksp01@gmail.com>
2023-12-27 17:27:00 +0530

    solved the week-3 middleware ratelimitter

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req,res,next){
+  const userId = req.header["user-id"];
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId]>5){
+      res.status(404).send("no entry");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
@@ -25,3 +41,5 @@ app.post('/user', function(req, res) {
 });
 
 module.exports = app;
+
+app.listen(3000);
