e2da2d0db8812cd154acccbd6f4c4fb32eebcc57
hrithik.vibhu <hrithik.vibhu@gmail.com>
2023-12-27 19:50:32 +0530

    week 3 03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://root:1234@cluster0.kmmktxz.mongodb.net/user_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    courses: [[String]]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    image: String,
+    published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
