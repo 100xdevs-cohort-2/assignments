a2d5c8aa8c0c19c045cb7bc2e51cc3171cdd393b
Shivam29k <skumarshivam50@gmail.com>
2023-12-27 17:54:37 +0530

    middleware assignment completed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -12,9 +12,25 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
+
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+app.use((req, res, next)=>{
+  const userid = req.headers['user-id'];
+  if(!numberOfRequestsForUser[userid]){
+    numberOfRequestsForUser[userid] = 1;
+  } else {
+    numberOfRequestsForUser[userid]++;
+  }
+
+  if(numberOfRequestsForUser[userid]>5){
+    res.status(404).send()
+  } else {
+    next();
+  }
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
