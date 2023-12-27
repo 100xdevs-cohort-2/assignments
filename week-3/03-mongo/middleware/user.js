5d0faeb1e0553732427459d796fffa42025188f2
Yash <yashc14199@gmail.com>
2023-12-28 01:17:27 +0530

    week 3 done

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,20 @@
+const { User } = require("../db/index");
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const username = req.header.username;
+  const password = req.headers.password;
+  User.findOne({
+    username: username,
+    password: password,
+  }).then((value) => {
+    if (value) {
+      next();
+    } else {
+      res.status(403).send("user pta ni kya kr rha hai pr yha ni hai");
+    }
+  });
+
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,65 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const { User, Course } = require("../db/index");
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username,
+    password,
+  });
+  res.json({
+    msg: "User created successfully",
+  });
+  // Implement user signup logic
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", (req, res) => {
+  const response = Course.findOne({});
+  res.json({
+    courses: response,
+  });
+  // Implement listing all courses logic
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, (req, res) => {
+  // Implement course purchase logic
+  const courseId = req.params.courseId;
+  const username = req.headers.username;
+
+  User.updateOne(
+    {
+      username: username,
+    },
+    {
+      $push: {
+        purchasedCourses: courseId,
+      },
+    }
+  );
+  res.json({
+    message: "Purchase complete!",
+  });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = User.findOne({
+    username: req.headers.username,
+  });
+
+  console.log(user.purchasedCourses);
+  const courses = Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+
+  res.json({
+    courses: courses,
+  });
 });
 
-module.exports = router
+module.exports = router;
