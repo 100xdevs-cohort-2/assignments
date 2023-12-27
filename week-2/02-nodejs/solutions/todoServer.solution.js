d46958b013972fa6e818abeeca60fc1673f58934
ash k <ashg4m3r@gmail.com>
2023-12-27 19:56:40 +0530

    todo in mem

--- week-2/02-nodejs/solutions/todoServer.solution.js
+++ week-2/02-nodejs/solutions/todoServer.solution.js
@@ -32,62 +32,62 @@
     - For any other route not defined in the server return 404
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  let todos = [];
-  
-  app.get('/todos', (req, res) => {
-    res.json(todos);
-  });
-  
-  app.get('/todos/:id', (req, res) => {
-    const todo = todos.find(t => t.id === parseInt(req.params.id));
-    if (!todo) {
-      res.status(404).send();
-    } else {
-      res.json(todo);
-    }
-  });
-  
-  app.post('/todos', (req, res) => {
-    const newTodo = {
-      id: Math.floor(Math.random() * 1000000), // unique random id
-      title: req.body.title,
-      description: req.body.description
-    };
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
-  });
-  
-  app.put('/todos/:id', (req, res) => {
-    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
-    if (todoIndex === -1) {
-      res.status(404).send();
-    } else {
-      todos[todoIndex].title = req.body.title;
-      todos[todoIndex].description = req.body.description;
-      res.json(todos[todoIndex]);
-    }
-  });
-  
-  app.delete('/todos/:id', (req, res) => {
-    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
-    if (todoIndex === -1) {
-      res.status(404).send();
-    } else {
-      todos.splice(todoIndex, 1);
-      res.status(200).send();
-    }
-  });
-  
-  // for all other routes, return 404
-  app.use((req, res, next) => {
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) {
     res.status(404).send();
-  });
-  
-  module.exports = app;
+  } else {
+    res.json(todo);
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000), // unique random id
+    title: req.body.title,
+    description: req.body.description,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.json(todos[todoIndex]);
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+module.exports = app;
--- week-2/02-nodejs/todoServer copy.js
+++ week-2/02-nodejs/todoServer copy.js
@@ -41,52 +41,7 @@
 */
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 app.use(bodyParser.json());
-let todos = [];
-app.get("/todos", (req, res) => {
-  res.json(todos);
-});
-app.get("/todos/:id", (req, res) => {
-  const todo = todos.find((ind) => ind.id === parseInt(req.params.id));
-  if (!todo) {
-    res.status(404).send();
-  } else {
-    res.json(todo);
-  }
-});
-app.post("/todos", (req, res) => {
-  const newTodo = {
-    id: Math.floor(Math.random() * 1000),
-    title: req.body.title,
-    description: req.body.description,
-  };
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
-});
-app.put("/todos/:id", (req, res) => {
-  const tindex = todos.findIndex((ind) => ind.id === parseInt(req.params.id));
-  if (tindex === -1) {
-    res.status(404).send();
-  } else {
-    todos[tindex].title = req.body.title;
-    todos[tindex].description = req.body.description;
-    res.json(todos[tindex]);
-  }
-});
-app.delete("/todos/:id", (req, res) => {
-  const tindex = todos.findIndex((ind) => ind.id === parseInt(req.params.id));
-  if (tindex === -1) {
-    res.status(404).send();
-  } else {
-    todos.splice(tindex, 1);
-    res.status(200).send();
-  }
-});
-
-app.use((req, res, next) => {
-  res.status(404).send("Route not found");
-});
 
 module.exports = app;
