02f31813bf6db3ad0373c01c274d4d01eef976d3
Mahendra Kolhe <mkolhe23@gmail.com>
2023-12-28 23:30:38 +0530

    mern assignment db and  middleware

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,13 @@
+const user = require('../db/index')
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers['username']
+    const existingUser = user.User.findOne({ username: username })
+    if (!existingUser) {
+        return res.status(404).send("No Entry")
+    }
+    next()
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,14 +1,31 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const db = require('../db/index')
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+    const existingAdmin = await db.Admin.findOne({ username: username })
+    if (existingAdmin) {
+        return res.status(400).send("Admin already exists")
+    }
+    const admin = new db.Admin({
+        username: username,
+        password: password
+    })
+    admin.save()
+    res.json({
+        "Msg": "Admin created successfully"
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    let courseId = 1
+
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
