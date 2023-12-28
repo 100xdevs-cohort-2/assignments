1d6b09111d167e4ddf8a73a2e107cc48c8826d74
karananeja <karananeja.r@gmail.com>
2023-12-28 21:02:16 +0530

    created the middleware for the admin and user route

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,20 @@
+const jwt = require('jsonwebtoken');
+const environment = require('../utils/constants');
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const accessToken = req.headers.authorization?.split(' ')[1];
+
+  try {
+    const decodedValue = jwt.verify(accessToken, environment.JWT_SECRET);
+    if (decodedValue.username) {
+      req.username = decodedValue.username;
+      next();
+    } else {
+      res.status(403).json({ message: 'The AccessToken provided is Invalid' });
+    }
+  } catch (error) {
+    res.status(403).json({ message: 'Incorrect inputs' });
+  }
 }
 
 module.exports = userMiddleware;
