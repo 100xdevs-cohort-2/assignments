15ef5fa3da1cfcc119fcaae2d641d442acb38021
Divakar-Chouhan <divakarchouhan251@gmail.com>
2023-12-28 11:02:15 +0530

    completed assignment week 3

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,25 @@
 // Middleware for handling auth
+const jwt= require('jsonwebtoken');
+const jwtPassword="1234";
+
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    
+    try{
+        let auth= req.headers.authorization;
+        let parts=auth.split(' ');
+        let token=parts[1];
+        // console.log(token);
+        const decoded=jwt.verify(token,jwtPassword);
+        req.username=decoded.username;
+        next();
+    }
+    catch(err){
+        res.status(403).json({message:"Invalid token"})
+    }
+
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,21 @@
+const jwt= require('jsonwebtoken');
+const jwtPassword="1234";
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    try{
+        let auth= req.headers.authorization;
+        let parts=auth.split(' ');
+        let token=parts[1];
+        // console.log(token);
+        const decoded=jwt.verify(token,jwtPassword);
+        req.username=decoded.username;
+        next();
+    }
+    catch(err){
+        res.status(403).json({message:"Invalid token"})
+    }xz 
 }
 
 module.exports = userMiddleware;
