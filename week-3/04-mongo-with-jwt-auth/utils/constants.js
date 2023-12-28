52df7cc1d5323b533f9644c2c90046d167cc3c23
karananeja <karananeja.r@gmail.com>
2023-12-28 20:59:35 +0530

    created the constants file for the app

---
+++ week-3/04-mongo-with-jwt-auth/utils/constants.js
@@ -0,0 +1,6 @@
+const environment = {
+  DB_URL: `mongodb+srv://admin:courseDBsellingsite@testcluster.ctctl9v.mongodb.net/siteInfo?retryWrites=true&w=majority`,
+  JWT_SECRET: '110d70357d827272418c7f6a318a3092',
+};
+
+module.exports = environment;
