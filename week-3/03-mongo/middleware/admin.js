dc383c1161fac6fc194a314ccf60b3551dcb08ed
karananeja <karananeja.r@gmail.com>
2023-12-28 17:52:27 +0530

    created the middleware for the admin and user route

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
-// Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+const { Admin } = require('../db');
+
+async function adminMiddleware(req, res, next) {
+  const { username, password } = req.headers;
+  if (!username || !password)
+    return res.status(400).json({ message: 'Invalid Credentials' });
+
+  try {
+    const resp = await Admin.findOne({ username, password });
+    resp !== null
+      ? next()
+      : res.status(403).json({ message: 'You do not have access' });
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,18 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const { User } = require('../db');
+
+async function userMiddleware(req, res, next) {
+  const { username, password } = req.headers;
+  if (!username || !password)
+    return res.status(400).json({ message: 'Invalid Credentials' });
+
+  try {
+    const resp = await User.findOne({ username, password });
+    resp !== null
+      ? next()
+      : res.status(403).json({ message: 'You do not have access' });
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 module.exports = userMiddleware;
