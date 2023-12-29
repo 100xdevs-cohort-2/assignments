4734904289035663376f5c2ca55c3e150e1ea93a
mcace007 <mrinalace@gmail.com>
2023-12-29 23:52:47 +0530

    Update: Mounted createNewCourse adminController on /courses route

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,17 +1,13 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
-const router = Router();
 const adminController = require('../controllers/adminController');
+const router = Router();
 
 // Admin Routes
 router.post('/signup', adminController.createNewAdmin);
 
-router.post('/courses', adminMiddleware, (req, res) => {
-  // Implement course creation logic
-});
+router.post('/courses', adminMiddleware, adminController.createNewCourse);
 
-router.get('/courses', adminMiddleware, (req, res) => {
-  // Implement fetching all courses logic
-});
+router.get('/courses', adminMiddleware, adminController.getAllCourse);
 
 module.exports = router;
