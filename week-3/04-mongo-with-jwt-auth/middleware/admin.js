38ace997a5bb4d2778d72f946d1f5beb15a0ae51
Hashir <hashir0601.be21@chitkara.edu.in>
2023-12-29 12:08:47 +0530

    completed week-3 full

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,24 @@
 // Middleware for handling auth
+
+const jwt = require("jsonwebtoken");
+const { jwtsecret } = require("../config");
+
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+  const token = req.headers.authorization;
+
+  const words = token.split(" ");
+  const jwtToken = words[1];
+
+  const decodedValue = jwt.verify(jwtToken, jwtsecret);
+
+  if (decodedValue.username) {
+    next();
+  } else {
+    res.status(404).json({ msg: "You are not authenticated" });
+  }
 }
 
 module.exports = adminMiddleware;
