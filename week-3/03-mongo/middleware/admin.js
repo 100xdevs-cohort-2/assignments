232cca3f180fcde7fe02d0e6b5ab4cab839188f3
mcace007 <mrinalace@gmail.com>
2023-12-30 00:45:03 +0530

    Fix

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -3,7 +3,7 @@ const { Admin } = require('../db/index');
 // Middleware for handling auth
 async function adminMiddleware(req, res, next) {
   try {
-    const admin = await Admin.find({ username: req.headers.username });
+    const admin = await Admin.findOne({ username: req.headers.username });
     if (!admin) {
       return res.status(404).json({
         status: 'fail',
