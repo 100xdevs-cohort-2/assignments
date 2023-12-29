cee47a8d35d1b4785b5da293229fd2c1ae848bd8
vrajpatel002222021@outlook.com <vrajpatel002222021@outlook.com>
2023-12-29 16:43:05 +0530

    read from file done and also tried with expensive operation and after 8 0's machine start taking delay in output

--- week-2/01-async-js/easy/3-read-from-file.js
+++ week-2/01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,17 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+const fs = require('fs');
+
+fs.readFile("a.txt","utf-8",(err,data)=>{
+    console.log('file data:\n'+data);
+},expensiveOperation())
+
+function expensiveOperation(){
+    let result = 0;
+    for (let i=0;i<1000000000;i++){
+        result = result+1;
+    }
+    console.log(result);
+    console.log("operation completed")
+}
