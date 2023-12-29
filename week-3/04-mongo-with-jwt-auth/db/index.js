480e6fb405c2ed48a5239d69888bc966c7764278
mcace007 <mrinalace@gmail.com>
2023-12-30 01:49:50 +0530

    Update: Added schema for User, Admin and Course

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,67 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://McACE007:MrinalBaba007mc@cluster0.0gbxyhp.mongodb.net/week3-03?retryWrites=true');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  username: {
+    type: String,
+    required: [true, 'A user must have a username'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    trim: true,
+    minlength: [8, 'Password must have greater than equal to 8 characters'],
+    maxlength: [16, 'Password must have less than equal to 16 characters']
+  }
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  username: {
+    type: String,
+    required: [true, 'A user must have a username'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    trim: true,
+    minlength: [8, 'Password must have greater than equal to 8 characters'],
+    maxlength: [16, 'Password must have less than equal to 16 characters'],
+  },
+  purchasedCourses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+  }]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  title: {
+    type: String,
+    required: [true, 'A course must have a title'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'A course must have a description'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A course must have a price'],
+    min: [0,]
+  },
+  imageLink: {
+    type: String,
+    required: [true, 'A course must have a cover image']
+  },
+  published: {
+    type: Boolean,
+    default: true,
+  }
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
@@ -21,7 +69,7 @@ const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
+  Admin,
+  User,
+  Course
 }
