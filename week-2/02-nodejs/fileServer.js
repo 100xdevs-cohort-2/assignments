522cc9378c334b9be6756bb7498b16402e89b667
Nithin <nithindevathi@gmail.com>
2023-12-29 11:31:31 +0530

    week-2

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,23 +17,28 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 
-app.get("/files", (req, res) => {
-  fs.readdir("./files", (err, files) => {
-    if (err) throw new Error("Error");
-    else {
-      res.status(200).json({ files });
-    }
-  });
-});
-app.get("/files/:fileName", (req, res) => {
-  const { fileName } = req.params;
-  fs.readFile(`./files/${fileName}`, "utf-8", (err, data) => {
+app.get("/files", function (req, res) {
+  fs.readdir(path.join(__dirname, "./files/"), (err, files) => {
     if (err) {
-      res.status(404).send("File not found");
-    } else {
-      res.status(200).send(data);
+      return res.status(500).json({ error: "Failed to retrieve files" });
     }
+    res.json(files);
   });
 });
+
+app.get("/file/:filename", function (req, res) {
+  const filepath = path.join(__dirname, "./files/", req.params.filename);
+
+  fs.readFile(filepath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
+
 app.listen(3000);
 module.exports = app;
