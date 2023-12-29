3f58fdded86754b1d0476303cec927b4c7d589eb
mcace007 <mrinalace@gmail.com>
2023-12-30 00:14:37 +0530

    Update: Mounted createNewUser userController on /user/signup route

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,21 @@
 const { Router } = require("express");
-const router = Router();
 const userMiddleware = require("../middleware/user");
+const userController = require('../controllers/userController');
+const router = Router();
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
-});
+router.post('/signup', userController.createNewUser);
 
 router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+  // Implement listing all courses logic
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+  // Implement course purchase logic
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+  // Implement fetching purchased courses logic
 });
 
 module.exports = router
