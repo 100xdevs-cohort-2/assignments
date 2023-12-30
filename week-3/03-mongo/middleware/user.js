9eb22705b3daafef2a648cc56eb7c113cbe19637
mayank negi <mayank.negi0@gmail.com>
2023-12-31 02:21:58 +0530

    mongo assignments

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,21 @@
+const { User } = require("../db");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const { username, password } = req.headers;
+  User.findOne({
+    username: username,
+    password: password,
+  }).then((value) => {
+    if (value) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "User doesnt exist",
+      });
+    }
+  });
 }
 
 module.exports = userMiddleware;
