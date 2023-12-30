ef92e05c7b71999a493f2b438c40c45816b6bfac
mayank negi <mayank.negi0@gmail.com>
2023-12-31 03:18:35 +0530

    mongo with jwt assignments

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,28 @@
 // Middleware for handling auth
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+  const tokenArray = token.split(" ");
+  const jwtToken = tokenArray[1];
+  try {
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    console.log(decodedValue);
+    if (decodedValue.username) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "You are not authenticated",
+      });
+    }
+  } catch (e) {
+    res.json({
+      msg: "Incorrect inputs",
+    });
+  }
 }
 
 module.exports = adminMiddleware;
