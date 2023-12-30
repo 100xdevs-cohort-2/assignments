fa67aa712110b0a1b77f57502af548108266747f
Ujjwal1998 <mail2ujjwal98@gmail.com>
2023-12-30 15:01:36 +0530

    week-2 async done

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,35 @@
  */
 
 function wait1(t) {
-
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, t * 1000);
+  });
 }
 
-function calculateTime(t1, t2, t3) {
-
+async function calculateTime(t1, t2, t3) {
+  let start = performance.now();
+  await Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
+  let end = performance.now();
+  // console.log(end, start, end - start);
+  return end - start;
 }
-
+calculateTime(1, 2, 3);
 module.exports = calculateTime;
