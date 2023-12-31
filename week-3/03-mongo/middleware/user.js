e708e5ab413645942a3e0ed61edc56ebca8675a0
Harsh Sharma <harshopes@gmail.com>
2023-12-31 08:32:10 +0530

    Code till now

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,21 @@
+const { User } = require("../db/index");
+// Implement user auth logic
+// You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const username = req.header.username;
+  const password = req.header.password;
+  User.findOne({
+    username: username,
+    password: password,
+  }).then((value) => {
+    if (value) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "Admin doesn't exists",
+      });
+    }
+  });
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -3,20 +3,48 @@ const router = Router();
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  res.json({ message: "User created successfully" });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", (req, res) => {
+  // Implement listing all courses logic
+  res.json({
+    courses: [
+      {
+        id: 1,
+        title: "course title",
+        description: "course description",
+        price: 100,
+        imageLink: "https://linktoimage.com",
+        published: true,
+      },
+    ],
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, (req, res) => {
+  // Implement course purchase logic
+  res.json({ message: "Course purchased successfully" });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, (req, res) => {
+  // Implement fetching purchased courses logic
+  res.json({
+    purchasedCourses: [
+      {
+        id: 1,
+        title: "course title",
+        description: "course description",
+        price: 100,
+        imageLink: "https://linktoimage.com",
+        published: true,
+      },
+    ],
+  });
 });
 
-module.exports = router
+module.exports = router;
