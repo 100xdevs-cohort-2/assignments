10038470394d32f20dd48fc9a59c64f3f27fadf5
krishvsoni <sonikrish994@gmail.com>
2023-12-31 22:45:29 +0530

    commit

---
+++ NewYearApp/frontend/components/Todos.jsx
@@ -0,0 +1,12 @@
+export function Todos({todos}) {
+
+    return <div>
+        {todos.map(function(todo) {
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
+    </div>
+}
