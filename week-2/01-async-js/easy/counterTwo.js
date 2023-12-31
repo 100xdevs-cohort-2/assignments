3c94d9c1491616adeee175cea015cd76a10d6603
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2023-12-31 23:57:41 +0530

    Changed

--- week-2/01-async-js/easy/counterTwo.js
+++ week-2/01-async-js/easy/counterTwo.js
@@ -2,14 +2,14 @@
 // ## Counter without setInterval
 
 // Without using setInterval, try to code a counter in Javascript. 
-
+let inc = 1; 
 const counter = function(n)
 {
     for(let i = 1; i<=n;i++)
     {
             setTimeout(function()
             {
-                console.log(i)
+                console.log(inc++); 
             },i*1000); 
     }
 }
