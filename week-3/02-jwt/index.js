8a34991f9ba295b9dd6bb1581a4be2786664d4bc
ARIF MULANI <142739281+CoderInTheNorth-049@users.noreply.github.com>
2023-12-31 21:31:12 +0530

    Update index.js

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -33,8 +33,8 @@ function verifyJwt(token) {
  * Decodes a JWT to reveal its payload without verifying its authenticity.
  *
  * @param {string} token - The JWT string to decode.
- * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
- *                         Returns false if the token is not a valid JWT format.
+ * @returns {boolean} True if the token is a valid JWT format.
+ *                    Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
     // Your code here
