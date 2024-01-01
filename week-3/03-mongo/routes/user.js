01ebadac5d7612673b27f0ce67b16606aba923e1
knilesh9 <neutron_stays@protonmail.com>
2024-01-01 22:45:37 +0530

    fix: Correct typo in README.md Changed 'users' to 'user' for improved clarity, in ### User routes section. Since, index.js used 'app.use('/user', userRouter)'

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,14 +1,42 @@
 const { Router } = require("express");
-const router = Router();
 const userMiddleware = require("../middleware/user");
+const router = Router();
+
+const { User } = require("../db/")
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
+    console.log(req.headers)
     // Implement user signup logic
+    let username = req.headers.username
+    let password = req.headers.password
+
+    try{
+        let isUser = await User.create({
+            username: username,
+            // email: email,
+            password: password
+        })
+        if(isUser){
+            res.status(200).json({
+                Msg: "User created successfully"
+            })
+        }else{
+            res.status(404).json({
+                Error: "Unable to create user."
+            })
+        }
+    }catch (err){
+        console.log(err)
+    }
+
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    res.send(200).json({
+        OK: "All ok here"
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
