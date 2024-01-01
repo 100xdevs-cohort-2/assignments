f5da96ea183844634ea7c22377a37b310a9aa450
Yash Ginoya <yashginoya2003@gmail.com>
2024-01-01 16:07:10 +0530

    asd

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,18 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req,res,next) => {
+  const userid = req.header.user-id;
+  if(numberOfRequestsForUser.hasOwnProperty(userid)){
+    if(numberOfRequestsForUser.userid > 5) return res.status(404).send();
+    else numberOfRequestsForUser.userid++;
+  }
+  else{
+    numberOfRequestsForUser.userid = 1;
+  }
+  next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
