511c720114d13959f927354ba08dfe7020079f8f
Aditya <devchakkeaditya37@gmail.com>
2024-01-02 20:32:59 +0530

    01_async-js Done

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,18 +6,43 @@
  */
 
 function wait1(t) {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
+
 
 }
 
 function wait2(t) {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
 
 }
 
 function wait3(t) {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
 
 }
 
 function calculateTime(t1, t2, t3) {
+    const start = Date.now();
+    return wait1(t1).then(()=>{
+        return wait2(t2).then(()=>{
+            return wait3(t3).then(()=>{
+                const duration = Date.now()-start;
+                return duration;
+            })
+        })
+    })
 
 }
 
