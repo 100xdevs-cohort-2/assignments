75d5639dfcb7420b0293db91177ac3a74657f21f
Devananth <devananthnkl@gmail.com>
2024-01-02 09:34:48 +0530

    done:completed hard qns

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,6 +11,41 @@
 */
 
 class Todo {
+  constructor(){
+    this.allTodos = [];
+  }
+
+  add(todo){
+    this.allTodos.push(todo);
+  }
+
+  remove(indexOfTodo){
+    if(indexOfTodo>=0 && indexOfTodo<this.allTodos.length){
+      this.allTodos = this.allTodos.slice(0 , indexOfTodo).concat(this.allTodos.slice(indexOfTodo+1));
+    }
+  }
+
+  update(index , updatedTodo){
+    if(index >= 0 && index<this.allTodos.length){
+      this.allTodos[index] = updatedTodo;
+    }
+  }
+
+  getAll(){
+    return this.allTodos;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo >=0 && indexOfTodo<this.allTodos.length){
+      return this.allTodos[indexOfTodo];
+    } else {
+      return null;
+    }
+  }
+
+  clear(){
+    this.allTodos = [];
+  }
 
 }
 
