2d8a6374d6ada59d98246bcc04917476ab86d31d
Yash Ginoya <yashginoya2003@gmail.com>
2024-01-03 03:53:10 +0530

    week 3 mongo done

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,41 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
-
+async function connectDB(){
+   try{
+        await mongoose.connect('mongodb+srv://yashginoya_:yashginoya2402@cluster0.gpdycqd.mongodb.net/CoursesDB');
+        // await mongoose.connect('mongodb://localhost:27017/abcd');
+        console.log("connected to DB");
+    }
+    catch(e)
+    {
+        console.log("there is some errro connecting to the DB")
+    }
+}
+connectDB();
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String,
+    purchasedCourse : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : String,
+    description : String,
+    imageLink : String,
+    price : Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,76 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.create({
+        username,
+        password
+    })
+    res.json({
+        msg : 'User created successfully'
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const response = await Course.find();
+    res.json({
+        courses : response
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+    try{
+        const courses = await Course.find({ _id : courseId});
+    
+        if(courses.length == 0){
+            res.status(404).json({msg : 'send valid course ID'})
+        }
+        else{ 
+            try{
+                await User.updateOne({
+                    username : username
+                },{
+                    "$push" :{
+                        purchasedCourse : courseId
+                    }
+                });
+                res.json({msg : "Purchase Complete"});
+            }
+            catch(e){
+                // console.log(e);
+                res.json({msg : "there is some error"});
+            }
+        }
+    }catch(e){
+        console.log("error in finding course")
+        res.json({msg : "error in finding course ID"})
+    }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    try{
+        const courseIds = await User.findOne({username : req.headers.username} , {purchasedCourse : 1 , _id : 0})
+        // console.log("hello");
+        const courses = await Course.find({_id : { "$in" : courseIds.purchasedCourse}}) // use courseIds[0].purchasedCourse if earlier use find function to find courseIds
+        res.json({
+            purchasedCourses : courses
+        })
+    }
+    catch(e){
+        res.json("there is some error here");
+    }
 });
 
 module.exports = router
