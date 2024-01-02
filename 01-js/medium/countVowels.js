d0ecdad42201fcc953177db13f143d367040dfce
Mohammed Gazi <51954077+mdgazi786@users.noreply.github.com>
2024-01-02 14:33:05 +0530

    completed countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (const char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
