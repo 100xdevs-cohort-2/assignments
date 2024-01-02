a74c0109b1dbd9a192d05487866a3b71676f2189
saif <mdsaifurrahman@navneettoptech.com>
2024-01-02 18:15:46 +0530

    mongo assignment comopleted

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,32 @@
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+const { Admin } = require("../db/index");
+
+async function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+  const { username } = req.headers;
+  let findAdmin = await Admin.findOne({ username });
+  if (findAdmin) {
+    next();
+  } else {
+    res.status(400).json({ message: `no user exist with ${username} email` });
+  }
 }
 
-module.exports = adminMiddleware;
+async function existingUserName(req, res, next) {
+  const { username } = req.body;
+  let findAdmin = await Admin.findOne({ username });
+  if (!findAdmin) {
+    next();
+  } else {
+    res
+      .status(400)
+      .json({
+        message: `username is already registered with  ${username} email`,
+      });
+  }
+}
+
+module.exports = { adminMiddleware, existingUserName };
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,43 @@
 const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+const { adminMiddleware, existingUserName } = require("../middleware/admin");
+const { Admin, Course } = require("../db/index");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", existingUserName, (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  let newAdmin = new Admin({ username, password });
+  newAdmin.save().then(() => {
+    res.json({ message: "Admin created successfully" });
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
+  const { title, description, price, imageLink } = req.body;
+  let createNewCourse = new Course({ title, description, price, imageLink ,published:true});
+  createNewCourse.save().then(() => {
+    res.json({
+      message: "Course created successfully",
+      courseId: createNewCourse._id,
+    });
+  });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  let allListedCourses = await Course.find();
+
+  if (allListedCourses.length > 0) {
+    res.status(200).json({
+      courses: allListedCourses,
+    });
+  } else {
+    res.status(200).json({
+      message: "There are no courses listed as of now",
+    });
+  }
 });
 
 module.exports = router;
