24d822425307dacdf1a47978c8d9e0d9c0d1033b
sidyr6002 <siddhardh6002@gmail.com>
2024-01-02 20:43:39 +0530

    Complete Week-2 Assignments

--- week-2/01-async-js/medium/1-file-cleaner.js
+++ week-2/01-async-js/medium/1-file-cleaner.js
@@ -24,17 +24,22 @@ const writeFile = (filePath, data) => {
     });
 }
 
+/**
+ * Cleans the file at the specified file path by removing any extra white spaces and writes the updated data back to the file.
+ *
+ * @param {string} filePath - The path of the file to be cleaned.
+ * @return {Promise<void>} - A promise that resolves once the file has been successfully cleaned.
+ */
 const cleanFile = async (filePath) => {
    try {
        const data = await readFile(filePath);
        const newData = data.replace(/\s+/g, ' ');
-       const fullData =  `oldData : ${data}\nnewData : ${newData}`;
+       console.log(`oldData : ${data}\nnewData : ${newData}`);
 
-       await writeFile(filePath, fullData);
-       console.log("File cleaned successfully");
+       await writeFile(filePath, newData);
    } catch (err) {
        console.error(err);
    }
 }
 
-cleanFile('./medium/file.txt');
+cleanFile('./medium/file.txt').then(() => console.log("File cleaned successfully"));
--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -3,9 +3,20 @@ let hours = curTime.getHours();
 let minutes = curTime.getMinutes();
 let seconds = curTime.getSeconds();
 
+function displayTime(hours, minutes, seconds) {
+    console.clear();
+    console.log(`Current time: ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+}
+
+/**
+ * Updates the current time every second and logs it to the console.
+ *
+ * @return {void} This function does not return a value.
+ */
 const clock = () => {
     setInterval(() => {
         seconds = seconds + 1;
+
         if (seconds === 60) {
             seconds = 0;
             minutes = minutes + 1;
@@ -14,13 +25,11 @@ const clock = () => {
             minutes = 0;
             hours = hours + 1;
         }
-
         if (hours === 24) {
             hours = 0;
         }
 
-        console.clear();
-        console.log(`Current time: ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+        displayTime(hours, minutes, seconds);
     }, 1000);
 }
 
