9ba173d48184e0fc645d4304ed7305654c188669
Raj04 <rajjoinsall@gmail.com>
2024-01-04 00:53:39 +0530

    week-3 schema

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,27 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("your-mongodb-url");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  name: { type: String, default: "Annonymous" },
+  email: { type: String, match: /[/\S+@\S+\.\S+/]/ },
+  password: { type: String },
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  name: { type: String, default: "Annonymous" },
+  age: { type: Number, min: 18, index: true },
+  bio: { type: String, match: /[a-zA-Z ]/ },
+  date: { type: Date, default: Date.now },
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  department: { type: String },
+  category: { type: String },
+  subject: { type: String },
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB.
+  // Check readme for the exact headers to be expected
+  // Input: Headers: { 'username': 'username', 'password': 'password' },
+
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  //check mongoDB admin username/pwd
+  if (username === "admin" && password === "password") {
+    res.status(200).send("admin authenticated");
+    next();
+  }
 }
 
 module.exports = adminMiddleware;
