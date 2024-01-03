605f4b68c4c8b859e95e180117d1530a672a2508
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-03 19:14:00 +0530

    Week 2 NODE.js

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -44,6 +44,72 @@
   
   const app = express();
   
-  app.use(bodyParser.json());
+  app.use(bodyParser.json()); // body-parser is a middleware allowing Express to automatically parse the incoming request body as JSON. FOR FURTHER PROCESSING WITHING ROUTE HANDLER
   
+
+
+  let todos = [];
+
+  app.get('/todos', (req, res) => {
+      res.json(todos);
+  });
+  
+  
+  
+  app.get('/todos/:id', (req,res) => {
+      const todo = todos.find(t => t.id === parseInt(req.params.id));
+      if(!todo){
+          res.status(404).send();
+      }else {
+          res.json(todo);
+      }
+  });
+  
+  
+  app.post('/todos', (req,res) => {
+      const newTodo = {
+          id: Math.floor(Math.random() * 10000000), // unique random id
+          title: req.body.title,
+          description: req.body.description
+      };
+  
+      todos.push(newTodo);
+      res.status(201).json(newTodo);
+  });
+  
+  
+  app.put('/todos/:id', (req, res) => {
+      const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+      if(todoIndex === -1){
+          res.status(404).send();
+      }else{
+          todos[todoIndex].title = req.body.title;
+          todos[todoIndex].description = req.body.description;
+          res.json(todos[todoIndex]);
+      }
+  });
+  
+  
+  app.delete('/todos/:id', (req,res) => {
+      const todoIndex = todos.findIndex(t=> t.id === parseInt(req.params.id));
+  
+      if(todoIndex === -1){
+          res.status(404).send();
+      }else{
+          todos.splice(todoIndex , 1);
+          res.status(200).send();
+      }
+  });
+  
+  
+  // for all other routes, return 404
+  
+  app.use((req,res,next) => {
+      res.status(404).send();
+  })
+  
+  
+
+
+
   module.exports = app;
