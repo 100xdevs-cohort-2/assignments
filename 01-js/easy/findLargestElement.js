afe099477bd1f77944d096a2c5ba36e5589970ba
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-01-04 15:59:38 +0530

    Completed 01-js easy

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+
+    let largestElement = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > largestElement) {
+        largestElement = numbers[i];
+    }
 }
 
-module.exports = findLargestElement;
+return largestElement;
+}
+
+module.exports = findLargestElement;  
