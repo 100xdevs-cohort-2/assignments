ec5950fd3b62b08cd8a4f91f27521c987661f3df
Shenoy <shravanshenoy1998@hotmail.com>
2024-01-04 08:58:15 +0530

    W4 Assignment : Todo App using React

---
+++ week-4/react-basics/todo-app/src/App.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+
+function App(){
+  
+  let [todoList, setTodoList] = useState([{
+    "title":"Go to gym",
+    "description" : "Go to gym from 5 to 6pm"
+  }]);
+  
+  let titleRef = useRef();
+  let descRef = useRef();  
+
+  const clickHandler = (e)=>{
+      setTodoList([...todoList, {
+        "title":titleRef.current.value,
+        "description":descRef.current.value
+      }]);
+      //console.log(todoList);
+  }
+
+  return (
+    <>
+      <input type="text" placeholder="title" ref={titleRef}></input>
+      <br></br> <br></br>
+      <input type="text" placeholder="description" ref={descRef}></input>
+      <br></br> <br></br>
+      <button onClick={clickHandler}>Add todo item</button>
+
+      <div>
+        {todoList.map((ele, index) => (
+          <div>
+            <h4>Task {index + 1}</h4>
+            <p>Title: {ele.title}</p>
+            <p>Description: {ele.description}</p>
+          </div>
+          )
+          )
+        }
+      </div>
+    </>
+
+  );
+}
+
+export default App;
