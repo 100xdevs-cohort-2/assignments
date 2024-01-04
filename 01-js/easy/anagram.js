b37735656c56723e722a934cd96d75bcf49359dd
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2024-01-04 12:43:32 +0000

    added assignment1 solutions

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,9 +3,38 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
+// function sort(str){
+//   let array=str.split("");
+//   array=array.sort();
+//   let sortedStr=array.join("");
+//   return sortedStr;
+
+// }
+
+
+// function isAnagram(str1, str2) {
+
+//   let strA=str1.toLower();
+//   let strB=str2.toLower();
+
+//   if (sort(strA)==sort(strB)){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+
+// }  
+
 
 function isAnagram(str1, str2) {
+    return (
+      str1.toLowerCase().split("").sort().join("") ===
+      str2.toLowerCase().split("").sort().join("")
+    );
+  }
 
-}
+
+
 
 module.exports = isAnagram;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,51 @@
 */
 
 class Todo {
+  todos;
+  constructor(){
+    this.todos = new Array();
+  }
 
+  InvalidIndex(index){
+    if(index<0 || index >= this.todos.length) return true;
+
+    return false;
+  }
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo){
+    if(this.InvalidIndex(indexOfTodo)){
+      return;
+    }
+    this.todos.splice(indexOfTodo,1);
+
+  }
+
+  update(index,updatedTodo){
+    if(this.InvalidIndex(index)){
+      return;
+    }
+    this.todos[index] = updatedTodo;
+  }
+
+  getAll(){
+    return this.todos;
+  }
+
+  get(indexOfTodo){
+    if(this.InvalidIndex(indexOfTodo)){
+      return null;
+    }
+    return this.todos[indexOfTodo];
+
+
+  }
+
+  clear(){
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
