fa10e7b567b6d2fbc4a2e6f84ea73f67bc77a5c5
Sahil kumar <28SKOFFICIAL01@GMAIL.COM>
2024-01-04 17:09:55 +0530

    completed 04 -mongodb

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,26 @@
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = require("../config");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization; // bearer token
+    const words = token.split(" "); // ["Bearer", "token"]
+    const jwtToken = words[1]; // token
+    try {
+        const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+        if (decodedValue.username) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: "You are not authenticated"
+            })
+        }
+    } catch(e) {
+        res.json({
+            msg: "Incorrect inputs"
+        })
+    }
+    
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,21 @@
+const {JWT_SECRET} = require("../config");
+const jwt = require("jsonwebtoken");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+
+    if (decodedValue.username) {    
+        req.username = decodedValue.username;
+        req.randomData = "Adsadsadsadssd";
+        next();
+    } else {
+        res.status(403).json({
+            msg: "You are not authenticated"
+        })
+    }
 }
 
 module.exports = userMiddleware;
