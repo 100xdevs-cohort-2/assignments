d894a91eb3a1971b688a71f09eb9ce468cb8b040
Yash Ginoya <yashginoya2003@gmail.com>
2024-01-04 16:33:19 +0530

    Week 3 All Completed

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: "config.env"});
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const jwtToken = req.headers.authorization;
+    const words = jwtToken.split(' ');
+    if(words.length < 2) return res.json({msg : "invalid authorization"});
+    const token = words[1];
+    
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        if(decoded.username){
+            console.log("user verified successfully")
+            next();
+        }else{  
+            return res.json({msg : "invalid authorization"});
+        }
+    }
+    catch(e){
+        return res.json({msg : "invalid authorization"})
+    }
 }
 
 module.exports = userMiddleware;
