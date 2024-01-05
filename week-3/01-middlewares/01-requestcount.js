cdf343a3da2d1e549fd2d92ceed852f7a171793c
Ujjwal Talwar <mail2ujjwal98@gmail.com>
2024-01-05 09:57:07 +0530

    middlewares done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,25 +1,32 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
-
+const PORT = 3000;
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+function requestCounter(req, res, next) {
+  requestCount += 1;
+  next();
+}
+app.use(requestCounter);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
 });
 
+app.listen(PORT);
 module.exports = app;
--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -13,15 +13,34 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+function rateLimiterMiddleWare(req, res, next) {
+  const userName = req.headers["user-id"];
+  console.log(numberOfRequestsForUser[userName], req.headers);
+  if (numberOfRequestsForUser.hasOwnProperty(userName)) {
+    if (numberOfRequestsForUser[userName] <= 5) {
+      numberOfRequestsForUser[userName] += 1;
+      next();
+    } else {
+      res.status(404).send({ msg: "blocked" });
+    }
+  } else {
+    numberOfRequestsForUser[userName] = 1;
+    next();
+  }
+}
+
+app.use(rateLimiterMiddleWare);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
+app.listen(3000);
 module.exports = app;
