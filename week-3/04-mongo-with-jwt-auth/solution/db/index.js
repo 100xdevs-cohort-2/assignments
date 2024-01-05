132e825cb648e54c29228261893a809343b6e8be
Priyam More <morepriyam99@gmail.com>
2024-01-05 17:13:06 +0530

    Removed Sensitive Links for Sol.
    Use Your Own MongoDB url

--- week-3/04-mongo-with-jwt-auth/solution/db/index.js
+++ week-3/04-mongo-with-jwt-auth/solution/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app2');
+mongoose.connect('mongodb url');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
