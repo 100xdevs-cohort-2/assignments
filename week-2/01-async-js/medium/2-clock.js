7e38fc93ea2c2203bffa65a5ae1b336ccd52b83e
Ashish <ashishktiwari1203@gmail.com>
2024-01-06 13:13:08 +0530

    Done Hard Assgn 1

--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -7,16 +7,26 @@
 
 // - HH:MM::SS AM/PM (Eg 01:45:23 PM)
 
-var currtime = new Date();
+function currTime(){
+    var currtime = new Date();
 
-console.log(`Current Machine time: ${currtime.getHours()} : ${currtime.getMinutes()} : ${currtime.getSeconds()}`);
+    console.log(`Current Machine time: ${currtime.getHours()} : ${currtime.getMinutes()} : ${currtime.getSeconds()}`);
+}
 
 //For displaying in am and pm 
 
-var hr = currtime.getHours();
-var min = currtime.getMinutes();
-var sec = currtime.getSeconds();
+// var hr = currtime.getHours();
+// var min = currtime.getMinutes();
+// var sec = currtime.getSeconds();
 
-var samay = hr>=12 ? "PM" : "AM" ;
-hr = hr % 12;
-console.log(`${hr} : ${currtime.getMinutes()} : ${currtime.getSeconds()} ${samay}`)
+// var samay = hr>=12 ? "PM" : "AM" ;
+// hr = hr % 12;
+// console.log(`${hr} : ${currtime.getMinutes()} : ${currtime.getSeconds()} ${samay}`)
+
+//To update everysecond
+
+function update(t){
+    setInterval(currTime, t*1000);
+}
+
+update(1);
