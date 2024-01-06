922f15528db0024263807684fcb3c35c9e511f7e
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-07 01:20:58 +0530

    Week 3 03-mongo

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,25 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const { User } = require("../db");
+
+function userMiddleware(req, res, next){
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB.
+
+    const username = req.headers.username; // harkirat@gmail.com
+    const password = req.headers.password; // 123456
+
+    User.findOne({
+        username: username,
+        password: password
+    })
+    .then(function(value){
+        if(value){
+            next();
+        }else {
+            res.status(403).json({
+                msg: "User doesn't exist"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
