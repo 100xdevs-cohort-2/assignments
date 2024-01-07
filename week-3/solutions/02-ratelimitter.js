08d293f2abfe5c99b314c0109fab3fb55851de20
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-01-07 15:13:12 +0530

    Add rate limiting middleware to the express server

--- week-3/solutions/02-ratelimitter.js
+++ week-3/solutions/02-ratelimitter.js
@@ -1,6 +1,8 @@
-
+const request = require('supertest');
+const assert = require('assert');
 const express = require('express');
 const app = express();
+app.use(express.json())
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
@@ -10,27 +12,30 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
-let numberOfRequestsForUser = {};
+let numberOfRequestsForUser = {
+};
+
 setInterval(() => {
     numberOfRequestsForUser = {};
-}, 1000)
-// 
-app.use(function(req, res, next) {
-  const userId = req.headers["user-id"];
+    // console.log("Insideeee")
+}, 10000)
 
-  if (numberOfRequestsForUser[userId]) {
-    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
-    if (numberOfRequestsForUser[userId] > 5) {
-      res.status(404).send("no entry");
-    } else {
-      next();
+app.use(function(req, res, next){
+    const userid = req.headers.id
+    if(numberOfRequestsForUser[userid]){
+        if(numberOfRequestsForUser[userid]<5){
+            numberOfRequestsForUser[userid]++;
+        } else{
+            res.statusCode(404).json({
+                msg: "Cant sent more than 5 req per second."
+            })
+        }
+    } else{
+        numberOfRequestsForUser[userid] = 1;
     }
-  } else {
-    numberOfRequestsForUser[userId] = 1;
-    next();
-  }
+    console.log(numberOfRequestsForUser)
+    next()
 })
-
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -40,4 +45,13 @@ app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
-module.exports = app;
+app.use(function(err, req, res, next){
+    if(err){
+        console.log("Error occured")
+        res.json({
+            msg: "Error occureddddd"
+        })
+    }
+})
+
+app.listen(4000, ()=> console.log("Listening on Port 30000"))
