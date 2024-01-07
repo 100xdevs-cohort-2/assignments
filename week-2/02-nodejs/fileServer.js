73b71613e7f7735099df3e41acf3a128b9618327
TarunPereddi <tarun.pereddi.work@gmail.com>
2024-01-07 20:52:44 +0530

    update

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,30 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get("/files", (req, res)=>{
+  const filePath = path.join(__dirname, "files");
+  fs.readdir(filePath,(err,data)=>{
+    if(err) throw err;
+    res.status(200).json(data);
+  })
+})
+
+app.get("/files/:filename",(req,res)=>{
+  const fileName = req.params.filename;
+  const filePath = path.join(__dirname, "files", fileName);
+  fs.readFile(filePath,"utf-8",(err,data)=>{
+    if(err){
+      res.status(404).send("File not found");
+    }else{
+      res.status(200).send(data);
+    }
+  })
+})
+
+app.use((req,res)=>{
+  res.status(404).send("");
+})
+
+app.listen(3000);
 
 module.exports = app;
