fc7890a68a2e92ce9c052e88ae0007db2d2601b8
Harkirat Singh <harkirat96@gmail.com>
2024-01-09 02:21:15 +0530

    Added assignments

---
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useMemo } from 'react';
+// You have been given a list of items you shopped from the grocery store
+// You need to calculate the total amount of money you spent
+
+const Assignment3 = () => {
+    const [items, setItems] = useState([
+        { name: 'Chocolates', value: 10 },
+        { name: 'Chips', value: 20 },
+        { name: 'Onion', value: 30 },
+        { name: 'Tomato', value: 30 },
+        // Add more items as needed
+    ]);
+
+    // Your code starts here
+    const totalValue = 0;
+    // Your code ends here
+    return (
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                ))}
+            </ul>
+            <p>Total Value: {totalValue}</p>
+        </div>
+    );
+};
+
+export default Assignment3;
