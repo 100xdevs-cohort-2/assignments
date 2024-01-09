1d569a8737250a930caf32413816737be76c17c4
AtharvaMane23 <103356325+AtharvaMane23@users.noreply.github.com>
2024-01-09 22:54:38 +0530

    Fixed anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,18 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+function isAnagram(input1,input2){
+  const cleanInput1 = input1.replace(/\s/g, '').toLowerCase();
+  const cleanInput2 = input2.replace(/\s/g, '').toLowerCase();
+  //console.log(cleanInput1.length);
+  //console.log(cleanInput2.length);
+  if(cleanInput1.length !== cleanInput2.length){
+    return false;
+  }
+  const sortedInput1 = cleanInput1.split('').sort().join('');
+  const sortedInput2 = cleanInput2.split('').sort().join('');
+
+  return sortedInput1 === sortedInput2
 
 }
-
 module.exports = isAnagram;
