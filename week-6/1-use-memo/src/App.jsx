57770f8f5d2580e8c3105398df129f9da069895d
Ahd-Kabeer-Hadi <ahd.kabeerpi@gmail.com>
2024-01-10 20:51:38 +0530

    Fix - added solutions for 1 and 2 assignments from week 6

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
+import { Assignment1 } from './components/Assignment1'
+import { Assignment2 } from './components/Assignment2'
 import Assignment3 from './components/Assignment3'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <>
       <Assignment1 />
       {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment3 />
     </>
   )
 }
--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
@@ -6,13 +6,15 @@ export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
-
-    }
-
-    function handleDecrement() {
-        
-    }
+    const handleIncrement = useCallback(() => {
+        console.log("handleIncrement called");
+        setCount(count + 1);
+      }, [count]);
+    
+      const handleDecrement = useCallback(() => {
+        console.log("handleDecrement called");
+        setCount(count - 1);
+      }, [count]);
     // Your code ends here
 
     return (
