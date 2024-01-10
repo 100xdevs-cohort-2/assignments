3a88cd27d3c59f7ea5ffb16f4c8089f2caea54fa
Prakhar Pathak <prakharpathak9450@gmail.com>
2024-01-10 19:35:53 +0530

    Added anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
+  let newStr1 = str1.toLowerCase();
+  let newStr2 = str2.toLowerCase();
 
+  if (newStr1.split("").sort().join("") === newStr2.split("").sort().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
