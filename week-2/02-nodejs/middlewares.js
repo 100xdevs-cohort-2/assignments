518fc35959f9b96543ce614e36f84c14a8f7fb7a
kumarguddu609 <kumarguddu609@gmail.com>
2024-01-10 17:11:12 +0530

    updated better way to add middleware

--- week-2/02-nodejs/middlewares.js
+++ week-2/02-nodejs/middlewares.js
@@ -1,27 +1,39 @@
 const express = require("express");
 const app = express();
 
-app.get("/health-checkup", function (req, res) {
+//Middleware for user authentication
+function userMiddleware(req, res, next) {
   const username = req.headers.username;
   const password = req.headers.password;
-  const kidneyId = req.query.kidneyId;
-
-  // Check if the username and password are correct
-  if (!(username === "guddu" && password === "guddu")) {
-    res.status(401).send("Invalid username or password");
-    return;
-  }
-  // Check if the kidneyId is correct
-  if (kidneyId == 1 || kidneyId == 2) {
-    res.json({
-      msg: "Your Kidney is healthy",
-    });
+  if (username != "guddu" || password != "guddu") {
+    res.status(403).send("Invalid username or password");
   } else {
+    next();
+  }
+}
+
+//Middleware for kidney input validation
+function kidneyMiddleware(req, res, next) {
+  const kidneyId = req.query.kidneyId;
+  if (kidneyId != 1 && kidneyId != 2) {
     res.json({
       msg: "Wrong input",
     });
+  } else {
+    next();
   }
-});
+}
+
+app.get(
+  "/health-checkup",
+  userMiddleware,
+  kidneyMiddleware,
+  function (req, res) {
+    res.json({
+      msg: "Your Kidney is healthy",
+    });
+  }
+);
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
