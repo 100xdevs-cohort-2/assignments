fff98f2c2995b27e8f70de688d3770a5d8395cd8
Mani-IITM <68952375+Mani-IITM@users.noreply.github.com>
2024-01-13 00:41:27 +0530

    solved useCallback

--- week-6/2-use-callback/src/App.jsx
+++ week-6/2-use-callback/src/App.jsx
@@ -5,7 +5,7 @@ import { Assignment2 } from './components/Assignment2'
 function App() {
   return (
     <>
-      {/* <Assignment1 /> */}
+      <Assignment1 />
       <Assignment2 />
     </>
   )
