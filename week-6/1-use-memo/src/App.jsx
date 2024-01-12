53c2a97cb8013a666a42bd8c4b2f90571953fe6f
keshavjha123 <31789243+keshavjha123@users.noreply.github.com>
2024-01-13 02:10:43 +0530

    week 6 assignment

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
+import { Assignment1 } from './components/Assignment1'
+import { Assignment2 } from './components/Assignment2'
 import Assignment3 from './components/Assignment3'
 
 function App() {
   return (
     <>
       <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment2 />
+      <Assignment3 />
     </>
   )
 }
