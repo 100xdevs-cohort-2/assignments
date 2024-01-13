90846db8e4c93446d090ab0607b644d49a175ce6
Akash A <akashjain1536@email.com>
2024-01-13 18:59:48 +0530

    jwt

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,23 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('../index')
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+	const token = req.headers.authorization
+	const words = token.split(' ');
+	const jwtToken = words[1];
+
+	const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+
+	if(decodedValue.username){
+		next();
+	}else{
+		res.status(403).json({
+			message: 'You are not authorized to access this resource'
+		})
+	}
 }
+
 
 module.exports = adminMiddleware;
