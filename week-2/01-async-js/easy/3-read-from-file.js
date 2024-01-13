4c5771b865f206fb3b753b9504a8c6d5c97a6526
rahulkale22 <85778697+rahulkale22@users.noreply.github.com>
2024-01-13 13:04:22 +0530

    Update 3-read-from-file.js

--- week-2/01-async-js/easy/3-read-from-file.js
+++ week-2/01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,32 @@ You can use the fs library to as a black box, the goal is to understand async ta
 Try to do an expensive operation below the file read and see how it affects the output. 
 Make the expensive operation more and more expensive and see how it affects the output. 
 
+
+const fs = require('fs');
+
+
+
+fs.readFile('a.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  
+  console.log('File Contents:');
+  console.log(data);
+
+  performExpensiveOperation();
+});
+
+
+function performExpensiveOperation() {
+  console.log('Starting the expensive operation...');
+
+  
+  for (let i = 0; i < 1000000000; i++) {
+    const result = Math.sqrt(i);
+  }
+
+  console.log('Expensive operation completed.');
+}
