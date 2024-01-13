bec626f73b1c9a74c56f3b6b5e447dad381f3262
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2024-01-13 21:58:51 +0530

    Week 3 Assignment updated

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
-// mongoose.connect("mongodb+srv://priyankasati:12345@course-selling.d3psoi0.mongodb.net/");
-
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
@@ -12,48 +9,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-
-// // Define schemas
-// const AdminSchema = new mongoose.Schema({
-//   // Schema definition here
-//   username: String,
-//   password: String,
-// });
-
-// const UserSchema = new mongoose.Schema({
-//   // Schema definition here
-//   username: String,
-//   password: String,
-//   purchasedCourses: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Course",
-//     },
-//   ],
-// });
-
-// const CourseSchema = new mongoose.Schema({
-//   // Schema definition here
-//   title: String,
-//   description: String,
-//   price: Number,
-//   imageLink: String,
-// });
-
-// const Admin = mongoose.model("Admin", AdminSchema);
-// const User = mongoose.model("User", UserSchema);
-// const Course = mongoose.model("Course", CourseSchema);
-
-// module.exports = {
-//   Admin,
-//   User,
-//   Course,
-//   connectDB
-// };
-
-
-
-
 
 module.exports = { connectDB };
