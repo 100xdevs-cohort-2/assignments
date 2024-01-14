ed04d757b6f3f44f149884a38eb4907fba6928b7
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-14 14:00:56 +0530

    prmise-all-done

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,36 +5,23 @@
  */
 
 function wait1(t) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, t * 1000);
-  });
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait2(t) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, t * 1000);
-  });
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function wait3(t) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, t * 1000);
-  });
+  return new Promise((resolve) => setTimeout(resolve, t * 1000));
 }
 
 function calculateTime(t1, t2, t3) {
-  const startTime = Date.now();
+  const start = Date.now();
 
   return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(() => {
-    const endTime = Date.now();
-    const elapsedTime = endTime - startTime;
-    return elapsedTime;
+    const end = Date.now();
+    return end - start;
   });
 }
 
