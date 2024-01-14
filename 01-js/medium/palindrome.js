54d6fa89fe5e7500baa0eab7dadb16cf7390194f
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-14 13:39:13 +0530

    palindrome-times-done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,9 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  const newStr = str.replace("").toLowerCase();
+
+  return newStr === newStr.split("").reverse().join("");
 }
 
 module.exports = isPalindrome;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,18 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateTime() {
+  let n = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    n += i;
+  }
+  return n;
 }
+
+const beforeTime = new Date();
+console.log(beforeTime.getTime());
+calculateTime();
+const afterTime = new Date();
+console.log(afterTime.getTime());
+
+console.log(afterTime - beforeTime);
