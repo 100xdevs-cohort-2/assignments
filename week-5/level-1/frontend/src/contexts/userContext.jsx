90e89bc72e1a8df9785e7a7f488fba0ee5d615f8
piyush-ramnani <test@Git.com>
2024-01-16 02:32:39 +0530

    created user auth context API

---
+++ week-5/level-1/frontend/src/contexts/userContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+//1. Create user context
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+  console.log(userData);
+
+  const login = (userData) => {
+    setUserData(userData);
+  };
+
+  const logout = () => {
+    setUserData(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ userData, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
