048ce571ce583c1fc0a72dc41bace4cde3eab632
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-15 23:13:09 +0530

    Minor

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -4,12 +4,9 @@ import { Assignment2 } from './components/Assignment1'
 import Assignment3 from './components/Assignment3'
 
 function App() {
-  return (
+  return(
     <>
-      <Assignment1 />
-      {/* <Assignment2 /> */}
-sad
-      {/* <Assignment3 /> */}
+    
     </>
   )
 }
