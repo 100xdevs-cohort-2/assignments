22c3bcd01523466e067c359d1b8f05c9f1f0f0bc
ritiks236 <ritiks236@gmail.com>
2024-01-16 19:36:28 +0530

    assignment complted

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://codewithritiks:Singh1999@cluster0.wwndxnz.mongodb.net/random')
+.then(() => {console.log('Databse connection succesfull')})
+.catch((err) => {
+    console.log('Databse connection issue');
+    console.error(err);
+});
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    image: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,81 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
+const bcrypt = require('bcrypt');
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    try{
+        //get data
+        const {username, password} = req.body;
+
+        if(!username || !password){
+            return res.status(400).json({
+                success:false,
+                message:'Please fill all the details carefully'
+            })
+        }
+        
+        //check if already exists or not
+        const existingUser = await Admin.findOne({username});
+
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message : 'Admin already exists',
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newAdmin = await Admin.create({
+                                username : username,
+                                password : hashedPassword
+                            });
+
+        return res.status(200).json("Admin registered successfully");
+    }
+    catch(err){
+        console.error(err);
+        return res.status(404).json({
+            success: false,
+            msg : 'Admin registration failed, try again'
+        });
+    }
+   
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+    try{
+        const {title, description, price, image} = req.body;
+
+        const newCourse = await Course.create({
+            title,
+            description,
+            price,
+            image,
+        });
+
+        res.status(200).json('Course created sucessfully');
+    }
+    catch(err){
+        console.error(err);
+        return res.status(404).json({
+            success : false,
+            msg : 'Course creation failed!'
+        });
+    }
+    
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find().then(response => response.json()).then(data =>
+        {res.json(data)}
+    )
 });
 
 module.exports = router;
