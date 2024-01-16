39fa55bb6cfad68a77df7e26c4bef14db8bd2967
Prayag Hayagreev <66084810+haygreevprayag@users.noreply.github.com>
2024-01-16 20:59:57 +0530

    Update 1-promisify-setTimeout.js

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -6,3 +6,20 @@ function wait(n) {
 }
 
 module.exports = wait;
+function delay(seconds){
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve("Promise resolved after " +seconds +" seconds");
+    },seconds*1000);
+  });
+}
+
+const secondsToWait = 5;
+
+delay(secondsToWait)
+.then(function(result){
+  console.log(result);
+})
+.catch(function(error){
+  console.log("Error" +error);
+})
