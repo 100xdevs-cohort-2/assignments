b89d502a7fa7d91f2ca204f7a0eb0c953e5392bf
Yash Hegde <yash4success@gmail.com>
2024-01-16 18:21:51 +0530

    task1

---
+++ week-7-react-problems/react problems/src/App.jsx
@@ -0,0 +1,17 @@
+/* import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' */
+import './App.css'
+import  {Task1 } from './components/Task1'
+
+function App() {
+ /*  const [count, setCount] = useState(0) */
+
+  return (
+    <>
+       <Task1 />
+    </>
+  )
+}
+
+export default App
