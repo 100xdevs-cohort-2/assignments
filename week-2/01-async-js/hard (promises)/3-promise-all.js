dc944e90515da937b828d2116b421e3149533f22
rahulkale22 <kale42701@gmail.com>
2024-01-17 10:06:20 +0530

    week-2 solution async

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,18 +5,50 @@
  */
 
 function wait1(t) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+          }, t * 1000);
+
+    })
 
 }
 
 function wait2(t) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+          }, t * 1000);
+    })
 
 }
 
 function wait3(t) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+          }, t * 1000);
+    })
 
 }
 
 function calculateTime(t1, t2, t3) {
+     // Record the start time of the complete operation.
+  const start = Date.now();
+  // Calling the 3 funtions to get the promises.
+  const p1 = wait1(t1);
+  const p2 = wait2(t2);
+  const p3 = wait3(t3);
+  // Using the promise.all() api to wait for them to get resolved.
+  // It takes an array of the 3 promises as parameter.
+  // It returns a promise containing the results of all 3 promises in the form of an array.
+  return Promise.all([p1, p2, p3]).then(() => {
+    // Record the end time of the complete operation.
+    const end = Date.now();
+    // Find the time difference and return it.
+    const difference = end - start;
+    return difference;
+  });
 
 }
 
