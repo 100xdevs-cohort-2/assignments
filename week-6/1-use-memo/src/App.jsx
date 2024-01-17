6d769482fdb5d32ad1e2869eea79f4a13451feaa
tanishq-arya <tanishqarya24@gmail.com>
2024-01-17 23:19:43 +0530

    week-6\1-use-memo solved

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -6,8 +6,8 @@ import { Assignment3 } from './components/Assignment3'
 function App() {
   return (
     <>
-      <Assignment1 />
-      {/* <Assignment2 /> */}
+      {/* <Assignment1 /> */}
+      <Assignment2 />
       {/* <Assignment3 /> */}
     </>
   )
