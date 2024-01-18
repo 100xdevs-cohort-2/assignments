d18462d9fed652127e627e52235135f496174236
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-01-18 18:07:12 +0530

    week3

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,13 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+function requestCountMiddleware(req ,res ,next){
+  requestCount++
+  next();
+}
+
+app.use(requestCountMiddleware);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
@@ -21,5 +28,7 @@ app.post('/user', function(req, res) {
 app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
+
+app.listen(3000);
 
 module.exports = app;
--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -47,3 +47,4 @@ module.exports = {
   decodeJwt,
   jwtPassword,
 };
+
