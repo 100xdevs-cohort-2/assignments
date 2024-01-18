c13eb0038a6444864b81e91db77afaa568ca7793
krishvsoni <sonikrish994@gmail.com>
2024-01-18 22:37:01 +0530

    React

---
+++ week-7-messed-up-code/src/App.jsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  // wrap anyone that wants to use the teleported value inside a provider
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount} />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}) {
+  const count = useContext(CountContext);
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
---
+++ week-7-messed-up-code/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react"
+
+export default function Dashboard() {
+
+    return <div>
+        Dashboard page
+    </div>
+}
