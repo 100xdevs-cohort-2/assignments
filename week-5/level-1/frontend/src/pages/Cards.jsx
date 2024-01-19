020262f37b5f9218478a07345438b03102319ee0
piyush-ramnani <test@Git.com>
2024-01-19 17:14:56 +0530

    Fetch all card details | Props | Conditional rendering

--- week-5/level-1/frontend/src/pages/Cards.jsx
+++ week-5/level-1/frontend/src/pages/Cards.jsx
@@ -2,20 +2,51 @@ import React from "react";
 import CreateCards from "../components/CreateCards";
 import AllCards from "../components/AllCards";
 import { useAuth } from "../contexts/userContext";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function Cards() {
-  // const { userData } = useAuth();
+  const [allCards, setAllCards] = useState([]);
+  const { userData } = useAuth();
+
+  const fetchData = async () => {
+    console.log("userData", userData);
+    const token = userData.token;
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/user/get-all-cards",
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      console.log("allcards: ", response.data);
+      if (response.status == 200) {
+        setAllCards(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching all cards", error);
+      alert("Error fetching all cards");
+    }
+  };
+
+  useEffect(() => {
+    if (userData) {
+      fetchData();
+    }
+  }, []);
 
   return (
     <>
-      <CreateCards />
-      {/* {userData && userData.role === "admin" ? (
+      {userData && userData.role === "admin" ? (
         <CreateCards />
       ) : (
         <div className="container h-screen mx-auto flex flex-col border border-gray-400">
-          <AllCards />
+          <AllCards allCards={allCards} />
         </div>
-      )} */}
+      )}
     </>
   );
 }
