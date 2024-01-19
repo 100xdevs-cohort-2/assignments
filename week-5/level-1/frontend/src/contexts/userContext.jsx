55215239997ab0d249631042c4a53704fc86a60c
piyush-ramnani <test@Git.com>
2024-01-20 00:59:02 +0530

    useContext | localStorage | persistent storage

--- week-5/level-1/frontend/src/contexts/userContext.jsx
+++ week-5/level-1/frontend/src/contexts/userContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 //1. Create user context
 const AuthContext = createContext();
@@ -6,12 +6,22 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      console.log("context localstorage", storedUser);
+      setUserData(JSON.parse(storedUser));
+    }
+  }, []);
+
   const login = (userData) => {
     setUserData(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
   const logout = () => {
     setUserData(null);
+    localStorage.removeItem("user");
   };
 
   return (
