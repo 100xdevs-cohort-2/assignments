2b182f87188ca673ff1b371c0d893161447c755f
piyush-ramnani <test@Git.com>
2024-01-19 17:14:13 +0530

    Context API | user context | token and role

--- week-5/level-1/frontend/src/contexts/userContext.jsx
+++ week-5/level-1/frontend/src/contexts/userContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 //1. Create user context
 const AuthContext = createContext();
