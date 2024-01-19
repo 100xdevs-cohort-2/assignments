e1b3ff43d83d67920ff52b483d37b4475577aa58
KUSUMANCHI ESWAR ASHISH <20bec023@iiitdwd.ac.in>
2024-01-19 19:35:07 +0530

    d

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
 import { Assignment1 } from './components/Assignment1'
 import { Assignment2 } from './components/Assignment2'
-<<<<<<< HEAD
-import Assignment3 from './components/Assignment3'
-=======
+
 import { Assignment3 } from './components/Assignment3'
->>>>>>> 01e8f0ef912437dc2301754d9ff226fada22a26d
+
 
 function App() {
   return (
