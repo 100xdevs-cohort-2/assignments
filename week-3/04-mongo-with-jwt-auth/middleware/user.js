b61732ff02bb76a5d36772c082fc678bf6ce0e26
Prachi-2001 <prachipachang164@gmail.com>
2024-01-20 17:08:15 +0530

    done mongo with jwt assignmnet

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,21 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+  const words = token.split(" ");
+  const jwtToken = words[1];
+  const data = await jwt.verify(jwtToken, JWT_SECRET);
+  if (data.username) {
+    req.username = data.username;
+    next();
+  } else {
+    res.status(403).json({
+      message: "Admin does not exist",
+    });
+  }
 }
 
 module.exports = userMiddleware;
