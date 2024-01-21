bbf416c28c25058fa8f7bc81385b6c5e7686d1cd
Shahid Khan <shahidseran786@gmail.com>
2024-01-22 04:00:05 +0530

    File Handeling + Easy (Async)

---
+++ week-2/01-async-js/easy/solution/1-counter.js
@@ -0,0 +1,5 @@
+let count=0;
+setInterval(() => {
+    count++;
+    console.log(count);
+}, 1000);
---
+++ week-2/01-async-js/easy/solution/3-read-from-file.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const filePath = "./a.txt";
+
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log("File content:", data);
+});
