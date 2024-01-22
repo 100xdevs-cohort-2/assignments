9889dd6d4ce8e4745e542a0b90059765006d89f2
Vishnu B V <vishnubv944@gmail.com>
2024-01-22 08:57:14 +0530

    complete week-6 3-use-ref

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,20 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
 
-    useEffect(() => {
+    const inputRef = useRef()
 
+    useEffect(() => {
+        inputRef.current.focus()
     }, []);
 
     const handleButtonClick = () => {
-
+        inputRef.current.focus()
     };
 
     return (
         <div>
-            <input type="text" placeholder="Enter text here" />
+            <input ref={inputRef} type="text" placeholder="Enter text here" />
             <button onClick={handleButtonClick}>Focus Input</button>
         </div>
     );
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useCallback } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
-
+let count = 0
 export function Assignment2() {
-    const [, forceRender] = useState(0);
-
+    count++;
+    const [, forceRender] = useState(count);
+    const pRef = useRef()
+    
     const handleReRender = () => {
         // Update state to force re-render
         forceRender(Math.random());
     };
 
+    useEffect(()=>{
+        pRef.current.innerHTML = count
+    })
+
     return (
         <div>
-            <p>This component has rendered {0} times.</p>
+            <p ref={pRef}>This component has rendered {0} times.</p>
             <button onClick={handleReRender}>Force Re-render</button>
         </div>
     );
