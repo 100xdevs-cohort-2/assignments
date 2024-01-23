63054cefa6fb7264c7fb464416b006e0cf169ddf
HashirAKB <ahmedhashir96@gmail.com>
2024-01-23 18:59:24 +0530

    Completed assignement one.

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -3,9 +3,22 @@
   Example:
   - Input: [3, 7, 2, 9, 1]
   - Output: 9
+
+    How to write the solution:
+    - assign a variable and store the 1st element of the array in it.
+    - Loop through the array and compare each element with the variable.
+    - if the element is greater than the variable, update the variable.
+    - upon completion of the loop, return the variable.
 */
 
 function findLargestElement(numbers) {
+    let largest = numbers[0];
+    for (i=0; i<=numbers.length; i++){
+        if (numbers[i] > largest){
+            largest = numbers[i];
+        }
+    }
+    return largest;
     
 }
 
