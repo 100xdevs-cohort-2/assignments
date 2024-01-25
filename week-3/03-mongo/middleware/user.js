f6faa6b4c5217e62b03536167e0a4c4a349ccf44
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-01-26 02:35:24 +0530

    upto mongo and jwt

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,19 @@
-function userMiddleware(req, res, next) {
+const {User}=require("../db/index");
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const {username,password}=req.headers;
+
+    await User.findOne({username,password})
+    .then((value)=>{
+        if(value){
+            next();
+        }else{
+            res.status(400).json({message:"user does not exist!"})
+        }
+    })
 }
 
 module.exports = userMiddleware;
