2a67a81efa4868fbc26a411ed3f74cc863b3a798
Harshit <harshitpanari@gmail.com>
2024-01-26 00:06:39 +0530

    01 done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,11 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+ app.use( function(req, res, next) {
+  requestCount++
+  next();
+ })
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
@@ -22,4 +27,5 @@ app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
 
+app.listen(3000);
 module.exports = app;
--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,17 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req,res,next){
+  let userID = req.headers[user-id];
+  if(numberOfRequestsForUser[user-id]){
+    numberOfRequestsForUser[user - id] = numberOfRequestsForUser[user - id]+1;
+    if(numberOfRequestsForUser>5)
+    {
+      res.status(404).send("No No Entery")
+    }
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
