6b2c01f764bfc86675b81bf326edb5ee18cb0d87
vrajpatel002222021@outlook.com <vrajpatel002222021@outlook.com>
2024-01-27 00:41:20 +0530

    error count done

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,8 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use(function(err,req,res,next){
+  res.status(404).send({})
+  errorCount = errorCount + 1;
+})
 module.exports = app;
