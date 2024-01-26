fe89d43e58fae9155638bb57effb380a05b8577c
vrajpatel002222021@outlook.com <vrajpatel002222021@outlook.com>
2024-01-26 23:52:18 +0530

    request count done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -9,6 +9,10 @@ let requestCount = 0;
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
+app.use(function(req,res,next){
+  requestCount = requestCount + 1;
+  next();
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
