995355eb421c0c46d2b129e31b72b49aca40440c
HashirAKB <ahmedhashir96@gmail.com>
2024-01-27 15:33:40 +0530

    - Completed solution for countVowles. - Re-wrote the code with better coding practices.

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -3,10 +3,30 @@
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
   Once you've implemented the logic, test your code by running
+
+  Initial Solution:
+  function countVowels(str) {
+    let vowels = ['a','e','i','o','u'];
+    let count = 0;
+    str = str.toLowerCase().split('').sort();
+    for(i=0;i<str.length;i++){
+      if(vowels.includes(str[i])){
+        count = count + 1;
+      }
+    }
+    return count;
+}
+
+Optimised one:
+
 */
 
 function countVowels(str) {
-    // Your code here
+  const vowelsRegex = /[aeiou]/gi;
+  const matches = str.match(vowelsRegex);
+  return matches ? matches.length : 0;
 }
+
+count = countVowels('addseffiddo');
 
 module.exports = countVowels;
