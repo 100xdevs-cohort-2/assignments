c5fc44200203366eaa1c3eeec723df36f049a013
Sourav <souravmohanta4428@gmail.com>
2024-01-27 18:40:10 +0530

    done 03-mongo/middleware/user.js

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,3 +1,4 @@
+import { User } from "../db";
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
