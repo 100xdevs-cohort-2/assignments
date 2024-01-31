ca462fcf4ff552c12140ccbf7e60acc0947e91ee
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-31 22:10:21 +0530

    added answer folder

---
+++ week-5/level-1/answer/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam velit
+      atque odit nobis magni! Adipisci unde consequuntur quo provident, quia
+      fugiat, ea iusto praesentium nostrum pariatur distinctio! Vero, saepe
+      dolore?
+    </>
+  );
+}
+
+export default App;
