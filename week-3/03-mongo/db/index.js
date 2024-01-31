8f6f949ce5197724307f450e8135ab24c8138850
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-01-31 12:31:17 +0530

    week3

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,21 +1,48 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://Vijay:mmYVHo2KsiR2HsYN@cluster0.btjkz2i.mongodb.net/udemy');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    // email: {
+    //     type: email, 
+    //     required: true,
+    //     unique: true
+    // },
+
+    password: {
+        type: String,
+        required: true,
+    }
+
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourse: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String, 
+    description: String, 
+    imageLink: String,
+    price: Number
 });
-
+ 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
