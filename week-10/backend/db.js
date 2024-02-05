6f2e583e6089ab9d212ce783c7527d2612460e27
Yash Hegde <yash4success@gmail.com>
2024-02-05 20:20:06 +0530

    writing out functions

---
+++ week-10/backend/db.js
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function insertUser() {
+    
+}
+
+async function deleteUser() {
+    
+}
+
+async function updateUser() {
+    
+}
+
+async function getUser() {
+    
+}
+
+async function insertTodo() {
+    
+}
+
+async function deleteTodo() {
+    
+}
+
+async function updateTodo() {
+    
+}
+
+async function getTodo() {
+    
+}
+
+
+
---
+++ week-10/prisma/migrations/20240205143244_user_todos/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
