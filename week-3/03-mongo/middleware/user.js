2ddd3324a9155139c81afdf44caac65019310616
stainsleto <stainsmault@gmail.com>
2024-02-06 23:37:38 +0530

    @stainsleto/ 03-mongo assignment done

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,22 @@
+const { User } = require('../db/index')
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const {username, password} = req.headers
+
+    User.findOne( {
+        username : username,
+        password : password
+    })
+    .then ( value => {
+        if(value){
+            next()
+        }
+        else{
+            res.status(403).json({'msg' : 'User not found'})
+        }
+    })
+    
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,65 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require('../db/index')
 const router = Router();
 
+
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+
+    const { username, password } = req.headers
+
+    try{
+        await Admin.create( {
+            username: username,
+            password:password
+        })
+
+        res.status(200).json({ 'message' : 'Admin created successfully'})
+    }
+
+    catch(err){
+        res.status(403).json({'message' : 'Admin Not Created Error'})
+    }
+    
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    const {title, description, price, imageLink} = req.body
+
+    try {
+        const newCourse = await Course.create( {
+            title: title,
+            description : description,
+            price: price,
+            imageLink : imageLink
+        })
+        console.log(newCourse)
+
+        res.status(200).json({
+            message : 'Course created successfully',
+            courseId : newCourse._id
+        })
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(403).json({"message" : "Course creation failed "})
+    }
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async (req, res) => {
+
+    const response = await Course.find({})
+
+    res.json({
+        courses : response
+    })
+
+    res.status(200).json({
+
+    })
 });
 
 module.exports = router;
