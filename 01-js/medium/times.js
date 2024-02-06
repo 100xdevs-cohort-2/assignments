5214705b8b003d2df001522338eb893480c0e875
AtharvaMane23 <103356325+AtharvaMane23@users.noreply.github.com>
2024-02-06 23:47:30 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,23 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let abc= new Date()
+    let startMinute = abc.getMinutes()*60;
+    let startSeconds = abc.getSeconds();
+    let startTime = startMinute+startSeconds;
+    //console.log(startTime);
+    let sum=0;
+    for (let i=1; i<n; i++){
+        sum += sum;
+    }
+    let efg = new Date();
+    let endMinute = efg.getMinutes()*60;
+    let endSeconds = efg.getSeconds();
+    let endTime = endMinute+endSeconds;
+    //console.log(endTime);
+
+    let result= endTime - startTime;
+    return result;
 }
+
+console.log(calculateTime(10000000000));
