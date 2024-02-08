f3b2a029ccf5fa32dccde4037a0bb00936e5bf1f
jainam-b <jainambagrecha16@gmail.com>
2024-02-08 10:13:44 +0530

    DB and middleware functionality completed

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("mongodb+srv://jainamb:jainamBagrecha@cluster0.h5mn9fs.mongodb.net/course_selling_app");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+     username:String,
+     password:String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username:String,
+    password:String,
+    purchasedCourses:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Course"
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description:String,
+    imageLink:String,
+    price:String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,14 +1,35 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin }=require("../db")
+const { Course }=require("../db")
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup',async (req, res) => {
+   const username=req.body.username;
+   const password=req.body.password;
+   await Admin.create({
+    username:username,
+    password:password
+   })
+   res.json({
+    message:"User created Successfully"
+   }) 
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title=req.body.title;
+    const description=req.body.description;
+    const imageLink=req.body.imageLink;
+    const price=req.body.price;
+    Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
