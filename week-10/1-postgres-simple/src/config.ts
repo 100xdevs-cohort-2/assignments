7b33b8e2b02fe25580d074d0c3216c3817b8ab58
Shivam Kaushik <skshivam.1309@gmail.com>
2024-02-09 01:35:07 +0530

    week-10 -postgres assignment done

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
-
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL =
+  "postgresql://skshivam.1309:tlpKnuo60aki@ep-fancy-sound-a1yur0rd.ap-southeast-1.aws.neon.tech/postgres-test?sslmode=require";
--- week-10/1-postgres-simple/src/index.ts
+++ week-10/1-postgres-simple/src/index.ts
@@ -1,6 +1,6 @@
-import { Client } from 'pg'
-import { DB_URL } from './config';
+import { Client } from "pg";
+import { DB_URL } from "./config";
 
 export const client = new Client({
-    connectionString: DB_URL
+  connectionString: DB_URL,
 });
