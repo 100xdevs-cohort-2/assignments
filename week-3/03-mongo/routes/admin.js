bfa74c3ff166128ed31b32a3113a3d1fff7f3d11
jainam-b <jainambagrecha16@gmail.com>
2024-02-08 20:58:27 +0530

    mongo problem solved

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -18,22 +18,31 @@ router.post('/signup',async (req, res) => {
 
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
     const title=req.body.title;
     const description=req.body.description;
     const imageLink=req.body.imageLink;
     const price=req.body.price;
-    Course.create({
+    const newCourse= await Course.create({
         title,
         description,
         imageLink,
         price
     })
+    res.json({
+        msg:"Course created Succesfully",couseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware,async (req, res) => {
     // Implement fetching all courses logic
+     const response=await Course.find({});
+     res.json({
+        couses:response
+     })
+     
+
 });
 
 module.exports = router;
