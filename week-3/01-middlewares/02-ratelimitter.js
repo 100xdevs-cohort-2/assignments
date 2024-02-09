3fa97fd87ccf62aa092701d321ca72e76df8b4e5
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-08 22:58:44 -0500

    implemented middleware assignment 2

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,21 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next)=> {
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] += 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).json("you have been blocked. please try again after some time.")
+    } else {
+      next();
+    }
+  } else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
