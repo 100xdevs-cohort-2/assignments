a6d5bc7802df05079b3040df0497d44199192f33
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-08 22:44:06 -0500

    solved week 3 middleware 1

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,14 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+function requestCounter (req, res, next) {
+  requestCount += 1;
+  console.log("so far you have "+ requestCount+ " requests." )
+  next();
+}
+
+app.use(requestCounter);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
