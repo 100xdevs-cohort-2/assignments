a9ae13f3749723e751e52988827b633deb3052d6
Manish Jha <manishjhazyx@gmail.com>
2024-02-11 17:42:41 +0530

    week 3 assignment solutions

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_URL);
 
+const CourseSchema = new mongoose.Schema({
+  // Schema definition here
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageLink: { type: String, required: true },
+});
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  courses: { type: CourseSchema, required: false },
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
 
-const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
-
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,25 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+// Middleware for handling auth
+const jwt = require("jsonwebtoken");
+async function userMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+  if (!token) {
+    res.json({ msg: "Token not found!" });
+  }
+  const jwt_token = token.split(" ")[1];
+  try {
+    jwt.verify(jwt_token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        res.status(403).json({ msg: " You are not authorized user" });
+      } else {
+        req.username = user.username;
+        next();
+      }
+    });
+  } catch {
+    return res.status(404).send({ message: "Not Found" });
+  }
 }
 
 module.exports = userMiddleware;
