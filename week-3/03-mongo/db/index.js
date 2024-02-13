dd0f772150ef82741bb77433d9bdb82da44acf84
nehasable <nehasable45@gmail.com>
2024-02-13 17:54:46 +0530

    commit

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://localhost:27017/course/new');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
