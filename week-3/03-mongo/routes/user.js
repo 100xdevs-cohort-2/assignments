d6b5e03e937509d3f7ea8164d1af4e4b20cc9509
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-14 16:56:13 -0500

    user signup endpoint

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,34 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let userFound = await User.find({
+        username: username
+    })
+
+    if (userFound.length>0) {
+        res.status(403).json({
+            message: 'user already exists'
+        })
+    } else {
+        User.create({
+            username,
+            password,
+            purchasedCourses: []
+        }).then(function (){
+            res.status(200).json({
+                message: "user created successfully"
+            })
+        })
+    }
+
 });
 
 router.get('/courses', (req, res) => {
