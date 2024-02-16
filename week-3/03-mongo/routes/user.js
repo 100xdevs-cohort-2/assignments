aa551d6293e9743904b7cd244cafc68005a0f2de
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-02-16 00:10:13 -0500

    completed

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -40,12 +40,41 @@ router.get('/courses', async (req, res) => {
     })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.body.username;
+
+    await User.updateOne({
+        username
+    }, {
+        "$push":{
+            purchasedCourses: courseId
+        }
+    })
+
+    res.status(200).json({
+        message: "Purchase successfull!"
+    })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const username = req.headers.username;
+    const userData = await User.findOne({
+        username
+    })
+
+    const purchases = await Course.find({
+        _id:{
+            "$in": userData.purchasedCourses
+        }
+    })
+
+    res.status(200).json({
+        purchasedCourses: purchases
+    })
+
 });
 
 module.exports = router
