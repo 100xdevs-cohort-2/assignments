a31328202b3bcf99379222b5a6e65768874d4f8f
AbhayMishra <grabhaymishra@gmail.com>
2024-02-18 16:27:07 +0530

    Week 10 Assignments Done

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -11,6 +11,19 @@ import { client } from "..";
  */
 export async function createTodo(userId: number, title: string, description: string) {
     
+       try{
+        const InsertQuery = "INSERT INTO todos (user_id,title,description) values ($1,$2,$3) RETURNING title, description, done ,id"
+        const res = await client.query(InsertQuery,[userId,title,description])
+        if (res.rows && res.rows.length > 0) {
+            return res.rows[0]; // Return the first row of the result
+        } else {
+            throw new Error('User insertion failed'); // Throw an error if no rows were returned
+        }
+    }catch(e){
+        console.log("")
+    }
+    
+    
 }
 /*
  * mark done as true for this specific todo.
@@ -23,6 +36,15 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
+     const updateQuery = "UPDATE todos SET done = true WHERE id = ($1) RETURNING title,description,done,id";
+
+     const res = await client.query(updateQuery,[todoId])
+
+     if (res.rows && res.rows.length > 0) {
+        return res.rows[0]; // Return the first row of the result
+    } else {
+        throw new Error('User updation failed'); // Throw an error if no rows were returned
+    }
 
 }
 
@@ -37,5 +59,14 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
+    const allQuery = "SELECT * FROM todos WHERE user_id = ($1)"
+
+    const res = await client.query(allQuery,[userId])
+
+    if (res.rows && res.rows.length > 0) {
+        return res.rows; // Return the first row of the result
+    } else {
+        throw new Error('User not have todos'); // Throw an error if no rows were returned
+    }
 
 }
