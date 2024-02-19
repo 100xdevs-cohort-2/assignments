124993b8ef4d0fe0e6c6ce9af419daa11c1cb7cd
saralapujar <saralapujar@gmail.com>
2024-02-19 19:57:41 +0530

    added todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -9,9 +9,67 @@
 
   Once you've implemented the logic, test your code by running
 */
-
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    } else {
+      console.error("Invalid index. Todo not removed.");
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    } else {
+      console.error("Invalid index. Todo not updated.");
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } else {
+      console.error("Invalid index. Todo not found.");
+      return null;
+    }
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
+
+// Testing the code
+const myTodoList = new Todo();
+
+myTodoList.add("Buy groceries");
+myTodoList.add("Finish coding assignment");
+myTodoList.add("Go for a run");
+
+console.log("Initial Todo List:", myTodoList.getAll());
+
+myTodoList.remove(1);
+console.log("Todo List after removing item at index 1:", myTodoList.getAll());
+
+myTodoList.update(0, "Buy snacks");
+console.log("Todo List after updating item at index 0:", myTodoList.getAll());
+
+console.log("Todo at index 1:", myTodoList.get(1));
+
+myTodoList.clear();
+console.log("Todo List after clearing all todos:", myTodoList.getAll());
