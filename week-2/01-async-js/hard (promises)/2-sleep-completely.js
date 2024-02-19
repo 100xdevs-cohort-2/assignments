c2b4a85f4272b623a885c75d9616341f583a2457
Rohit Kumar <113338596+Rohitk131@users.noreply.github.com>
2024-02-20 00:09:15 +0530

    Update 2-sleep-completely.js

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,13 @@
  */
 
 function sleep(milliseconds) {
+
+
+    return new Promise((resolve) => {
+        setInterval(() => {
+            resolve();
+        }, milliseconds);
+    })
 }
 
 module.exports = sleep;
