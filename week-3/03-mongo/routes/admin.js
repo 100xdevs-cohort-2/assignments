3d88900475eee446796d4354e0dbe70d193b8a86
Vikaskumar Dane <90561371+Vikaskumardane@users.noreply.github.com>
2024-02-24 17:24:37 +0000

    working on the mongoDB assignment

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,14 +1,37 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+       res.json({
+        msg:"hi there"
+
+       })     //get the username
+    const username= req.body.username;
+    const password= req.body.password;
+    
+
+    //Check if user with this username already exists
+    await Admin.create({
+        username: username,
+        password: password 
+    })
+   
+        res.json({
+            message:" Admin created successfully"
+        })
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', (req, res) => {
     // Implement course creation logic
+    res.json({
+        msg:"hi there"
+
+       })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
