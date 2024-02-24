459daba338e8c9d9b5f625c33dc251154cd38f09
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-02-24 22:50:19 +0530

    fixed minor bugs

--- week-8-paytm/backend/src/index.ts
+++ week-8-paytm/backend/src/index.ts
@@ -8,7 +8,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://192.168.1.2:5173"],
+  })
+);
 config();
 
 app.use("/api/v1", mainRouter);
