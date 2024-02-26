a989bf942539e83044aa8e23ff0ec423c56c71f3
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-27 00:53:06 +0530

    Updated assignments of Week03
    01-Middlewares

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,5 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
@@ -10,15 +8,20 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
+app.use(function (req, res, next) {
+  requestCount = requestCount + 1;
+  next();
+})
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get('/requestCount', function (req, res) {
   res.status(200).json({ requestCount });
 });
 
