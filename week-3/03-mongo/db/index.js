35e0193767e8578893baaddb1e43122386cc506b
kabbo25 <kabbo25@gmail.com>
2024-02-26 17:41:11 +0600

    feature: implement user portal

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -12,7 +12,12 @@ const AdminSchema = new mongoose.Schema({
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
-
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
