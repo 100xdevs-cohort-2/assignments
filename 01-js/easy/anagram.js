d76a67839e1df6f447a17af4a73ee0aaef88a537
Saurabh Saxena <saurabh.jan03@gmail.com>
2024-03-04 01:38:18 -0500

    anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,35 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length){
+    return false;
+  }
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let map = new Map();
+  for(let i = 0; i <= str1.length-1; i++){
+    if(map.has(str1[i])){
+      map.set(str1[i], map.get(str1[i]) + 1);
+    }
+    else{
+      map.set(str1[i], 1);
+    }
+  }
+
+  for(let i = 0; i <= str2.length-1; i++){
+    if(map.has(str2[i])){
+      map.set(str2[i], map.get(str2[i]) - 1);
+      if(map.get(str2[i]) == 0){
+        map.delete(str2[i])
+      }
+    }
+  }
+  if(map.size == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 
 }
 
