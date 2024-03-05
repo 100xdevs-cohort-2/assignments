f5502c48a0146b7478a8172ab7e4407927533c13
neha sable <nehasable45@gmail.com>
2024-03-05 16:19:55 +0530

    commit

---
+++ week-6/use-memo/src/components/Assignment3.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import  { useState, useMemo } from 'react';
+// You have been given a list of items you shopped from the grocery store
+// You need to calculate the total amount of money you spent
+
+export const Assignment3 = () => {
+    const [items, setItems] = useState([
+        { name: 'Chocolates', value: 10 },
+        { name: 'Chips', value: 20 },
+        { name: 'Onion', value: 30 },
+        { name: 'Tomato', value: 30 },
+        // Add more items as needed
+    ]);
+
+    // Your code starts here
+    const totalValue=useMemo(()=>{
+        let tValue = 0;                 //set value to 0
+        for(let i=0;i<items.length;i++){//i=0 and it is < arr.length and it increases
+            tValue+=items[i].value  //totavalue=totalvalue+items[i].value
+        }
+        return tValue
+    },[items])
+   
+    // Your code ends here
+    return (
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                ))}
+            </ul>
+            <p>Total Value: {totalValue}</p>
+        </div>
+    );
+};
