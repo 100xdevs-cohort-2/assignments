3a11c2aa86b05de40cb1f60741b890c5b340c885
Saurabh Saxena <saurabh.jan03@gmail.com>
2024-03-05 00:58:40 -0500

    week1 assignment

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,26 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let map = new Map([
+    ['a',1], ['e',1], ['o',1], ['i',1], ['u',1], ['A',1], ['E',1], ['I',1], ['O',1], ['U',1]
+  ]);
+  // map.set('a',1);
+  // map.set('e',1);
+  // map.set('i',1);
+  // map.set('o',1);
+  // map.set('u',1);
+  // map.set('A',1);
+  // map.set('E',1);
+  // map.set('I',1);
+  // map.set('O',1);
+  // map.set('U',1);
+  let count = 0;
+  for(let i = 0; i <= str.length-1; i++){
+    if(map.has(str[i])){
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
