fca60de689f05f0bb9dc32125b263b4a4114646f
HashirAKB <ahmedhashir96@gmail.com>
2024-03-05 13:52:30 +0530

    "Refactor wait function to use async/await and promises for delay"

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,7 +2,12 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
-function wait(n) {
+async function wait(n) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        },n*1000);
+    });
 }
 
 module.exports = wait;
