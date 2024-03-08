e2ba90ea932d06b7ddca2f8f7ec9cca80dc662b0
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-03-08 19:20:40 +0530

    Assignment 1 week 7 added

---
+++ week-7/recoilers/src/App.jsx
@@ -0,0 +1,12 @@
+import Assignment1 from "./components/Assignment1";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Assignment1 />
+    </>
+  );
+}
+
+export default App;
---
+++ week-7/recoilers/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "week-7/recoilers/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
