0a15cbb31d1d75a5d73e88b2030f4ced4508c136
ShanPS <mohammedshan.p.s@gmail.com>
2024-03-08 19:16:03 +0530

    add solution for postgres - week10

--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -1,26 +1,37 @@
 import { client } from "..";
 
+type User = {
+  id: number;
+  username: string;
+  password: string;
+  name: string;
+};
+
 /*
  * Should insert into the users table
- * Should return the User object
- * {
- *   username: string,
- *   password: string,
- *   name: string
- * }
+ * Returns void
  */
-export async function createUser(username: string, password: string, name: string) {
-    
+export async function createUser(username: string, password: string, name: string): Promise<void> {
+  const query = `INSERT INTO users (username, password, name) 
+                 VALUES ($1, $2, $3)`;
+  const values = [username, password, name];
+  await client.query(query, values);
 }
 
 /*
  * Should return the User object
  * {
+ *   id: number,
  *   username: string,
  *   password: string,
  *   name: string
  * }
  */
-export async function getUser(userId: number) {
-    
+export async function getUser(userId: number): Promise<User> {
+  const query = `SELECT * 
+                 FROM users 
+                 WHERE id=$1`;
+  const values = [userId];
+  const res = await client.query(query, values);
+  return res.rows[0];
 }
