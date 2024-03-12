3901ff09582913394e6fa03f09bc9cc98dfb8805
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-03-12 11:16:41 +0530

    01-js solutions

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,20 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  const totalSpentByCategory = {}; 
+  transactions.forEach(transaction => {
+      const { category, price } = transaction;
+      if (totalSpentByCategory[category]) {
+          totalSpentByCategory[category] += price;
+      } else {
+          totalSpentByCategory[category] = price;
+      }
+  });
+  const result = Object.keys(totalSpentByCategory).map(category => ({
+      category,
+      totalSpent: totalSpentByCategory[category]
+  }));
 
+  return result;
+}
 module.exports = calculateTotalSpentByCategory;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+    if (numbers.length === 0) {
+        return undefined;
+    }
+    let largest = numbers[0]; 
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > largest) {
+            largest = numbers[i];
+        }
+    }
+    return largest;
 }
 
 module.exports = findLargestElement;
