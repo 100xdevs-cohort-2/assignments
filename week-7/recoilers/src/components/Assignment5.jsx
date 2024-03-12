dc581c8255477bc231c5250dacde2c4cfc8840e5
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-03-13 00:46:21 +0530

    Assignment 5 getting Github Data

--- week-7/recoilers/src/components/Assignment5.jsx
+++ week-7/recoilers/src/components/Assignment5.jsx
@@ -1,14 +1,44 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Assignment5 = () => {
-  async function getData(){
-    const data = await axios.get("https://api.github.com/users/CS50X-RGB");
-    console.log(data);
+  const [id, setId] = useState("");
+  const [user, setUser] = useState("");
+
+  async function getData() {
+    try {
+      const { data } = await axios.get(`https://api.github.com/users/CS50X-RGB`);
+      setUser(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
   }
-  useEffect(()=>{
-    getData();
-  })
-  return <div>Assignment5</div>;
+
+  useEffect(() => {
+    if (id) {
+      getData();
+    }
+  }, [id]);
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter Your Github Id"
+        onChange={(e) => setId(e.target.value)}
+      />
+      {user && (
+        <>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h1>{user.login}</h1>
+            <img src={user.avatar_url} alt="User Avatar" style={{height : "100px",width : "100px",borderRadius : "100px"}} />
+            {user.email ?  <h1>{user.email}</h1> : <></>}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
+
 export default Assignment5;
