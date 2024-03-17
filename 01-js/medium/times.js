b07ade04d02cab71d22d862c123d195b109773df
Mikasa8324 <129768324+Mikasa8324@users.noreply.github.com>
2024-03-17 18:24:15 +0530

    Update times.js
    Solution

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,11 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+   let startTime = new Date().getTime();
+   let sum = 0;
+  for(let i = 1; i <= n; i++){
+    sum =+ i;}
+  let endTime = new Date().getTime();
+  let timeTaken = endTime - startTime;
+  return timeTaken/1000;
 }
