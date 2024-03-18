bc3a4f8a70dae9da24e3ac7f61d4d7eaa35d074b
aditya gore <aditya_gore@hotmail.com>
2024-03-18 22:43:14 +0530

    useMemo done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -3,25 +3,29 @@ import React, { useState, useMemo } from 'react';
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: 'Chocolates', value: 10 },
+    { name: 'Chips', value: 20 },
+    { name: 'Onion', value: 30 },
+    { name: 'Tomato', value: 30 },
+    // Add more items as needed
+  ]);
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+  // Your code starts here
+  const totalValue = useMemo(() => {
+    return items.reduce((prev, curr) => (prev += curr.value), 0);
+  }, [items]);
+  // Your code ends here
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} - Price: ${item.value}
+          </li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+    </div>
+  );
 };
