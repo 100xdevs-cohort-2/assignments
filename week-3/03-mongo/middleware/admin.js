0e22859f163a1c483831eb02a6a8e48c7bda637a
HashirAKB <ahmedhashir96@gmail.com>
2024-03-18 17:00:13 +0530

    feat: Implement course selling website with admin and user functionalities
    - Implemented routes for admin and user functionalities as described in the task description.
    - Created endpoints for admin signup, course creation, and course listing.
    - Created endpoints for user signup, course listing, course purchase, and purchased course listing.
    - Utilized MongoDB to persistently store all data.

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,21 @@
+const db = require("../db/index");
+const adminModel = db.Admin;
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    try{
+        const username = req.headers.username;
+        const password = req.headers.password;
+        const user = await adminModel.findOne({username, password});
+        if (user){
+            console.log("Admin verifed.");
+            next();
+        }
+    }
+    catch(err){
+        res.status(500).json({error: err});
+    }
 }
 
 module.exports = adminMiddleware;
