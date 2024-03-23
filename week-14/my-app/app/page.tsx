f4000077e10e9b65da933c784c26db1de9f177c9
ShanPS <mohammedshan.p.s@gmail.com>
2024-03-24 03:06:10 +0530

    add solution - week14

---
+++ week-14/my-app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Welcome to Home Page</h1>
+      <p className="my-4">
+        💻 Client Page: Interactive client-side rendering in action. <br />
+        🚀 Server Page: Optimized static content for SEO.
+      </p>
+    </div>
+  );
+}
